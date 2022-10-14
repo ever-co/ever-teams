074531ad7089807382fa8c25f90e90afadb1b5ad
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2022-10-14 08:39:46 +0200

    minor changes

--- web/components/layout/header/teamDropdown.tsx
+++ web/components/layout/header/teamDropdown.tsx
@@ -18,7 +18,7 @@ export default function TeamDropdown() {
             <path
               fill-rule="evenodd"
               d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             />
           </svg>
         </button>
