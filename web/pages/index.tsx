13c8008a41ad87aabd0b537a4fa1ec4d2d8e9f5e
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2022-10-14 03:58:56 +0200

    tiny changes

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
       <TimerTasksSection />
       <TeamMemberSection />
       <div className="mt-6" />
-      <button className="px-9 py-2 inline-flex cursor-pointer rounded-md items-center border border-[#6E49E8]">
+      <button className="px-9 hover:bg-[#6e49e816] py-2 inline-flex cursor-pointer rounded-md items-center border border-[#6E49E8]">
         <span className="text-sm text-[#6E49E8] font-semibold">Invite</span>
       </button>
     </>
