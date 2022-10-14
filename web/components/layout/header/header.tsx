218680d0ed6001fddcbcc356658a9bdeeb08393f
Miracle Kasigwa <miraclekasigwa@gmail.com>
2022-10-14 22:10:51 +0100

    fixed image

--- web/components/layout/header/header.tsx
+++ web/components/layout/header/header.tsx
@@ -72,7 +72,7 @@ export const Header = () => (
               <span className="font-normal ml-3 block truncate dark:text-white">
                 Roslan Kan
               </span>
-              <Image
+              <img
                 src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                 alt=""
                 className="h-6 w-6 flex-shrink-0 rounded-full"
