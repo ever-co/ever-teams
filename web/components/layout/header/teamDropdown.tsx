ed7858a22c33a7f663f974c48435577ba635776a
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2022-10-14 08:42:26 +0200

    fixed: terminal warning and errors

--- web/components/layout/header/teamDropdown.tsx
+++ web/components/layout/header/teamDropdown.tsx
@@ -16,7 +16,7 @@ export default function TeamDropdown() {
             aria-hidden="true"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
               clipRule="evenodd"
             />
