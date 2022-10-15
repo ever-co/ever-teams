d07ddb1e85af2d3432571528dbb81afe07709381
Miracle Kasigwa <miraclekasigwa@gmail.com>
2022-10-15 16:55:19 +0100

    code format

--- web/components/common/dropDown.tsx
+++ web/components/common/dropDown.tsx
@@ -24,7 +24,7 @@ const DropDown = ({ data, handleSelectData, selectedData }: IDropDownProps) => {
   return (
     <Combobox value={selectedData} onChange={handleSelectData}>
       <div className="relative mt-1">
-        <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-red-500 dark:bg-red-500 text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-primary sm:text-sm">
+        <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-primary sm:text-sm">
           <Combobox.Input
             className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
             onChange={(event) => setQuery(event.target.value)}
