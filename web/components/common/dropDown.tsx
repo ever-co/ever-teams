2769e3f8117cfd3b3295d88a2bef0db077f4a155
Miracle Kasigwa <miraclekasigwa@gmail.com>
2022-10-15 16:28:11 +0100

    fixed dropdown issue

--- web/components/common/dropDown.tsx
+++ web/components/common/dropDown.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
-import { Combobox } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import { Combobox, Transition } from "@headlessui/react";
+// import { CheckIcon } from "@heroicons/react/solid";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/24/solid";
 
 interface IDropDownProps {
   data: string[];
@@ -13,20 +15,77 @@ const DropDown = ({ data, handleSelectData, selectedData }: IDropDownProps) => {
   const filteredData =
     query === ""
       ? data
-      : data.filter((value) => {
-          return value.toLowerCase().includes(query.toLowerCase());
-        });
+      : data.filter((value) =>
+          value
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
 
   return (
     <Combobox value={selectedData} onChange={handleSelectData}>
-      <Combobox.Input onChange={(event) => setQuery(event.target.value)} />
-      <Combobox.Options>
-        {filteredData.map((singleData) => (
-          <Combobox.Option key={singleData} value={singleData}>
-            {singleData}
-          </Combobox.Option>
-        ))}
-      </Combobox.Options>
+      <div className="relative mt-1">
+        <div className="relative w-full cursor-default overflow-hidden rounded bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-primary sm:text-sm">
+          <Combobox.Input
+            className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+            onChange={(event) => setQuery(event.target.value)}
+          />
+          <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+            <ChevronUpDownIcon
+              className="h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+          </Combobox.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          afterLeave={() => setQuery("")}
+        >
+          <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            {filteredData.length === 0 && query !== "" ? (
+              <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                Nothing found.
+              </div>
+            ) : (
+              filteredData.map((singleData) => (
+                <Combobox.Option
+                  key={singleData}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? "bg-primary text-white" : "text-gray-900"
+                    }`
+                  }
+                  value={singleData}
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {singleData}
+                      </span>
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? "text-white" : "text-primary"
+                          }`}
+                        >
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))
+            )}
+          </Combobox.Options>
+        </Transition>
+      </div>
     </Combobox>
   );
 };
