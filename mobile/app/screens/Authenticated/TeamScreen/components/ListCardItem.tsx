b66d48088f6f9a995ab52d001cd1c9496fd1c58e
Neosoulink <neodarksoulink@gmail.com>
2022-10-16 20:19:01 +0200

    refactor: improve list user task card ui

--- mobile/app/screens/Authenticated/TeamScreen/components/ListCardItem.tsx
+++ mobile/app/screens/Authenticated/TeamScreen/components/ListCardItem.tsx
@@ -1,98 +1,98 @@
 import React from "react"
-import { ScrollView, View, ViewStyle, TouchableOpacity } from "react-native"
+import { View, ViewStyle, Image, ImageStyle, TouchableNativeFeedback } from "react-native"
 
 // COMPONENTS
-import { Icon, Card, Text } from "../../../../components"
+import { Card, Text } from "../../../../components"
 
 // STYLES
 import { GLOBAL_STYLE as GS, CONSTANT_COLOR as CC } from "../../../../../assets/ts/styles"
 import { colors, spacing } from "../../../../theme"
 
-export type ListItemContentProps = {
+export type ListItemProps = {
   variant: "success" | "warning" | "danger"
 }
 
-export interface Props extends ListItemContentProps {}
+export interface Props extends ListItemProps {}
 
-export const ListItemContent: React.FC<ListItemContentProps> = ({ variant }) => (
-  <View style={{ ...GS.inlineItems }}>
-    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-      <TouchableOpacity>
-        <View style={{ ...GS.inlineItems }}>
-          <Text size="sm" weight="bold" style={{ ...GS.p2 }}>
-            User 1
+export const ListItemContent: React.FC<ListItemProps> = ({ variant }) => (
+  <TouchableNativeFeedback>
+    <View style={{ ...GS.p3 }}>
+      <View style={{ ...GS.inlineItems, ...GS.py2 }}>
+        <Image
+          source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Breezeicons-actions-22-im-user.svg/1200px-Breezeicons-actions-22-im-user.svg.png",
+          }}
+          style={$userProfile}
+        />
+
+        <Text preset="subheading">User name</Text>
+      </View>
+
+      <View style={{ ...GS.mb3 }}>
+        <Text weight="semiBold" size="md">
+          Task name:
+        </Text>
+        <Text>Task description</Text>
+      </View>
+
+      <View style={{}}>
+        <Text weight="semiBold" size="md" style={{ ...GS.mb1 }}>
+          Total worked time:
+        </Text>
+
+        <View style={{ ...GS.inlineItems, ...GS.justifyBetween }}>
+          <Text size="sm" style={{ ...GS.mb2 }}>
+            02:10:59
           </Text>
 
-          <View style={$listItemSeparatorContent} />
-
-          <Text size="sm" style={{ ...GS.p2, ...GS.flex1 }}>
-            Task description
-          </Text>
-
-          <View style={$listItemSeparatorContent} />
-
-          <View
-            style={{
-              ...GS.positionRelative,
-              ...GS.overflowHidden,
-              ...GS.roundedSm,
-              ...GS.mx2,
-              ...GS.mb2,
-              backgroundColor: colors.palette.neutral200,
-              width: parseInt("80"),
-            }}
-          >
-            <View style={{ ...GS.py1, width: `${70}%`, backgroundColor: CC[variant] }} />
-          </View>
-
-          <View style={$listItemSeparatorContent} />
-
-          <Text size="sm" style={{ ...GS.p2, ...GS.flex1 }}>
-            02.10.59
-          </Text>
-
-          <View style={$listItemSeparatorContent} />
-
-          <Text size="sm" style={{ ...GS.p2, ...GS.flex1 }}>
-            02.10.59
+          <Text size="sm" style={{ ...GS.mb2 }}>
+            03:00:00
           </Text>
         </View>
-      </TouchableOpacity>
-    </ScrollView>
 
-    <TouchableOpacity>
-      <Icon icon="caretRight" />
-    </TouchableOpacity>
-  </View>
+        <View
+          style={{
+            ...GS.positionRelative,
+            ...GS.overflowHidden,
+            ...GS.roundedSm,
+            ...GS.mb2,
+            backgroundColor: colors.palette.neutral200,
+          }}
+        >
+          <View style={{ ...GS.py1, width: `${70}%`, backgroundColor: CC[variant] }} />
+        </View>
+      </View>
+    </View>
+  </TouchableNativeFeedback>
 )
 
 const ListCardItem: React.FC<Props> = (props) => (
-  <View style={{ ...GS.inlineItems, ...GS.mb2 }}>
-    <Card
-      style={{
-        ...$listCard,
-        backgroundColor: colors.palette.neutral200 + "55",
-        borderColor: CC[props.variant],
-      }}
-      HeadingComponent={<ListItemContent {...props} />}
-    />
-  </View>
+  <Card
+    style={{
+      ...$listCard,
+      ...GS.mb3,
+      borderColor: CC[props.variant],
+    }}
+    ContentComponent={<ListItemContent {...props} />}
+  />
 )
 
 export default ListCardItem
 
 const $listCard: ViewStyle = {
-  ...GS.py0,
   ...GS.flex1,
-  ...GS.rounded0,
-  ...GS.noShadow,
+  ...GS.p0,
+  ...GS.rounded,
   ...GS.noBorder,
+  ...GS.shadow,
   borderLeftWidth: spacing.extraSmall - spacing.micro,
   minHeight: null,
 }
-
-const $listItemSeparatorContent: ViewStyle = {
-  backgroundColor: colors.palette.neutral900,
-  width: spacing.micro,
-  height: "100%",
+const $userProfile: ImageStyle = {
+  ...GS.roundedFull,
+  ...GS.mr2,
+  ...GS.borderSm,
+  backgroundColor: colors.background,
+  width: spacing.huge - spacing.tiny,
+  height: spacing.huge - spacing.tiny,
 }
