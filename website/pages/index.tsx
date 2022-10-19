ea7e40936e8c9baeba43df615a6cf23aca9dbb09
Ruslan Konviser <evereq@gmail.com>
2022-10-19 19:57:17 +0200

    Update index.tsx

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return <section>
-    <h1 className="text-3xl font-bold flex items-center justify-center h-screen">Gauzy Team</h1>    
+    <h1 className="text-3xl font-bold flex items-center justify-center h-screen">Gauzy Teams</h1>    
   </section>
 };
 
