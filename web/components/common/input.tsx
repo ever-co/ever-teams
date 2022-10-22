828dcc83596367f40f4577a360a47dd4f1153776
Miracle Kasigwa <miraclekasigwa@gmail.com>
2022-10-22 10:49:06 +0100

    improved design for invite user

--- web/components/common/input.tsx
+++ web/components/common/input.tsx
@@ -11,13 +11,13 @@ const Input = ({
   centered = false,
 }: IInputProps) => {
   return (
-    <div className="mt-10">
+    <div className="mt-[39px]">
       {value.length > 0 && (
         <label
           htmlFor={name}
           className={`block ${
             centered && "text-center"
-          } text-sm font-light text-[#ACB3BB]`}
+          } text-[14px] font-light text-[#ACB3BB]`}
         >
           {label}
         </label>
@@ -30,9 +30,12 @@ const Input = ({
         placeholder={placeholder}
         required={required}
         onChange={onChange}
-        className={`w-full pt-1 ${
+        className={`w-full text-[18px] pt-1 ${
           centered && "text-center"
-        } border-b placeholder:font-light border-[#D7E1EB] bg-white pb-1 font-medium text-primary dark:text-white outline-none dark:bg-transparent`}
+        } border-b focus:border-b-2
+        placeholder:font-light placeholder:text-[16px] border-[#D7E1EB]
+         focus:border-[#1B005D] bg-white pb-1 font-medium text-primary
+          dark:text-white outline-none dark:bg-transparent`}
       />
     </div>
   );
