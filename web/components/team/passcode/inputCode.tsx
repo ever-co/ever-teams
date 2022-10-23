d59ba5c42ddab4220908613e7b2f3825c8605a18
Miracle Kasigwa <miraclekasigwa@gmail.com>
2022-10-23 07:22:46 +0100

    fixed alignment

--- web/components/team/passcode/inputCode.tsx
+++ web/components/team/passcode/inputCode.tsx
@@ -3,7 +3,9 @@ const InputCode = () => {
     <input
       type="text"
       maxLength={1}
-      className="rounded-md w-[45px] round-[8px] border border-[#F5F6FB] bg-gray-100 py-2 px-2 text-xl text-center font-light text-[#9490A0] outline-none focus:border-[#7D56fd] focus:border focus:shadow-md dark:bg-dark_background_color"
+      className="rounded-md w-[45px] round-[8px] border border-[#F5F6FB] bg-[#F5F6FB] 
+      py-2 px-2 text-xl text-center font-light text-[#9490A0] outline-none focus:border-[#7D56fd] 
+      focus:border focus:shadow-md dark:bg-dark_background_color"
     />
   );
 };
