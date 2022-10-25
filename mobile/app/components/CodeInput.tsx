c3fa8cb6bfd5b38f74eb0999bd17ff80a62de92f
Serge Muhundu <sergemuhundu2@gmail.com>
2022-10-25 07:54:49 +0100

    Improve the create new Team and join existed team screen

---
+++ mobile/app/components/CodeInput.tsx
@@ -0,0 +1,41 @@
+import React from "react"
+import { TextInput, View } from "react-native"
+import { colors } from "../theme"
+
+interface inputType {
+  index: number
+  item: string
+}
+
+export const CodeInput = () => {
+  let inputs = [1, 2, 3, 4, 5, 6]
+  return (
+    <View style={{ flexDirection: "row" }}>
+      {inputs.map((item) => (
+        <View
+          key={item}
+          style={{
+            width: "13%",
+            height: 35,
+            display: "flex",
+            borderRadius: 5,
+            justifyContent: "center",
+            marginHorizontal: 5,
+            borderWidth: 1,
+            borderColor: colors.border,
+          }}
+        >
+          <TextInput
+            placeholder="1"
+            style={{
+              width: "100%",
+              height: "96%",
+              textAlign: "center",
+              backgroundColor: colors.palette.neutral200,
+            }}
+          />
+        </View>
+      ))}
+    </View>
+  )
+}
--- mobile/app/screens/LoginScreen.tsx
+++ mobile/app/screens/LoginScreen.tsx
@@ -3,7 +3,14 @@ import { observer } from "mobx-react-lite"
 import React, { FC, useEffect, useMemo, useRef, useState } from "react"
 import { Pressable } from "react-native"
 import { TextInput, TextStyle, View, ViewStyle, Image, ImageStyle } from "react-native"
-import { Button, Icon, Screen, Text, TextField, TextFieldAccessoryProps } from "../components"
+import {
+  Button,
+  Icon,
+  Screen,
+  Text,
+  TextField,
+  TextFieldAccessoryProps,
+} from "../components"
 import { useStores } from "../models"
 import { AppStackScreenProps } from "../navigators"
 import { colors, spacing } from "../theme"
@@ -11,9 +18,11 @@ import { typography } from "../theme"
 import { Api } from "../services/api"
 
 import * as Animatable from "react-native-animatable"
+import { CodeInput } from "../components/CodeInput"
 const pkg = require("../../package.json")
 
-const welcomeLogo = require("../../assets/images/logo.png")
+const welcomeLogo = require("../../assets/images/gauzy-teams-blue.png")
+const inviteCodeLogo = require("../../assets/images/lock-cloud.png")
 interface LoginScreenProps extends AppStackScreenProps<"Login"> {}
 
 export const LoginScreen: FC<LoginScreenProps> = observer(function LoginScreen(_props) {
@@ -317,6 +326,10 @@ export const LoginScreen: FC<LoginScreenProps> = observer(function LoginScreen(_
         //JOIN TEAM SCREEN STARTS HERE
         <Animatable.View animation="bounceInUp" style={$container}>
           <View style={$form}>
+            <View style={$joinTeamLogoContainer}>
+              <Image style={$joinTeamLogo} source={inviteCodeLogo} resizeMode="contain" />
+            </View>
+
             <Text
               testID="login-heading"
               tx="loginScreen.enterDetails2"
@@ -324,7 +337,14 @@ export const LoginScreen: FC<LoginScreenProps> = observer(function LoginScreen(_
               style={$text}
             />
 
-            <TextField
+<Text
+              testID="login-heading"
+              tx="loginScreen.confirmDetails2"
+              preset="heading"
+              style={$confirmtext}
+            />
+
+            {/* <TextField
               ref={authTeamInput}
               value={authInviteCode}
               onChangeText={setAuthInviteCode}
@@ -336,7 +356,10 @@ export const LoginScreen: FC<LoginScreenProps> = observer(function LoginScreen(_
               helper={errors?.authUsername}
               status={errors?.authUsername ? "error" : undefined}
               onSubmitEditing={() => authTeamInput.current?.focus()}
-            />
+            /> */}
+
+            <CodeInput />
+
             <TextField
               value={authEmail}
               onChangeText={setAuthEmail}
@@ -410,6 +433,7 @@ const $header: ViewStyle = {
   width: "100%",
   display: "flex",
   alignItems: "center",
+  paddingTop: 15,
   justifyContent: "flex-start",
 }
 
@@ -433,25 +457,46 @@ const $form: ViewStyle = {
 const $smalltext: TextStyle = {
   marginBottom: spacing.small,
   position: "absolute",
-  top: 50,
+  top: 40,
   fontSize: 16,
   color: colors.text,
   fontFamily: typography.secondary.normal,
-  fontWeight: "700",
+  fontWeight: "300",
 }
 const $text: TextStyle = {
   marginBottom: spacing.small,
-
-  fontSize: 26,
+  fontSize: 20,
   color: colors.text,
   fontFamily: typography.secondary.normal,
   fontWeight: "700",
 }
 
+const $confirmtext : TextStyle={
+  marginBottom: spacing.small,
+  fontSize: 16,
+  color: colors.text,
+  textAlign:'center',
+  fontFamily: typography.secondary.normal,
+  fontWeight: "300",
+}
+
 const $welcomeLogo: ImageStyle = {
+  width: "40%",
+}
+
+const $joinTeamLogo: ImageStyle = {
   width: "100%",
 }
 
+const $joinTeamLogoContainer: ViewStyle = {
+  width: 120,
+  height: 120,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: colors.palette.neutral200,
+  borderRadius: 60,
+}
 const $textField: ViewStyle = {
   marginBottom: spacing.large,
   width: "98%",
