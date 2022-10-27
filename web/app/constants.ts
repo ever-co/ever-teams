e66e92af29705e6504d21afa4d5fbfe24ca73339
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2022-10-27 13:03:15 +0200

    minor changes

--- web/app/constants.ts
+++ web/app/constants.ts
@@ -9,4 +9,4 @@ export const REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
 export const RECAPTCHA_SITE_KEY = process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY;
 export const RECAPTCHA_SECRET_KEY = process.env.CAPTCHA_SECRET_KEY;
 
-export const GAUZY_API_SERVER_URL = process.env.GAUZY_DEMOAPI_SERVER_URL;
+export const GAUZY_API_SERVER_URL = process.env.GAUZY_API_SERVER_URL;
