4b5329ff14367f078a340d8161b1964b3d6df1a1
Miracle Kasigwa <miraclekasigwa@gmail.com>
2022-10-31 21:08:53 +0100

    added icons to the task lisk

---
+++ web/components/common/main/dropdownIcons/unassigned-task.tsx
@@ -0,0 +1,22 @@
+export const UnassignedTask = () => {
+  return (
+    <svg
+      aria-hidden="true"
+      height="16"
+      viewBox="0 0 16 16"
+      version="1.1"
+      width="16"
+      data-view-component="true"
+      className="octicon octicon-issue-opened mr-4"
+    >
+      <path d="M8 9.5a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" fill="#8b949e"></path>
+      <path
+        fill-rule="evenodd"
+        d="M8 0a8 8 0 100 16A8 8 0 008 0zM1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0z"
+        fill="#8b949e"
+      ></path>
+    </svg>
+  );
+};
+
+export default UnassignedTask;
