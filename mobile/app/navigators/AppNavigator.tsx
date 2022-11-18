936f99d607ce8a48c148578d40f560f59dbba330
Serge Muhundu <sergemuhundu2@gmail.com>
2022-11-18 23:35:39 +0800

    improve team screen design and remove Welcome screen

--- mobile/app/navigators/AppNavigator.tsx
+++ mobile/app/navigators/AppNavigator.tsx
@@ -70,15 +70,15 @@ const AppStack = observer(function AppStack() {
   return (
     <Stack.Navigator
       screenOptions={{ headerShown: false }}
-      initialRouteName={isAuthenticated ? "Welcome" : "Login"} // @demo remove-current-line
+      initialRouteName={isAuthenticated ? "Authenticated" : "Login"} // @demo remove-current-line
     >
       {/* @demo remove-block-start */}
       {isAuthenticated ? (
         <>
           {/* @demo remove-block-end */}
-          <Stack.Screen name="Welcome" component={WelcomeScreen} />
+          {/* <Stack.Screen name="Welcome" component={WelcomeScreen} /> */}
           {/* @demo remove-block-start */}
-          <Stack.Screen name="Demo" component={DemoNavigator} />
+          {/* <Stack.Screen name="Demo" component={DemoNavigator} /> */}
           {/* @app custom-block */}
           <Stack.Screen name="Authenticated" component={AuthenticatedNavigator} />
         </>
