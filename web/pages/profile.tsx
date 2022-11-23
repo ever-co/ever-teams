cb9d56ed6faeb60c7854fb65917d7687f9415e28
Miracle Kasigwa <miraclekasigwa@gmail.com>
2022-11-23 22:57:51 +0100

    added assignt ask button and filter by status selector

--- web/pages/profile.tsx
+++ web/pages/profile.tsx
@@ -8,6 +8,7 @@ import useAuthenticateUser from "@app/hooks/useAuthenticateUser";
 import { Capitalize } from "@components/layout/header/profile";
 import Link from "next/link";
 import { useTeamTasks } from "@app/hooks/useTeamTasks";
+import StatusDropdown from "@components/common/main/status-dropdown";
 
 interface ITimerTasksSection {
   started: boolean;
@@ -93,39 +94,49 @@ const Profile = () => {
           </div>
         </div>
 
-        <div className="my-[41px] text-[18px] text-[#ACB3BB] font-light flex">
-          <div
-            className={`mr-10 ${
-              tab === "worked" && "font-medium"
-            } cursor-pointer`}
-            onClick={() => setTab("worked")}
-          >
-            Worked
-            {tab === "worked" && (
-              <div className="w-[65px] h-[2px] bg-[#ACB3BB]" />
-            )}
+        <div className="my-[41px] text-[18px] text-[#ACB3BB] font-light flex justify-between items-center w-full">
+          <div className="flex">
+            <div
+              className={`mr-10 ${
+                tab === "worked" && "font-medium"
+              } cursor-pointer`}
+              onClick={() => setTab("worked")}
+            >
+              Worked
+              {tab === "worked" && (
+                <div className="w-[65px] h-[2px] bg-[#ACB3BB]" />
+              )}
+            </div>
+            <div
+              className={`mr-10 ${
+                tab === "assigned" && "font-medium"
+              } cursor-pointer`}
+              onClick={() => setTab("assigned")}
+            >
+              Assigned
+              {tab === "assigned" && (
+                <div className="w-[78px] h-[2px] bg-[#ACB3BB]" />
+              )}
+            </div>
+            <div
+              className={`mr-10 ${
+                tab === "unassigned" && "font-medium"
+              } cursor-pointer`}
+              onClick={() => setTab("unassigned")}
+            >
+              Unassigned
+              {tab === "unassigned" && (
+                <div className="w-[98px] h-[2px] bg-[#ACB3BB]" />
+              )}
+            </div>
           </div>
-          <div
-            className={`mr-10 ${
-              tab === "assigned" && "font-medium"
-            } cursor-pointer`}
-            onClick={() => setTab("assigned")}
-          >
-            Assigned
-            {tab === "assigned" && (
-              <div className="w-[78px] h-[2px] bg-[#ACB3BB]" />
-            )}
-          </div>
-          <div
-            className={`mr-10 ${
-              tab === "unassigned" && "font-medium"
-            } cursor-pointer`}
-            onClick={() => setTab("unassigned")}
-          >
-            Unassigned
-            {tab === "unassigned" && (
-              <div className="w-[98px] h-[2px] bg-[#ACB3BB]" />
-            )}
+          <div className="flex items-center">
+            <div className="mr-4 h-full">
+              <StatusDropdown />
+            </div>
+            <button className="rounded-[7px] hover:bg-opacity-80 w-[140px] text-md h-[36px] bg-primary text-white dark:bg-[#1B1B1E] dark:text-[#ACB3BB] dark:border-white dark:hover:text-white">
+              Assign Task
+            </button>
           </div>
         </div>
         <div className="flex items-center justify-between">
