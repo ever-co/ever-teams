60bfad0917ae29c77188e3c17fd597634f86f111
Miracle Kasigwa <miraclekasigwa@gmail.com>
2022-11-25 00:05:29 +0100

    added timer into store

--- web/app/hooks/useTimer.ts
+++ web/app/hooks/useTimer.ts
@@ -2,7 +2,12 @@ import {
   getTimerStatusAPI,
   toggleTimerAPI,
 } from "@app/services/client/api/timer";
-import { timerStatusState } from "@app/stores";
+import {
+  intervState,
+  runningStatusStateBiss,
+  timerDataState,
+  timerStatusState,
+} from "@app/stores";
 import { useCallback, useEffect, useRef } from "react";
 import { useRecoilState } from "recoil";
 import { useQuery } from "./useQuery";
@@ -13,6 +18,10 @@ export function useTimer() {
   const { queryCall, loading } = useQuery(getTimerStatusAPI);
   const { queryCall: toggleQueryCall } = useQuery(toggleTimerAPI);
   const [timerStatus, setTimerStatus] = useRecoilState(timerStatusState);
+  const [time, setTime] = useRecoilState(timerDataState);
+  const [interv, setInterv] = useRecoilState(intervState);
+  const [running, setRunning] = useRecoilState(runningStatusStateBiss);
+
   const firstLoad = useRef(false);
 
   const getTimerStatus = useCallback(() => {
@@ -22,11 +31,42 @@ export function useTimer() {
       }
       return res;
     });
-  }, []);
+  }, [queryCall, setTimerStatus]);
 
-  const startTimer = useCallback(() => {}, []);
+  const startTimer = () => {
+    run();
+    setRunning(true);
+    setInterv(setInterval(run, 10));
+  };
 
-  const stopTimer = useCallback(() => {}, []);
+  const stopTimer = () => {
+    clearInterval(interv);
+    setRunning(false);
+  };
+
+  var updatedMs = time.ms,
+    updatedS = time.s,
+    updatedM = time.m,
+    updatedH = time.h;
+
+  const run = () => {
+    if (updatedM === 60) {
+      updatedH++;
+      updatedM = 0;
+    }
+
+    if (updatedS === 60) {
+      updatedM++;
+      updatedS = 0;
+    }
+
+    if (updatedMs === 100) {
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ ms: updatedMs, s: updatedS, m: updatedM, h: updatedH });
+  };
 
   // To be called once
   const initiateTimer = useCallback(() => {
@@ -43,6 +83,8 @@ export function useTimer() {
     getTimerStatus,
     loading,
     timerStatus,
+    running,
+    time,
     initiateTimer,
     startTimer,
     stopTimer,
--- web/app/stores/timer.ts
+++ web/app/stores/timer.ts
@@ -1,7 +1,22 @@
-import { ITimerStatus } from "@app/interfaces/ITimer";
+import { ITimerData, ITimerStatus } from "@app/interfaces/ITimer";
 import { atom } from "recoil";
 
 export const timerStatusState = atom<ITimerStatus | null>({
   key: "timerStatusState",
   default: null,
+});
+
+export const timerDataState = atom<ITimerData>({
+  key: "timerDataState",
+  default: { ms: 0, s: 0, m: 0, h: 0 },
+});
+
+export const intervState = atom<any>({
+  key: "intervState",
+  default: null,
+});
+
+export const runningStatusStateBiss = atom<boolean>({
+  key: "runningStatusStateBiss",
+  default: false,
 });
