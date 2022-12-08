051535a41dac61f779691061530e99cbce829644
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2022-12-08 17:53:27 +0200

    Fixes / Improvements WEB #341
    * text bigger on edit task input user card

--- apps/web/components/main/card.tsx
+++ apps/web/components/main/card.tsx
@@ -235,7 +235,7 @@ const Card = ({ member }: { member: IMember }) => {
 							value={formValues.devTask}
 							onChange={handleChange}
 							onKeyPress={(event) => event.key === 'Enter' && handleTaskEdit()}
-							className="w-full resize-none h-[48px] text-xs rounded-lg px-2 py-2 shadow-inner border border-[#D7E1EB] dark:border-[#27272A]"
+							className="w-full resize-none h-[48px] rounded-lg px-2 py-2 shadow-inner border border-[#D7E1EB] dark:border-[#27272A]"
 						/>
 						<span className="w-3 h-5 ml-2">
 							{updateLoading && <Spinner dark={false} />}
