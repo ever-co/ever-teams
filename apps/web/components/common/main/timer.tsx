fbc87a7ebc1eb8c374d7dde4c6f274fa004bf91d
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2022-12-20 18:38:29 +0200

    reverse new ui design

--- apps/web/components/common/main/timer.tsx
+++ apps/web/components/common/main/timer.tsx
@@ -1,12 +1,9 @@
 import { pad } from '@app/helpers/number';
 import { useTaskStatistics } from '@app/hooks/features/useTaskStatistics';
 import { useTimer } from '@app/hooks/features/useTimer';
-import { useTheme } from 'next-themes';
 import { ProgressBar } from '../progress-bar';
 import { PauseIcon } from './pauseIcon';
 import { PlayIcon } from './playIcon';
-import { SlashIcon } from './slashIcon';
-import { SlashIconDark } from './slashIconDark';
 
 const Timer = () => {
 	const {
@@ -29,33 +26,32 @@ const Timer = () => {
 			startTimer();
 		}
 	};
-	const { theme } = useTheme();
 
 	return (
 		<>
-			<div className="flex flex-col min-w-[227px] ml-[30px]">
-				<h1 className="text-[40px] text-[#282048] dark:text-[rgb(255,255,255)] font-['Plus Jakarta Sans'] font-medium">
-					{pad(hours)} : {pad(minutes)} : {pad(seconds)}
-					<span className="text-[20px] w-7 inline-block">:{pad(ms_p)}</span>
+			<div className="flex flex-col min-w-[300px]">
+				<h1 className="text-[53px] text-primary dark:text-[#FFFFFF]">
+					{pad(hours)} : {pad(minutes)} : {pad(seconds)}:
+					<span className="text-[35px] w-7 inline-block">{pad(ms_p)}</span>
 				</h1>
-				<ProgressBar width={221} progress={`${activeTaskEstimation}%`} />
+				<ProgressBar width={284} progress={`${activeTaskEstimation}%`} />
 			</div>
-			<div className="">
-				{theme === 'dark' ? <SlashIconDark /> : <SlashIcon />}
-			</div>
-
 			<div
 				title={
 					timerStatusFetching || !canRunTimer
 						? 'Please, select or create a new task to start tracking the time'
 						: undefined
 				}
-				className={`cursor-pointer mt-[35px] ${
-					timerStatusFetching || !canRunTimer ? '' : ''
+				className={`cursor-pointer ${
+					timerStatusFetching || !canRunTimer ? 'opacity-30' : ''
 				}`}
 				onClick={!timerStatusFetching ? timerHanlder : undefined}
 			>
-				{timerStatus?.running ? <PauseIcon /> : <PlayIcon />}
+				{timerStatus?.running ? (
+					<PauseIcon width={68} height={68} />
+				) : (
+					<PlayIcon width={68} height={68} />
+				)}
 			</div>
 		</>
 	);
--- apps/web/components/layout/footer/footer.tsx
+++ apps/web/components/layout/footer/footer.tsx
@@ -1,63 +1,63 @@
-import ToggleThemeContainer from "../toggleThemeBtns";
+import ToggleThemeContainer from '../toggleThemeBtns';
 
 const Footer = () => {
-  return (
-    <footer className="py-2 flex flex-col md:flex-row items-center text-sm lg:text-base text-center sm:text-start justify-between font-light x-container-fluid">
-      <div className="text-light flex flex-col space-x-1 sm:flex-row md:flex-start sm:items-center  text-center md:flex-row mx-[80px]">
-        <div className="flex items-center justify-between sm:justify-start sm:space-x-2 space-x-1">
-          <span className="px-1">@ 2022-Present,</span>
-          <a
-            href="https://gauzy.team"
-            target="_blank"
-            className="text-primary dark:text-gray-300"
-            rel="noreferrer"
-          >
-            Gauzy Teams
-          </a>
-          <div>by</div>
-          <a
-            href="https://ever.co"
-            target="_blank"
-            className="text-primary dark:text-gray-300"
-            rel="noreferrer"
-          >
-            Ever Co. LTD.
-          </a>{" "}
-          <div className="hidden xs:flex space-x-2 sm:hidden">
-            <ToggleThemeContainer />
-          </div>
-        </div>
-        <div>All rights reserved.</div>
-        <div className="hidden sm:flex space-x-2 md:hidden">
-          <ToggleThemeContainer />
-        </div>
-      </div>
+	return (
+		<footer className="py-2 flex flex-col md:flex-row items-center text-sm lg:text-base text-center sm:text-start justify-between font-light x-container-fluid">
+			<div className="text-light flex flex-col space-x-1 sm:flex-row md:flex-start sm:items-center  text-center md:flex-row">
+				<div className="flex items-center justify-between sm:justify-start sm:space-x-2 space-x-1">
+					<span className="px-1">@ 2022-Present,</span>
+					<a
+						href="https://gauzy.team"
+						target="_blank"
+						className="text-primary dark:text-gray-300"
+						rel="noreferrer"
+					>
+						Gauzy Teams
+					</a>
+					<div>by</div>
+					<a
+						href="https://ever.co"
+						target="_blank"
+						className="text-primary dark:text-gray-300"
+						rel="noreferrer"
+					>
+						Ever Co. LTD.
+					</a>{' '}
+					<div className="hidden xs:flex space-x-2 sm:hidden">
+						<ToggleThemeContainer />
+					</div>
+				</div>
+				<div>All rights reserved.</div>
+				<div className="hidden sm:flex space-x-2 md:hidden">
+					<ToggleThemeContainer />
+				</div>
+			</div>
 
-      <div className="flex flex-col xs:flex-row sm:flex-col items-center md:flex-row">
-        <div className="flex space-x-4 text-center w-full justify-center">
-          <a
-            href="https://demo.gauzy.co/#/pages/legal/terms"
-            target="_blank"
-            className="text-primary dark:text-gray-300"
-            rel="noreferrer"
-          >
-            Terms Of Service
-          </a>
-          <a
-            href="https://demo.gauzy.co/#/pages/legal/privacy"
-            target="_blank"
-            className="text-primary dark:text-gray-300"
-            rel="noreferrer"
-          >
-            Privacy Policy
-          </a>
-        </div>
-        <div className="space-x-2 px-4 flex xs:hidden md:flex">
-          <ToggleThemeContainer />
-        </div>
-      </div>
-    </footer>
-  );
+			<div className="flex flex-col xs:flex-row sm:flex-col items-center md:flex-row">
+				<div className="flex space-x-4 text-center w-full justify-center">
+					<a
+						href="https://demo.gauzy.co/#/pages/legal/terms"
+						target="_blank"
+						className="text-primary dark:text-gray-300"
+						rel="noreferrer"
+					>
+						Terms Of Service
+					</a>
+					<a
+						href="https://demo.gauzy.co/#/pages/legal/privacy"
+						target="_blank"
+						className="text-primary dark:text-gray-300"
+						rel="noreferrer"
+					>
+						Privacy Policy
+					</a>
+				</div>
+				<div className="space-x-2 px-4 flex xs:hidden md:flex">
+					<ToggleThemeContainer />
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
