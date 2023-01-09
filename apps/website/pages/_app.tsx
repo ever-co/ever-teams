17f7d2b6398d1e7cb2e42cfbafb7dcb03cb5650e
Miracle Kasigwa <miraclekasigwa@gmail.com>
2023-01-09 11:43:00 +0200

    added vercel audience analytics analytics

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
@@ -16,6 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');`}
 			</Script>
 			<Component {...pageProps} />
+			<Analytics />
 		</>
 	);
 }
