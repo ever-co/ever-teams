f3d9a010abb71e4d5f24a70de4d1560f88c45a31
Miracle Kasigwa <miraclekasigwa@gmail.com>
2023-01-09 11:18:13 +0200

    integrated google analytics

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -4,10 +4,21 @@ import { ThemeProvider } from 'next-themes';
 import { RecoilRoot } from 'recoil';
 import { AppState } from '@components/app/InitState';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<Script
+				strategy="lazyOnload"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+			/>
+			<Script strategy="lazyOnload" id="google-analytic-script">
+				{` window.dataLayer = window.dataLayer || [];
+  				function gtag(){dataLayer.push(arguments);}
+  				gtag('js', new Date());
+  				gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');`}
+			</Script>
 			<Head>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link
