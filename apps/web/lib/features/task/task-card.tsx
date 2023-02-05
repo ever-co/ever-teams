5d23c03586ca5a29b3a42c16da714c6c6d6fcfd9
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2023-02-05 20:45:07 +0200

    fixed: build error and warning

--- apps/web/lib/features/task/task-card.tsx
+++ apps/web/lib/features/task/task-card.tsx
@@ -119,7 +119,7 @@ function TimerButtonCall({ task }: { task: ITeamTask }) {
 		window.setTimeout(startTimer, 100);
 
 		window.scrollTo({ top: 0, behavior: 'smooth' });
-	}, [timerStatus, setActiveTask, task, stopTimer]);
+	}, [timerStatus, setActiveTask, task, stopTimer, startTimer]);
 
 	return loading ? (
 		<SpinnerLoader size={30} />
