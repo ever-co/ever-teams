e8407972750c125fa95e69f65deb0f0d8892e1e3
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2023-02-19 17:06:53 +0200

    feat: confirm email modal and form

--- apps/web/lib/features/unverified-email.tsx
+++ apps/web/lib/features/unverified-email.tsx
@@ -1,7 +1,14 @@
 import { getAccessTokenCookie } from '@app/helpers';
-import { useAuthenticateUser } from '@app/hooks';
+import { useAuthenticateUser, useModal } from '@app/hooks';
 import { clsxm } from '@app/utils';
-import { Card, Text } from 'lib/components';
+import {
+	AuthCodeInputField,
+	BackButton,
+	Button,
+	Card,
+	Modal,
+	Text,
+} from 'lib/components';
 import { CloseIcon } from 'lib/components/svgs';
 import { useTranslation } from 'lib/i18n';
 import { useCallback, useEffect, useState } from 'react';
@@ -10,6 +17,8 @@ export function UnverifiedEmail() {
 	const { user } = useAuthenticateUser();
 	const { trans, translations } = useTranslation('home');
 	const [verified, setVefified] = useState(true);
+
+	const { openModal, isOpen, closeModal } = useModal();
 
 	const closeIt = useCallback(() => {
 		window.localStorage.setItem(
@@ -41,26 +50,74 @@ export function UnverifiedEmail() {
 	}, [user]);
 
 	return !verified ? (
-		<Card
-			shadow="bigger"
-			className={clsxm(
-				'w-full mt-6 flex justify-between',
-				'border dark:border-[#28292F] dark:shadow-lg dark:bg-[#1B1D22]'
-			)}
-		>
-			<Text>
-				{trans.SENT_EMAIL_VERIFICATION}. {translations.common.PLEASE}{' '}
-				<span className="text-primary dark:text-primary-light">
-					{translations.common.VERIFY}
-				</span>{' '}
-				{translations.common.YOUR_EMAIL}
-			</Text>
+		<>
+			<Card
+				shadow="bigger"
+				className={clsxm(
+					'w-full mt-6 flex justify-between',
+					'border dark:border-[#28292F] dark:shadow-lg dark:bg-[#1B1D22]'
+				)}
+			>
+				<Text>
+					{trans.SENT_EMAIL_VERIFICATION}. {translations.common.PLEASE}{' '}
+					<span
+						className="text-primary dark:text-primary-light cursor-pointer"
+						onClick={openModal}
+					>
+						{translations.common.VERIFY}
+					</span>{' '}
+					{translations.common.YOUR_EMAIL}
+				</Text>
 
-			<button onClick={closeIt}>
-				<CloseIcon />
-			</button>
-		</Card>
+				<button onClick={closeIt}>
+					<CloseIcon />
+				</button>
+			</Card>
+			<ConfirmUserModal open={isOpen} closeModal={closeModal} />
+		</>
 	) : (
 		<></>
+	);
+}
+
+export function ConfirmUserModal({
+	open,
+	closeModal,
+}: {
+	open: boolean;
+	closeModal: () => void;
+}) {
+	const { trans } = useTranslation();
+
+	return (
+		<Modal isOpen={open} closeModal={closeModal}>
+			<form className="w-[98%] md:w-[530px]" autoComplete="off">
+				<Card className="w-full" shadow="custom">
+					<div className="flex flex-col justify-between items-center">
+						<Text.Heading as="h3" className="text-center">
+							{trans.common.SECURITY_CODE}
+						</Text.Heading>
+
+						<div className="w-full mt-5">
+							<AuthCodeInputField
+								allowedCharacters="numeric"
+								length={6}
+								containerClassName="mt-[21px] w-full flex justify-between"
+								inputClassName="w-[40px] xs:w-[50px]"
+								onChange={(code) => {}}
+							/>
+						</div>
+
+						<div className="w-full flex justify-between mt-3 items-center">
+							<BackButton onClick={closeModal} />
+
+							<Button type="submit" loading={false}>
+								{trans.common.CONFIRM}
+							</Button>
+						</div>
+					</div>
+				</Card>
+			</form>
+		</Modal>
 	);
 }
