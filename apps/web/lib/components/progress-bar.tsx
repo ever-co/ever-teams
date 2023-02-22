4dc93cc62532d2a53accbe2c07409399d52301fb
Sineastra <lord.of.light.0002@gmail.com>
2023-02-22 13:16:12 +0200

    added showPercents  to be used

--- apps/web/lib/components/progress-bar.tsx
+++ apps/web/lib/components/progress-bar.tsx
@@ -26,9 +26,11 @@ export function ProgressBar({
 				/>
 				<div className="bg-[#F0F0F0] dark:bg-[#2B303B] h-2 rounded-full w-full  absolute z-[0]" />
 			</div>
-			<div className="not-italic font-medium text-[12px] leading-[140%] tracking-[-0.02em] ml-2 text-[rgba(40,32,72,0.5)]">
-				{progress}
-			</div>
+			{showPercents && (
+				<div className="not-italic font-medium text-[12px] leading-[140%] tracking-[-0.02em] ml-2 text-[rgba(40,32,72,0.5)]">
+					{progress}
+				</div>
+			)}
 		</>
 	);
 }
