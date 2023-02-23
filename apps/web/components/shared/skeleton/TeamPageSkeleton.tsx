8db778846ec97b22aa604ed7eddc1826f23b37cb
zlatina-moga <zlatina.delcheva@gmail.com>
2023-02-23 08:13:10 +0200

    team page skeleton

--- apps/web/components/shared/skeleton/TeamPageSkeleton.tsx
+++ apps/web/components/shared/skeleton/TeamPageSkeleton.tsx
@@ -1,7 +1,14 @@
 import Skeleton from 'react-loading-skeleton';
 import { MainLayout, MainHeader } from 'lib/layout';
+import { Card, Container, VerticalSeparator } from 'lib/components';
+import { DraggerIcon } from 'lib/components/svgs';
+import UserTeamCardSkeleton from './UserTeamCardSkeleton';
 
 const TeamPageSkeleton = () => {
+	const TeamCards = Array.from({ length: 5 }, (_, index) => {
+		return <UserTeamCardSkeleton key={index} />;
+	});
+
 	return (
 		<MainLayout>
 			<MainHeader>
@@ -69,6 +76,81 @@ const TeamPageSkeleton = () => {
 					</li>
 				</ul>
 			</MainHeader>
+			<Container className="mb-10">
+				{TeamCards}
+				<Card shadow="bigger" className="relative flex items-center py-4 my-6">
+					<div className="opacity-40 absolute -left-0">
+						<DraggerIcon />
+					</div>
+					<div className="w-[330px] px-4 flex space-x-3 items-center">
+						<div className="opacity-40 w-8 h-8 bg-slate-400 rounded-full" />
+						<Skeleton
+							height={20}
+							width={180}
+							borderRadius={10}
+							className="dark:bg-dark--theme-light"
+						/>
+					</div>
+					<VerticalSeparator />
+
+					<div className="w-[330px] px-4 flex items-start">
+						<Skeleton
+							height={10}
+							width={120}
+							borderRadius={10}
+							className="dark:bg-dark--theme-light mr-2"
+						/>
+					</div>
+					<VerticalSeparator className="ml-2" />
+
+					<div className="flex space-y-2 items-center w-48 justify-center flex-col">
+						<Skeleton
+							height={20}
+							width={120}
+							borderRadius={10}
+							className="dark:bg-dark--theme-light"
+						/>
+						<Skeleton
+							height={10}
+							width={160}
+							borderRadius={10}
+							className="dark:bg-dark--theme-light"
+						/>
+					</div>
+					<VerticalSeparator />
+
+					<div className="flex space-y-2 items-center w-48 justify-center flex-col">
+						<Skeleton
+							height={20}
+							width={120}
+							borderRadius={10}
+							className="dark:bg-dark--theme-light"
+						/>
+						<Skeleton
+							height={10}
+							width={160}
+							borderRadius={10}
+							className="dark:bg-dark--theme-light"
+						/>
+					</div>
+					<VerticalSeparator />
+
+					<div className="flex space-y-2 items-center w-48 justify-center flex-col">
+						<Skeleton
+							height={20}
+							width={120}
+							borderRadius={10}
+							className="dark:bg-dark--theme-light"
+						/>
+						<Skeleton
+							height={10}
+							width={160}
+							borderRadius={10}
+							className="dark:bg-dark--theme-light"
+						/>
+					</div>
+				</Card>
+			</Container>
 		</MainLayout>
 	);
 };
