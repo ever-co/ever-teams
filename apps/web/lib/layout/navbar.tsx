5663fd57f80dc19967fc1f90eed5f220d0e71d85
Badal Khatri <badal.khatri0924@gmail.com>
2023-02-28 11:59:00 +0530

    Added Request to join button, Removed Dropdown to select team, Showing only 1st letter of User

--- apps/web/lib/layout/navbar.tsx
+++ apps/web/lib/layout/navbar.tsx
@@ -1,6 +1,6 @@
 import { IClassName } from '@app/interfaces';
 import { clsxm } from '@app/utils';
-import { Container } from 'lib/components';
+import { Button, Container } from 'lib/components';
 import { AppLogo } from 'lib/components/svgs';
 import { MinTimerFrame, TeamsDropDown, UserNavAvatar } from 'lib/features';
 import Skeleton from 'react-loading-skeleton';
@@ -11,10 +11,25 @@ const HeaderSkeleton = () => {
 		<nav className="bg-white dark:bg-dark-high w-full nav-items--shadow fixed z-[999]">
 			<Container>
 				<div className="w-full flex justify-between items-center min-h-[70px]">
-					<Skeleton height={45} width={200} borderRadius={20} className="dark:bg-[#272930]"/>
+					<Skeleton
+						height={45}
+						width={200}
+						borderRadius={20}
+						className="dark:bg-[#272930]"
+					/>
 					<div className="flex space-x-5 items-center">
-						<Skeleton height={45} width={175} borderRadius={20} className="dark:bg-[#272930]" />
-						<Skeleton circle={true} height={45} width={45} className="dark:bg-[#272930]" />
+						<Skeleton
+							height={45}
+							width={175}
+							borderRadius={20}
+							className="dark:bg-[#272930]"
+						/>
+						<Skeleton
+							circle={true}
+							height={45}
+							width={45}
+							className="dark:bg-[#272930]"
+						/>
 					</div>
 				</div>
 			</Container>
@@ -43,6 +58,11 @@ export function Navbar({
 						<div className="w-full flex justify-between items-center min-h-[70px]">
 							<AppLogo dash className="scale-[0.7] origin-[0]" />
 							<div className="flex space-x-5 items-center">
+								{publicTeam && (
+									<Button className="pr-[2rem] pl-[2rem] rounded-lg">
+										Request to join
+									</Button>
+								)}
 								{showTimer && <MinTimerFrame />}
 								<TeamsDropDown publicTeam={publicTeam || false} />
 								{!publicTeam && <UserNavAvatar />}
