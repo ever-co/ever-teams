f7785ed76052508edfb537b27d086ddc338aea39
zlatina-moga <zlatina.delcheva@gmail.com>
2023-03-15 11:04:50 +0200

    added join request and join team

--- apps/web/components/layout/header/request-to-join-modal.tsx
+++ apps/web/components/layout/header/request-to-join-modal.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { AxiosError } from 'axios';
+import { useAuthenticationPasscode, useTeamInvitations } from '@app/hooks';
 import { useTranslation } from 'lib/i18n';
 import {
 	Button,
@@ -8,6 +9,7 @@ import {
 	Card,
 	InputField,
 	AuthCodeInputField,
+	SpinnerLoader,
 } from 'lib/components';
 import { clsxm } from '@app/utils';
 import { ArrowLeft } from 'lib/components/svgs';
@@ -21,9 +23,20 @@ export const RequestToJoinModal = ({
 }) => {
 	const [member, setMember] = useState<boolean>(true);
 	const [join, setJoin] = useState<boolean>(false);
-	const { register } = useForm();
-	//const [message, setMessage] = useState<string>('');
 	const { trans } = useTranslation();
+	const {
+		loading,
+		formValues,
+		setFormValues,
+		errors,
+		handleChange,
+		handleSubmit,
+		sendCodeLoading,
+		sendAuthCodeHandler,
+		inputCodeRef,
+	} = useAuthenticationPasscode();
+	const { invateUser, inviteLoading } = useTeamInvitations();
+	const [errorsOnJoin, setErrorsOnJoin] = useState({});
 
 	const handleClickMember = () => {
 		member == false && setMember(true);
@@ -35,12 +48,32 @@ export const RequestToJoinModal = ({
 		join == false && setJoin(true);
 	};
 
+	const handleSubmitRequest = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		const form = new FormData(e.currentTarget);
+
+		invateUser(
+			form.get('email')?.toString() || '',
+			form.get('name')?.toString() || ''
+		)
+			.then(() => {
+				closeModal();
+				e.currentTarget.reset();
+			})
+			.catch((err: AxiosError) => {
+				if (err.response?.status === 400) {
+					setErrorsOnJoin((err.response?.data as any)?.errors || {});
+				}
+			});
+	};
+
 	return (
 		<Modal isOpen={open} closeModal={closeModal}>
 			<form
 				className="w-[98%] md:w-[480px]"
 				autoComplete="off"
-				onSubmit={() => ''}
+				onSubmit={member ? handleSubmit : handleSubmitRequest}
 			>
 				<Card className="w-full" shadow="custom">
 					<div className="flex justify-between items-center border-b ">
@@ -71,17 +104,27 @@ export const RequestToJoinModal = ({
 							<div className="w-full mt-8">
 								<InputField
 									type="text"
-									placeholder="Enter your name "
+									placeholder="Enter your name"
+									name="name"
 									className={`md:w-[220px] m-0 h-[54px]`}
+									errors={errorsOnJoin}
+									setErrors={setErrorsOnJoin}
+									required
 								/>
 								<InputField
 									type="email"
 									placeholder="Enter your email address "
 									className={`md:w-[220px] m-0 h-[54px]`}
+									name="email"
+									errors={errorsOnJoin}
+									value={formValues.email}
+									setErrors={setErrorsOnJoin}
+									onChange={handleChange}
+									required
 								/>
 								<InputField
 									type="text"
-									placeholder="Enter your Link address "
+									placeholder="Enter your link"
 									className={`md:w-[220px] m-0 h-[54px]`}
 								/>
 								<div
@@ -91,7 +134,7 @@ export const RequestToJoinModal = ({
 								>
 									<div className="">
 										<select data-te-select-init>
-											<option value="1">Add Posisiton</option>
+											<option value="1">Add Position</option>
 											<option value="2">Position</option>
 											<option value="3">Position</option>
 										</select>
@@ -104,18 +147,13 @@ export const RequestToJoinModal = ({
 								<InputField
 									type="email"
 									placeholder="Yourmail@mail.com"
-									{...register('email', {
-										required: true,
-										pattern:
-											/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-									})}
 									className={`md:w-[220px] m-0 h-[54px]`}
+									name="email"
+									value={formValues.email}
+									onChange={handleChange}
+									errors={errors}
+									required
 								/>
-								{/*message && (
-									<Text.Error className="self-start justify-self-start">
-										{message}
-									</Text.Error>
-								)*/}
 							</div>
 						)}
 						<div className="mt-5">
@@ -127,31 +165,38 @@ export const RequestToJoinModal = ({
 									<AuthCodeInputField
 										allowedCharacters="numeric"
 										length={6}
+										ref={inputCodeRef}
 										containerClassName="mt-[21px] w-full flex justify-between"
 										inputClassName="w-[40px] xs:w-[50px]"
-										onChange={() => ''}
+										defaultValue={formValues.code}
+										onChange={(code) => {
+											setFormValues((v) => ({ ...v, code }));
+										}}
 									/>
-									{/*message && (
+									{errors['code'] && (
 										<Text.Error className="self-start justify-self-start">
-											{message}
+											{errors['code']}
 										</Text.Error>
-									)*/}
+									)}
 								</div>
 								<div className="w-full flex justify-between items-center mt-5">
 									<div className="flex flex-col items-start">
 										<div className="text-xs text-gray-500 dark:text-gray-400 font-normal">
-											{"Didn't recieve code ?"}
-											{!false && (
+											{trans.pages.auth.UNRECEIVED_CODE}
+											{!sendCodeLoading && (
 												<button
 													type="button"
 													className="text-xs text-gray-500 dark:text-gray-400 font-normal"
-													onClick={() => ''}
+													onClick={sendAuthCodeHandler}
 												>
 													{'Re'}
 													<span className="text-primary dark:text-primary-light">
 														{trans.pages.auth.SEND_CODE}
 													</span>
 												</button>
+											)}
+											{sendCodeLoading && (
+												<SpinnerLoader size={22} className="self-center" />
 											)}
 										</div>
 									</div>
@@ -169,7 +214,8 @@ export const RequestToJoinModal = ({
 										className={
 											'font-medium border border-primary disabled:border-0 md:min-w-[180px] rounded-xl'
 										}
-										onClick={() => ''}
+										loading={member ? loading : inviteLoading}
+										disabled={member ? loading : inviteLoading}
 									>
 										{join ? trans.common.JOIN_REQUEST : trans.pages.auth.JOIN}
 									</Button>
