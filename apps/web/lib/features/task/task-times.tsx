7559b29a96c655c5002f435973edcfe80a592ddd
Badal Khatri <badal.khatri0924@gmail.com>
2023-03-31 14:19:33 +0530

    fix: #674 Updated Team Page live refresh of tasks

--- apps/web/lib/features/task/task-times.tsx
+++ apps/web/lib/features/task/task-times.tsx
@@ -30,54 +30,34 @@ export function TaskTimes({
 	showDaily = true,
 	showTotal = true,
 }: Props) {
-	// Get current timer seconds
-	const seconds = useRecoilValue(timerSecondsState);
-	const { activeTaskDailyStat, activeTaskTotalStat, getTaskStat, addSeconds } =
-		useTaskStatistics(seconds);
-
 	// For public page
-	const { teams } = useOrganizationTeams();
-	const currentMember = teams[0]?.members.find(
+	const { activeTeam } = useOrganizationTeams();
+	const currentMember = activeTeam?.members.find(
 		(member) => member.id === memberInfo?.member?.id
 	);
 
-	/**
-	 * If showing the the current auth auth then show live update
-	 */
-	if (isAuthUser && activeAuthTask) {
-		const { h, m } = secondsToTime(
-			(activeTaskTotalStat?.duration || 0) + addSeconds
-		);
-		const { h: dh, m: dm } = secondsToTime(
-			(activeTaskDailyStat?.duration || 0) + addSeconds
-		);
-
-		return (
-			<div className={clsxm(className)}>
-				<TimeInfo
-					showDaily={showDaily}
-					showTotal={showTotal}
-					daily={{ h: dh, m: dm }}
-					total={{ h, m }}
-				/>
-			</div>
-		);
-	}
-
-	/** Other member team status */
-	const { taskDailyStat, taskTotalStat } = getTaskStat(task);
 	const { h, m } = secondsToTime(
-		taskTotalStat?.duration ||
-			(currentMember?.totalTodayTasks &&
-				currentMember?.totalTodayTasks?.length &&
-				currentMember?.totalTodayTasks[0]?.duration) ||
+		(currentMember?.totalWorkedTasks &&
+			currentMember?.totalWorkedTasks?.length &&
+			currentMember?.totalWorkedTasks
+				.filter((t) => t.id === task?.id)
+				.reduce(
+					(previousValue, currentValue) =>
+						previousValue + currentValue.duration,
+					0
+				)) ||
 			0
 	);
 	const { h: dh, m: dm } = secondsToTime(
-		taskDailyStat?.duration ||
-			(currentMember?.totalWorkedTasks &&
-				currentMember?.totalWorkedTasks.length &&
-				currentMember?.totalWorkedTasks[0]?.duration) ||
+		(currentMember?.totalTodayTasks &&
+			currentMember?.totalTodayTasks.length &&
+			currentMember?.totalTodayTasks
+				.filter((t) => t.id === task?.id)
+				.reduce(
+					(previousValue, currentValue) =>
+						previousValue + currentValue.duration,
+					0
+				)) ||
 			0
 	);
 
@@ -112,7 +92,7 @@ function TimeInfo({
 					<span className="text-gray-500 lg:text-sm text-xs">
 						{trans.common.TODAY}:
 					</span>
-					<Text className='lg:text-sm text-xs'>
+					<Text className="lg:text-sm text-xs">
 						{daily.h}h : {daily.m}m
 					</Text>
 				</div>
@@ -128,7 +108,7 @@ function TimeInfo({
 					<span className="text-gray-500 lg:text-sm text-xs">
 						{trans.common.TOTAL}:
 					</span>
-					<Text className='lg:text-sm text-xs'>
+					<Text className="lg:text-sm text-xs">
 						{total.h}h : {total.m}m
 					</Text>
 				</div>
@@ -144,15 +124,17 @@ export function TodayWorkedTime({
 }: Omit<Props, 'task' | 'activeAuthTask'>) {
 	// Get current timer seconds
 	const { time } = useLiveTimerStatus();
-	const { teams } = useOrganizationTeams();
+	const { activeTeam } = useOrganizationTeams();
 
-	// For public page
-	const currentMember = teams[0].members.find(
+	const currentMember = activeTeam?.members.find(
 		(member) => member.id === memberInfo?.member?.id
 	);
 	const { h, m } = secondsToTime(
 		(currentMember?.totalTodayTasks &&
-			currentMember?.totalTodayTasks[0]?.duration) ||
+			currentMember?.totalTodayTasks.reduce(
+				(previousValue, currentValue) => previousValue + currentValue.duration,
+				0
+			)) ||
 			0
 	);
 
