7b0e4424a79852c677d161b62fd575f5d4bc4a41
Ruslan K <evereq@gmail.com>
2023-04-02 14:32:23 +0200

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -9,12 +9,13 @@ const nextConfig = {
 			'localhost',
 			'127.0.0.1',
 			'cdn-icons-png.flaticon.com', // Remove this domain once Backend Icons list are added
+			'api.gauzy.co',
 			'apistage.gauzy.co',
-			'gauzy.s3.wasabisys.com',
+			'gauzy.s3.wasabisys.com'
 		],
 	},
 	experimental: {
-		appDir: true,
+		appDir: true
 	},
 };
 
