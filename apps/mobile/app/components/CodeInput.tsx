9b5efce0ba786cbc8c1ca15b6f84bb7d011697b0
Serge Muhundu <sergemuhundu2@gmail.com>
2023-05-10 22:01:51 +0100

    update login flow

--- apps/mobile/app/components/CodeInput.tsx
+++ apps/mobile/app/components/CodeInput.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react-native/no-color-literals */
 import React, { FC, useRef, useState } from "react"
 import {
 	TextInput,
@@ -11,13 +13,19 @@ import { colors, typography } from "../theme"
 interface IInput {
 	onChange: (code: string) => void
 	editable: boolean
+	length?: number
+	defaultValue?: string
 }
 
-export const CodeInput: FC<IInput> = ({ onChange, editable }) => {
+export const CodeInput: FC<IInput> = (props) => {
+	const { onChange, editable, length = 6, defaultValue } = props
+
 	const inputsRef = useRef<TextInput[] | null[]>([])
 	const [active, setActive] = useState<number>(0)
 	const [inviteCode, setInviteCode] = useState([])
-	const inputs = [0, 1, 2, 3, 4, 5]
+	const inputs = []
+
+	const validDefaultValue = defaultValue && defaultValue.length === length
 
 	const onKeyPress = ({ nativeEvent }: NativeSyntheticEvent<TextInputKeyPressEventData>) => {
 		if (nativeEvent.key === "Backspace") {
@@ -26,42 +34,49 @@ export const CodeInput: FC<IInput> = ({ onChange, editable }) => {
 				return setActive(active - 1)
 			}
 		} else {
-			inputsRef.current[active + 1]?.focus()
-			return setActive(active + 1)
+			if (!isNaN(nativeEvent.key)) {
+				inputsRef.current[active + 1]?.focus()
+				return setActive(active + 1)
+			}
 		}
 		return null
 	}
 
 	const onChangeCode = (inputCode, inputIndex) => {
-		const codes = inviteCode
-		codes[inputIndex] = inputCode
-		setInviteCode(codes)
-		onChange(codes.join(""))
+		if (!isNaN(inputCode)) {
+			const codes = inviteCode
+			codes[inputIndex] = inputCode
+			setInviteCode(codes)
+			onChange(codes.join(""))
+		}
 	}
 
-	return (
-		<View style={styles.container}>
-			{inputs.map((i) => (
-				<TextInput
-					key={i}
-					maxLength={1}
-					keyboardType="numeric"
-					style={[
-						styles.inputStyle,
-						editable ? { borderColor: active === i ? colors.primary : "rgba(0, 0, 0, 0.1)" } : null,
-					]}
-					onKeyPress={onKeyPress}
-					autoFocus={active === i}
-					editable={editable}
-					ref={(r) => {
-						inputsRef.current[i] = r
-					}}
-					onChangeText={(num) => onChangeCode(num, i)}
-				/>
-			))}
-		</View>
-	)
+	for (let i = 0; i < length; i++) {
+		const dvalue = validDefaultValue ? defaultValue?.charAt(i) : inviteCode[i]
+		inputs.push(
+			<TextInput
+				key={i}
+				maxLength={1}
+				value={dvalue}
+				defaultValue={dvalue}
+				keyboardType="numeric"
+				style={[
+					styles.inputStyle,
+					editable ? { borderColor: active === i ? colors.primary : "rgba(0, 0, 0, 0.1)" } : null,
+				]}
+				onKeyPress={onKeyPress}
+				autoFocus={active === i}
+				editable={editable}
+				ref={(r) => {
+					inputsRef.current[i] = r
+				}}
+				onChangeText={(num) => onChangeCode(num, i)}
+			/>,
+		)
+	}
+	return <View style={styles.container}>{inputs}</View>
 }
+
 type InputProps = {
 	onKeyPress: () => unknown
 	onFocus: () => unknown
@@ -69,21 +84,6 @@ type InputProps = {
 	inputsRef: any
 	index: number
 }
-const Input: FC<InputProps> = ({ onKeyPress, active, inputsRef, index }) => (
-	<View style={styles.container}>
-		<TextInput
-			maxLength={1}
-			keyboardType="numeric"
-			style={styles.inputStyle}
-			onKeyPress={onKeyPress}
-			onFocus={onKeyPress}
-			autoFocus={active === index}
-			ref={(r) => {
-				inputsRef.current[index] = r
-			}}
-		/>
-	</View>
-)
 
 const styles = StyleSheet.create({
 	container: {
