11dbc3a20e16a09fa44ce1851083f7e4d7f72e0b
Badal Khatri <badal.khatri0924@gmail.com>
2023-05-14 09:58:12 +0530

    fix:#734 Worked tab tasks filtered as per current user

--- apps/web/app/hooks/features/useAuthTeamTasks.ts
+++ apps/web/app/hooks/features/useAuthTeamTasks.ts
@@ -2,10 +2,16 @@ import { IUser } from '@app/interfaces';
 import { tasksByTeamState, tasksStatisticsState } from '@app/stores';
 import { useMemo } from 'react';
 import { useRecoilValue } from 'recoil';
+import { useOrganizationTeams } from './useOrganizationTeams';
 
 export function useAuthTeamTasks(user: IUser | undefined) {
 	const tasks = useRecoilValue(tasksByTeamState);
 	const statTasks = useRecoilValue(tasksStatisticsState);
+
+	const { activeTeam } = useOrganizationTeams();
+	const currentMember = activeTeam?.members.find(
+		(member) => member.employee.userId === user?.id
+	);
 
 	const assignedTasks = useMemo(() => {
 		if (!user) return [];
@@ -21,9 +27,13 @@ export function useAuthTeamTasks(user: IUser | undefined) {
 		});
 	}, [tasks, user]);
 
+	const totalWorkedTasks =
+		currentMember?.totalWorkedTasks && currentMember?.totalWorkedTasks.length
+			? currentMember?.totalWorkedTasks.map((task) => task.id)
+			: [];
 	const workedTasks = useMemo(() => {
 		return tasks.filter((tsk) => {
-			return statTasks.today.some((st) => st.id === tsk.id);
+			return totalWorkedTasks.includes(tsk.id);
 		});
 	}, [statTasks, tasks]);
 
