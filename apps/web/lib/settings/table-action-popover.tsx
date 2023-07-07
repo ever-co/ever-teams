a6bf60b73b534dab3da854594951d7db40c81352
Badal Khatri <badal.khatri0924@gmail.com>
2023-07-07 12:34:25 +0530

    Updated Role & Permission UI

--- apps/web/lib/settings/table-action-popover.tsx
+++ apps/web/lib/settings/table-action-popover.tsx
@@ -1,6 +1,6 @@
 import { Popover, Transition } from '@headlessui/react';
 import { MenuIcon } from 'lib/components/svgs';
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import React from 'react';
 import { useDropdownAction } from 'lib/features/team/user-team-card/user-team-card-menu';
 import {
@@ -12,6 +12,7 @@ import {
 import { OT_Member } from '@app/interfaces';
 import { ConfirmationModal } from './confirmation-modal';
 import { useTranslation } from 'lib/i18n';
+import { useRoles } from '@app/hooks/features/useRoles';
 
 type Props = {
 	member: OT_Member;
@@ -122,48 +123,44 @@ export const TableActionPopover = ({ member, handleEdit }: Props) => {
 	);
 };
 
-const RolePopover = () => (
-	<Popover className="relative border-none no-underline w-full">
-		<Transition
-			as={Fragment}
-			enter="transition ease-out duration-200"
-			enterFrom="opacity-0 translate-y-1"
-			enterTo="opacity-100 translate-y-0"
-			leave="transition ease-in duration-150"
-			leaveFrom="opacity-100 translate-y-0"
-			leaveTo="opacity-0 translate-y-1"
-		>
-			<Popover.Panel
-				className="z-10 absolute right-0 bg-white dark:bg-[#202023] rounded-2xl w-[9.5rem] flex flex-col pl-5 pr-5 pt-2 pb-2 mt-10 mr-10"
-				style={{ boxShadow: ' rgba(0, 0, 0, 0.12) -24px 17px 49px' }}
+const RolePopover = () => {
+	const { getRoles, roles } = useRoles();
+	useEffect(() => {
+		getRoles();
+	}, [getRoles]);
+
+	return (
+		<Popover className="relative border-none no-underline w-full">
+			<Transition
+				as={Fragment}
+				enter="transition ease-out duration-200"
+				enterFrom="opacity-0 translate-y-1"
+				enterTo="opacity-100 translate-y-0"
+				leave="transition ease-in duration-150"
+				leaveFrom="opacity-100 translate-y-0"
+				leaveTo="opacity-0 translate-y-1"
 			>
-				<div className="flex items-center h-8 w-auto  hover:cursor-pointer">
-					<span className="text-[#282048] text-xs font-semibold dark:text-white">
-						Manager (admin)
-					</span>
-				</div>
-				<div className="flex items-center h-8 w-auto  hover:cursor-pointer">
-					<span className="text-[#282048] text-xs font-semibold dark:text-white">
-						Manager
-					</span>
-				</div>
-				<div className="flex items-center h-8 w-auto  hover:cursor-pointer">
-					<span className="text-[#282048] text-xs font-semibold dark:text-white">
-						Viewer
-					</span>
-				</div>
-				<div className="flex items-center h-8 w-auto  hover:cursor-pointer">
-					<span className="text-[#282048] text-xs font-semibold dark:text-white">
-						Member
-					</span>
-				</div>
-			</Popover.Panel>
-		</Transition>
-		{/* TODO */}
-		{/* <Popover.Button className="flex items-center h-8 w-auto hover:cursor-pointer outline-none">
-			<span className="text-[#282048] text-xs font-semibold dark:text-white">
-				Change Role
-			</span>
-		</Popover.Button> */}
-	</Popover>
-);
+				<Popover.Panel
+					className="z-10 absolute right-0 bg-white dark:bg-[#202023] rounded-2xl w-[9.5rem] flex flex-col pl-5 pr-5 pt-2 pb-2 mt-10 mr-10"
+					style={{ boxShadow: ' rgba(0, 0, 0, 0.12) -24px 17px 49px' }}
+				>
+					{roles.map((role) => (
+						<div
+							className="flex items-center h-8 w-auto  hover:cursor-pointer"
+							key={role.id}
+						>
+							<span className="text-[#282048] text-xs font-semibold dark:text-white">
+								{role.name}
+							</span>
+						</div>
+					))}
+				</Popover.Panel>
+			</Transition>
+			{/* <Popover.Button className="flex items-center h-8 w-auto hover:cursor-pointer outline-none">
+				<span className="text-[#282048] text-xs font-semibold dark:text-white">
+					Change Role
+				</span>
+			</Popover.Button> */}
+		</Popover>
+	);
+};
