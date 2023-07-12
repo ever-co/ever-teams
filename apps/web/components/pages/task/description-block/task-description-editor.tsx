ea0c58d27989979993397635620c1c193b866d2f
desperado1802 <mumun_ali@hotmail.com>
2023-07-12 17:52:43 +0300

    added the functionality of copy/paste by keeping the same format

--- apps/web/components/pages/task/description-block/task-description-editor.tsx
+++ apps/web/components/pages/task/description-block/task-description-editor.tsx
@@ -1,5 +1,8 @@
 import Toolbar from './editor-toolbar';
-import { TextEditorService } from './editor-components/TextEditorService';
+import {
+	TextEditorService,
+	withHtml,
+} from './editor-components/TextEditorService';
 import isHotkey from 'is-hotkey';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { Editor, createEditor, Element as SlateElement } from 'slate';
@@ -27,7 +30,10 @@ interface IRichTextProps {
 const RichTextEditor = ({ readonly }: IRichTextProps) => {
 	const renderElement = useCallback((props: any) => <Element {...props} />, []);
 	const renderLeaf = useCallback((props: any) => <Leaf {...props} />, []);
-	const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+	const editor = useMemo(
+		() => withHtml(withHistory(withReact(createEditor()))),
+		[]
+	);
 	const [task] = useRecoilState(detailedTaskState);
 	const [isUpdated, setIsUpdated] = useState<boolean>(false);
 	const [key, setKey] = useState(0); // Add key state, we need it as it re-renders the editor
@@ -134,6 +140,12 @@ const Element = ({ attributes, children, element }: any) => {
 					{children}
 				</blockquote>
 			);
+		case 'code':
+			return (
+				<pre>
+					<code {...attributes}>{children}</code>
+				</pre>
+			);
 		case 'ul':
 			return (
 				<ul style={style} {...attributes}>
@@ -190,7 +202,111 @@ const Leaf = ({ attributes, children, leaf }: any) => {
 		children = <u>{children}</u>;
 	}
 
-	return <p {...attributes}>{children}</p>;
+	return <span {...attributes}>{children}</span>;
 };
 
 export default RichTextEditor;
+
+// interface ElementAttributes {
+// 	[key: string]: string | null;
+// }
+
+// const ELEMENT_TAGS: { [key: string]: (el: HTMLElement) => ElementAttributes } =
+// 	{
+// 		A: (el) => ({ type: 'link', url: el.getAttribute('href') }),
+// 		BLOCKQUOTE: () => ({ type: 'quote' }),
+// 		H1: () => ({ type: 'h1' }),
+// 		H2: () => ({ type: 'h2' }),
+// 		H3: () => ({ type: 'heading-three' }),
+// 		H4: () => ({ type: 'heading-four' }),
+// 		H5: () => ({ type: 'heading-five' }),
+// 		H6: () => ({ type: 'heading-six' }),
+// 		IMG: (el) => ({ type: 'image', url: el.getAttribute('src') }),
+// 		LI: () => ({ type: 'li' }),
+// 		OL: () => ({ type: 'ol' }),
+// 		P: () => ({ type: 'paragraph' }),
+// 		PRE: () => ({ type: 'code' }),
+// 		UL: () => ({ type: 'bulleted-list' }),
+// 	};
+
+// // COMPAT: `B` is omitted here because Google Docs uses `<b>` in weird ways.
+// const TEXT_TAGS: { [key: string]: () => { [key: string]: boolean } } = {
+// 	CODE: () => ({ code: true }),
+// 	DEL: () => ({ strikethrough: true }),
+// 	EM: () => ({ italic: true }),
+// 	I: () => ({ italic: true }),
+// 	S: () => ({ strikethrough: true }),
+// 	STRONG: () => ({ bold: true }),
+// 	U: () => ({ underline: true }),
+// };
+
+// export const deserialize = (el: Node): any => {
+// 	if (el.nodeType === 3) {
+// 		return el.textContent;
+// 	} else if (el.nodeType !== 1) {
+// 		return null;
+// 	} else if (el.nodeName === 'BR') {
+// 		return '\n';
+// 	}
+
+// 	const { nodeName } = el;
+// 	let parent = el;
+
+// 	if (
+// 		nodeName === 'PRE' &&
+// 		el.childNodes[0] &&
+// 		el.childNodes[0].nodeName === 'CODE'
+// 	) {
+// 		parent = el.childNodes[0];
+// 	}
+// 	let children = Array.from(parent.childNodes).map(deserialize).flat();
+
+// 	if (children.length === 0) {
+// 		children = [{ text: '' }];
+// 	}
+
+// 	if (el.nodeName === 'BODY') {
+// 		return jsx('fragment', {}, children);
+// 	}
+
+// 	if (ELEMENT_TAGS[nodeName]) {
+// 		//@ts-ignore
+// 		const attrs = ELEMENT_TAGS[nodeName](el);
+// 		return jsx('element', attrs, children);
+// 	}
+
+// 	if (TEXT_TAGS[nodeName]) {
+// 		//@ts-ignore
+// 		const attrs = TEXT_TAGS[nodeName](el);
+// 		return children.map((child) => jsx('text', attrs, child));
+// 	}
+
+// 	return children;
+// };
+
+// const withHtml = (editor: ReactEditor): ReactEditor => {
+// 	const { insertData, isInline, isVoid } = editor;
+
+// 	editor.isInline = (element: any) => {
+// 		return element.type === 'link' ? true : isInline(element);
+// 	};
+
+// 	editor.isVoid = (element: any) => {
+// 		return element.type === 'image' ? true : isVoid(element);
+// 	};
+
+// 	editor.insertData = (data) => {
+// 		const html = data.getData('text/html');
+
+// 		if (html) {
+// 			const parsed = new DOMParser().parseFromString(html, 'text/html');
+// 			const fragment = deserialize(parsed.body);
+// 			Transforms.insertFragment(editor, fragment);
+// 			return;
+// 		}
+
+// 		insertData(data);
+// 	};
+
+// 	return editor;
+// };
