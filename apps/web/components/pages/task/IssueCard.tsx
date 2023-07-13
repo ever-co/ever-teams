95c596cd72babcf9d99cee5e601074b5e40ba908
paradoxe35 <paradoxngwasi@gmail.com>
2023-07-13 18:21:55 +0000

    linked task creation modal  - task page

--- apps/web/components/pages/task/IssueCard.tsx
+++ apps/web/components/pages/task/IssueCard.tsx
@@ -1,6 +1,7 @@
 // import ToolButton from '@components/pages/task/description-block/tool-button';
 import { ChevronUpIcon } from '@heroicons/react/20/solid';
-import { Card } from 'lib/components';
+import { Card, Modal, Text } from 'lib/components';
+
 // import Image from 'next/image';
 import { PlusIcon } from '@heroicons/react/20/solid';
 // import bugIcon from '../../../public/assets/svg/bug.svg';
@@ -8,10 +9,13 @@ import { PlusIcon } from '@heroicons/react/20/solid';
 import { TaskIssueStatus, TaskStatusDropdown } from 'lib/features';
 import { useRecoilValue } from 'recoil';
 import { detailedTaskState } from '@app/stores';
-import { useLinkedTasks } from '@app/hooks';
+import { IHookModal, useLinkedTasks, useModal } from '@app/hooks';
 import { ITeamTask } from '@app/interfaces';
+import { useTranslation } from 'lib/i18n';
 
 const IssueCard = ({ related }: { related: boolean }) => {
+	const modal = useModal();
+
 	const task = useRecoilValue(detailedTaskState);
 	const { tasks } = useLinkedTasks(task);
 
@@ -28,7 +32,11 @@ const IssueCard = ({ related }: { related: boolean }) => {
 					{/* <ToolButton iconSource="/assets/svg/add.svg" />
 					<ToolButton iconSource="/assets/svg/more.svg" /> */}
 
-					<PlusIcon className="h-5 w-5 text-[#292D32] dark:text-white cursor-pointer" />
+					<PlusIcon
+						onClick={modal.openModal}
+						className="h-5 w-5 text-[#292D32] dark:text-white cursor-pointer"
+					/>
+
 					<ChevronUpIcon className="h-5 w-5 text-[#292D32] dark:text-white cursor-pointer" />
 				</div>
 			</div>
@@ -39,9 +47,29 @@ const IssueCard = ({ related }: { related: boolean }) => {
 					return <RelatedTask key={task.id} task={task} />;
 				})}
 			</div>
+
+			<CreateTask modal={modal} />
 		</Card>
 	);
 };
+
+function CreateTask({ modal }: { modal: IHookModal }) {
+	const { trans } = useTranslation();
+
+	return (
+		<Modal isOpen={modal.isOpen} closeModal={modal.closeModal}>
+			<div className="w-[98%] md:w-[530px]">
+				<Card className="w-full" shadow="custom">
+					<div className="flex flex-col justify-between items-center w-full">
+						<Text.Heading as="h3" className="text-center mb-2">
+							{trans.common.CREATE_TASK}
+						</Text.Heading>
+					</div>
+				</Card>
+			</div>
+		</Modal>
+	);
+}
 
 function RelatedTask({ task }: { task: ITeamTask }) {
 	return (
