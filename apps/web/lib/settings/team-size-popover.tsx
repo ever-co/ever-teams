aaea7fd4a48b02a2ef4a8e9d662494de448fc5fc
desperado1802 <124465103+desperado1802@users.noreply.github.com>
2023-07-25 12:33:32 +0300

    Bug/team settings disable (#1065)
    * made the team name and team color field to be disabled and be enabled when pressed the edit icon
    
    * added the disable functionality to emoji picker and team size selector
    
    * removed the unused state and function
    
    * added types
    
    * added functionality to close and disable dialogs when pressed outside of modal, but in the input field, excluding team name input
    
    * added tick icon when team name is in edit mode to indicate that it can be saved from the icon

--- apps/web/lib/settings/team-size-popover.tsx
+++ apps/web/lib/settings/team-size-popover.tsx
@@ -30,23 +30,51 @@ const TeamSize = ({
 	isTeamManager: boolean;
 }) => {
 	const [value, setValue] = useState(defaultValue || 'Only me');
-	const buttonRef = useRef<any>();
+	const buttonRef = useRef<HTMLButtonElement>(null);
+	const panelRef = useRef<HTMLDivElement>(null);
+	const [disabled, setDisabled] = useState<boolean>(true);
+
 	const onSelect = (value: any) => {
 		setValue(value);
 	};
-	const Close = () => {
-		setValue('');
-		buttonRef.current?.click();
-	};
+	// const Close = () => {
+	// 	setValue('');
+	// 	buttonRef.current?.click();
+	// };
 
 	const handleSave = useCallback(() => {
 		onChange(value);
-		buttonRef.current?.click();
+		setDisabled(true);
 	}, [value, onChange]);
 
 	useEffect(() => {
 		setValue(defaultValue);
 	}, [defaultValue]);
+
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				panelRef.current &&
+				!panelRef.current.contains(event.target as Node)
+			) {
+				setDisabled(true);
+			}
+		};
+
+		const handleKeyPress = (event: KeyboardEvent) => {
+			if (event.key === 'Escape') {
+				setDisabled(true);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+		document.addEventListener('keydown', handleKeyPress);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+			document.removeEventListener('keydown', handleKeyPress);
+		};
+	}, []);
 
 	return (
 		<Popover className="relative border-none no-underline w-full">
@@ -55,21 +83,28 @@ const TeamSize = ({
 					<Popover.Button
 						className="outline-none mb-[15px] w-full"
 						ref={buttonRef}
-						disabled={!isTeamManager}
+						disabled={disabled}
+						onClick={() => setDisabled(true)}
 					>
 						<div
-							className={`${
-								isTeamManager ? 'cursor-pointer ' : ''
-							} relative w-[100%] h-[48px] ${
-								!isTeamManager ? 'bg-[#FCFCFC]' : ''
+							className={`relative w-[100%] h-[48px] ${
+								disabled ? 'bg-[#FCFCFC]' : ''
 							} bg-light--theme-light dark:bg-dark--theme-light border rounded-[10px] flex items-center justify-between input-border`}
 						>
 							<div className="flex gap-[8px] h-[40px] items-center pl-[15px]">
 								<div className="text-[16px] font-medium">{value}</div>
 							</div>
-							<div className="flex mr-[0.5rem] gap-3">
+							<button
+								className={`flex mr-[0.5rem] gap-3 outline-none ${
+									!isTeamManager && 'pointer-events-none'
+								}`}
+								disabled={!isTeamManager}
+								onClick={() => {
+									setDisabled(!disabled);
+								}}
+							>
 								<Edit2Icon />
-							</div>
+							</button>
 						</div>
 					</Popover.Button>
 					<Transition
@@ -80,8 +115,12 @@ const TeamSize = ({
 						leave="transition ease-in duration-150"
 						leaveFrom="opacity-100 translate-y-0"
 						leaveTo="opacity-0 translate-y-1"
+						show={!disabled}
 					>
-						<Popover.Panel className="absolute left-1/2 z-10 mt-0 w-[354px] max-w-sm -translate-x-1/2 transform  sm:px-0 lg:max-w-3xl shandow ">
+						<Popover.Panel
+							ref={panelRef}
+							className="absolute left-1/2 z-10 mt-0 w-[354px] max-w-sm -translate-x-1/2 transform  sm:px-0 lg:max-w-3xl shandow "
+						>
 							<div className="bg-white shadow dark:bg-[#202023] rounded-[10px] text-[14px] font-light p-[16px]">
 								<div className="text-[18px] text-[#7E7991] font-[500]">
 									Select Team Size
@@ -89,7 +128,7 @@ const TeamSize = ({
 								<div className="bg-[#EDEEF2] h-[1px] w-full mt-[10px]"></div>
 								<div className="mt-[10px]">
 									<div className="flex flex-col gap-y-[15px] hover:cursor-pointer">
-										{sizeOption?.map((size, index) => {
+										{sizeOption.map((size, index) => {
 											return (
 												<div
 													key={index}
@@ -113,7 +152,7 @@ const TeamSize = ({
 											className="font-normal rounded-xl text-md min-w-[90px] bg-[#E6E6E9] text-[#1A1C1E]"
 											type="submit"
 											style={{ background: '#E6E6E9' }}
-											onClick={Close}
+											onClick={() => setDisabled(true)}
 										>
 											Cancel
 										</Button>
