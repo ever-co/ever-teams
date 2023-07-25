0f6f4a40ff48a5f99f8d8d8282bb5052a66f3fb5
paradoxe35 <paradoxngwasi@gmail.com>
2023-07-25 13:12:07 +0000

    fixed: Make child issues clickable, and redirect it to actual issue/task page #1057

--- apps/web/lib/features/task/task-linked-issue.tsx
+++ apps/web/lib/features/task/task-linked-issue.tsx
@@ -1,14 +1,15 @@
 import { ITeamTask } from '@app/interfaces';
 import { Card } from 'lib/components';
+import Link from 'next/link';
 import { TaskNameInfoDisplay } from './task-displays';
 import { ActiveTaskStatusDropdown } from './task-status';
 
 export function TaskLinkedIssue({ task }: { task: ITeamTask }) {
 	return (
 		<Card shadow="custom" className="flex justify-between py-3 px-0 md:px-0">
-			<div className="flex space-x-1">
+			<Link href={`/task/${task.id}`} className="flex space-x-1">
 				<TaskNameInfoDisplay task={task} />
-			</div>
+			</Link>
 
 			<ActiveTaskStatusDropdown task={task} defaultValue={task.status} />
 		</Card>
