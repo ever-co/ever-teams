5f519b91c871b3d9e7af36372be8608bc53710fe
paradoxe35 <paradoxngwasi@gmail.com>
2023-07-30 01:27:25 +0000

    fixed: Incorrect Alignment/Spacing in Member Task page #1094

--- apps/web/pages/profile/[memberId].tsx
+++ apps/web/pages/profile/[memberId].tsx
@@ -37,7 +37,7 @@ const Profile = () => {
 	return (
 		<>
 			<MainLayout showTimer={!profileIsAuthUser && isTrackingEnabled}>
-				<MainHeader className={clsxm(hookFilterType && ['pb-0'])}>
+				<MainHeader className={clsxm(hookFilterType && ['pb-0'], 'pb-2')}>
 					{/* Breadcrumb */}
 					<div className="flex items-center space-x-3">
 						<Link href="/">
