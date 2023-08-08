717dc4b316f10153b3eba3f77d18bc37de21fd50
desperado1802 <mumun_ali@hotmail.com>
2023-08-08 10:44:20 +0300

    added the circle icon to the dropdowns when they have no value

--- apps/web/lib/components/svgs/icons.tsx
+++ apps/web/lib/components/svgs/icons.tsx
@@ -1005,7 +1005,7 @@ export function CircleIcon({ className }: IClassName) {
 			viewBox="0 0 17 18"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
-			className={clsxm('stroke-[#292D32]', className)}
+			className={clsxm('stroke-[#292D32] dark:stroke-white', className)}
 		>
 			<path
 				d="M8.5 16.5C12.625 16.5 16 13.125 16 9C16 4.875 12.625 1.5 8.5 1.5C4.375 1.5 1 4.875 1 9C1 13.125 4.375 16.5 8.5 16.5Z"
