952ce7cbe3530beded186e13e7fc085ef872fe4a
desperado1802 <mumun_ali@hotmail.com>
2023-08-11 18:00:55 +0300

    fixed margin

--- apps/web/components/pages/task/details-section/blocks/task-estimations-info.tsx
+++ apps/web/components/pages/task/details-section/blocks/task-estimations-info.tsx
@@ -23,7 +23,7 @@ const TaskEstimationsInfo = () => {
 			>
 				<Disclosure>
 					{({ open }) => (
-						<div className="flex flex-col w-full mt-[0.1875rem]">
+						<div className="flex flex-col w-full mt-[0.125rem]">
 							<Disclosure.Button className="flex justify-between items-center w-full">
 								<TaskEstimate
 									_task={task}
