c9f7ddd6a98ac53f518fd16d8492fea95b430b2a
Badal Khatri <81486442+badalkhatri0924@users.noreply.github.com>
2023-08-12 12:22:02 +0530

    Removed unused code (#1230)

--- apps/web/components/pages/task/details-section/components/profile-info-with-time.tsx
+++ apps/web/components/pages/task/details-section/components/profile-info-with-time.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import ProfileInfo from './profile-info';
 
 type Props = {
@@ -5,6 +6,7 @@ type Props = {
 	names?: string;
 	profileInfoWrapperClassName?: string;
 	time: string;
+	userId: string;
 };
 
 const ProfileInfoWithTime = ({
@@ -12,14 +14,17 @@ const ProfileInfoWithTime = ({
 	names,
 	profileInfoWrapperClassName,
 	time,
+	userId,
 }: Props) => {
 	return (
 		<div className="flex justify-between items-center w-full">
-			<ProfileInfo
-				profilePicSrc={profilePicSrc}
-				names={names?.slice(0, 10) + '...'}
-				wrapperClassName={profileInfoWrapperClassName}
-			/>
+			<Link href={`/profile/${userId}`} target="_blank">
+				<ProfileInfo
+					profilePicSrc={profilePicSrc}
+					names={names?.slice(0, 10) + '...'}
+					wrapperClassName={profileInfoWrapperClassName}
+				/>
+			</Link>
 			<div className="not-italic font-medium text-[0.75rem] leading-[140%] tracking-[-0.02em] text-[#282048] dark:text-white">
 				{time}
 			</div>
