a6cdb0a29ba9c97b207010448541f3d9b335c3d0
Badal Khatri <badal.khatri0924@gmail.com>
2023-08-14 15:16:06 +0530

    Updated Manager icon in user card

--- apps/web/lib/components/svgs/icons.tsx
+++ apps/web/lib/components/svgs/icons.tsx
@@ -2576,3 +2576,23 @@ export function ChevronDownIcon({ className }: IClassName) {
 		</svg>
 	);
 }
+
+export function UserManagerIcon({ className }: IClassName) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			className={className}
+		>
+			<path
+				fill="none"
+				stroke="currentColor"
+				strokeWidth="2"
+				d="M16 12c2.374 1.183 4 3.65 4 7v4H4v-4c0-3.354 1.631-5.825 4-7m4 1a6 6 0 1 0 0-12a6 6 0 0 0 0 12Zm6-6c-1.5 0-3 .36-5-2c-2 2.36-4.5 3-7 2m1 6l5.025 5.257L17 13m-5 5v5"
+				className={className}
+			/>
+		</svg>
+	);
+}
