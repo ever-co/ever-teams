5671a233232d310911205205b07e127fd79dd4f7
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2023-08-22 20:11:56 +0200

    Feat/Action type dropdown (#1280)
    * fixed: fixed width dropdown on related task card
    
    * fixed: react unique key props warning
    
    * move action from card to item card
    
    * fixed: tiny dropdown ui bugs
    
    * feat: update related task issue API implementation

--- apps/web/pages/api/tasks/task-linked-issue.ts
+++ apps/web/pages/api/tasks/task-linked-issue.ts
@@ -1,6 +1,9 @@
-import { ITaskLinkedIssue } from '@app/interfaces/ITask';
+import { ITaskLinkedIssue, LinkedTaskIssue } from '@app/interfaces/ITask';
 import { authenticatedGuard } from '@app/services/server/guards/authenticated-guard';
-import { createTaskLinkedIsssue } from '@app/services/server/requests';
+import {
+	createTaskLinkedIsssue,
+	updateTaskLinkedIssue,
+} from '@app/services/server/requests';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
@@ -13,12 +16,19 @@ export default async function handler(
 	);
 	if (!user) return $res();
 
-	if (req.method !== 'POST') {
+	if (!['POST', 'PUT'].includes(req.method as string)) {
 		return res.status(405).json({ status: 'failed' });
 	}
 
-	const body = req.body as ITaskLinkedIssue;
+	if (req.method === 'PUT') {
+		const body = req.body as LinkedTaskIssue;
+		const response = await updateTaskLinkedIssue(body, access_token, tenantId);
 
+		$res.json(response.data);
+		return;
+	}
+
+	const body = req.body as ITaskLinkedIssue;
 	const response = await createTaskLinkedIsssue(body, access_token, tenantId);
 
 	$res.json(response.data);
