f41baf9f62958d188af3a683fe5fea6c03429266
desperado1802 <mumun_ali@hotmail.com>
2023-08-22 11:55:36 +0300

    fixed the task status page error, and added format name helper

---
+++ apps/mobile/app/helpers/name-format.ts
@@ -0,0 +1,7 @@
+export const formatName = (str: string): string => {
+	return str
+		.replace(/-/g, " ")
+		.split(" ")
+		.map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+		.join(" ")
+}
