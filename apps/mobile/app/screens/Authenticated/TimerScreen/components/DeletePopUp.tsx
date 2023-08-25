05f5a08704ab9915d58f9f3fcd708ff0ed9e130a
desperado1802 <mumun_ali@hotmail.com>
2023-08-25 13:35:49 +0300

    adjusted the popup's position because the neighbor parent's border top was overlapping with popup

--- apps/mobile/app/screens/Authenticated/TimerScreen/components/DeletePopUp.tsx
+++ apps/mobile/app/screens/Authenticated/TimerScreen/components/DeletePopUp.tsx
@@ -2,53 +2,53 @@ import React from "react"
 import { StyleSheet, Text, TouchableOpacity } from "react-native"
 
 type IDeletePopUp = {
-  onCloseTask: any
-  setShowDel: any
+	onCloseTask: any
+	setShowDel: any
 }
 
 const DeletePopUp = ({ onCloseTask, setShowDel }: IDeletePopUp) => {
-  return (
-    <TouchableOpacity activeOpacity={0.7} style={styles.container}>
-      <TouchableOpacity
-        activeOpacity={0.7}
-        onPress={() => {
-          onCloseTask()
-          setShowDel(false)
-        }}
-      >
-        <Text style={{ fontWeight: "bold", color: "#1B005D", fontSize: 10 }}>Confirm</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        activeOpacity={0.7}
-        style={{marginTop:5}}
-        onPress={() => {
-          setShowDel(false)
-        }}
-      >
-        <Text style={{ color: "#1B005D", fontSize: 10 }}>Cancel</Text>
-      </TouchableOpacity>
-    </TouchableOpacity>
-  )
+	return (
+		<TouchableOpacity activeOpacity={0.7} style={styles.container}>
+			<TouchableOpacity
+				activeOpacity={0.7}
+				onPress={() => {
+					onCloseTask()
+					setShowDel(false)
+				}}
+			>
+				<Text style={{ fontWeight: "bold", color: "#1B005D", fontSize: 10 }}>Confirm</Text>
+			</TouchableOpacity>
+			<TouchableOpacity
+				activeOpacity={0.7}
+				style={{ marginTop: 5 }}
+				onPress={() => {
+					setShowDel(false)
+				}}
+			>
+				<Text style={{ color: "#1B005D", fontSize: 10 }}>Cancel</Text>
+			</TouchableOpacity>
+		</TouchableOpacity>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    position: "absolute",
-    zIndex: 100000000000,
-    backgroundColor: "#fff",
-    width: 50,
-    borderRadius: 5,
-    shadowColor: "#1B005D",
-    shadowOffset: { width: 5, height: 5 },
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
-    elevation: 5,
-    justifyContent: "center",
-    alignItems: "flex-start",
-    paddingLeft: 5,
-    top: 14,
-    right: 9,
-  },
+	container: {
+		alignItems: "flex-start",
+		backgroundColor: "#fff",
+		borderRadius: 5,
+		elevation: 5,
+		justifyContent: "center",
+		paddingLeft: 5,
+		position: "absolute",
+		right: 10,
+		shadowColor: "#1B005D",
+		shadowOffset: { width: 5, height: 5 },
+		shadowOpacity: 0.3,
+		shadowRadius: 5,
+		top: 11,
+		width: 50,
+		zIndex: 100000000000,
+	},
 })
 
 export default DeletePopUp
