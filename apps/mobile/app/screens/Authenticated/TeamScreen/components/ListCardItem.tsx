caa912fbdbd67f628620da1f116a021c0eba28c7
desperado1802 <mumun_ali@hotmail.com>
2023-08-30 22:35:55 +0300

    added types, and created seperate file for the unassigned task list

--- apps/mobile/app/screens/Authenticated/TeamScreen/components/ListCardItem.tsx
+++ apps/mobile/app/screens/Authenticated/TeamScreen/components/ListCardItem.tsx
@@ -8,11 +8,8 @@ import {
 	TouchableOpacity,
 	StyleSheet,
 	TouchableWithoutFeedback,
-	Text,
-	Image,
-	ImageStyle,
 } from "react-native"
-import { Ionicons, Entypo, EvilIcons, MaterialCommunityIcons, AntDesign } from "@expo/vector-icons"
+import { Ionicons, Entypo } from "@expo/vector-icons"
 
 // COMPONENTS
 import { Card, ListItem } from "../../../../components"
@@ -37,8 +34,7 @@ import { TodayWorkedTime } from "./TodayWorkTime"
 import { TimeProgressBar } from "./TimeProgressBar"
 import { useNavigation } from "@react-navigation/native"
 import { WorkedOnTask } from "./WorkedOnTask"
-import { ScrollView } from "react-native-gesture-handler"
-import TaskStatus from "../../../../components/TaskStatus"
+import UnassignedTasksList from "./UnassignedTaskList"
 
 export type ListItemProps = {
 	member: OT_Member
@@ -122,121 +118,6 @@ export const ListItemContent: React.FC<IcontentProps> = observer(
 		)
 	},
 )
-
-const UnassignedTasksList = observer(({ memberInfo, setShowUnassignedList }: any) => {
-	const { colors, dark } = useAppTheme()
-
-	return (
-		<View
-			style={[
-				{
-					...GS.p3,
-					...GS.positionRelative,
-					backgroundColor: dark ? "#1E2025" : colors.background,
-				},
-				{ borderRadius: 14 },
-			]}
-		>
-			<View style={[styles.firstContainer, { marginBottom: 16 }]}>
-				<UserHeaderCard user={memberInfo.memberUser} member={memberInfo.member} />
-				<View style={styles.wrapTotalTime}>
-					<TodayWorkedTime isAuthUser={memberInfo.isAuthUser} memberInfo={memberInfo} />
-				</View>
-			</View>
-			<ScrollView style={{ height: 150 }}>
-				{memberInfo.memberUnassignTasks.map((task, index) => {
-					if (task?.status !== "closed") {
-						return (
-							<TouchableOpacity
-								onPress={() => {
-									memberInfo?.assignTask(task)
-									setShowUnassignedList(false)
-								}}
-								key={index}
-								style={[
-									styles.unassignedTaskContainer,
-									index === memberInfo?.memberUnassignTasks.length - 1 && { marginBottom: 10 },
-								]}
-							>
-								<View
-									style={{
-										flexDirection: "row",
-										justifyContent: "space-between",
-										alignItems: "center",
-										width: "60%",
-									}}
-								>
-									<View style={styles.wrapTaskNumber}>
-										<View style={styles.wrapBugIcon}>
-											<MaterialCommunityIcons name="bug-outline" size={14} color="#fff" />
-										</View>
-										<Text
-											style={{ color: "#9490A0", fontSize: 12, marginLeft: 5 }}
-										>{`#${task.taskNumber}`}</Text>
-									</View>
-
-									<Text
-										style={[styles.unasignedTaskTitle, { color: colors.primary }]}
-										numberOfLines={2}
-									>
-										{task.title}
-									</Text>
-								</View>
-								<View
-									style={{
-										flexDirection: "row",
-										width: "40%",
-										alignItems: "center",
-										zIndex: 1000,
-										justifyContent: "space-between",
-									}}
-								>
-									<View>
-										<TaskStatus
-											iconsOnly={true}
-											task={task}
-											containerStyle={styles.statusContainer}
-										/>
-									</View>
-									<View
-										style={{
-											flexDirection: "row",
-											alignItems: "center",
-											justifyContent: "space-between",
-											width: "35%",
-										}}
-									>
-										<View style={{ flexDirection: "row" }}>
-											{task.members[0]?.user?.imageUrl ? (
-												<Image
-													source={{ uri: task.members[0]?.user?.imageUrl }}
-													style={$usersProfile}
-												/>
-											) : null}
-											{task.members[1]?.user?.imageUrl ? (
-												<Image
-													source={{ uri: task.members[1]?.user?.imageUrl }}
-													style={$usersProfile2}
-												/>
-											) : null}
-										</View>
-										{/* {task.status === "closed" ? (
-											<EvilIcons name="refresh" size={24} color="#8F97A1" />
-										) : (
-											<View>
-												<Entypo name="cross" size={15} color="#8F97A1" />
-											</View>
-										)} */}
-									</View>
-								</View>
-							</TouchableOpacity>
-						)
-					}
-				})}
-			</ScrollView>
-		</View>
-	)
-})
 
 const ListCardItem: React.FC<Props> = (props) => {
 	const { colors } = useAppTheme()
@@ -426,26 +307,6 @@ const $listCard: ViewStyle = {
 	shadowOffset: { width: 0, height: 0 },
 }
 
-const $usersProfile: ImageStyle = {
-	...GS.roundedFull,
-	backgroundColor: "#FFFFFF",
-	width: spacing.extraLarge - spacing.tiny,
-	height: spacing.extraLarge - spacing.tiny,
-	borderColor: "#fff",
-	borderWidth: 2,
-}
-
-const $usersProfile2: ImageStyle = {
-	...GS.roundedFull,
-	backgroundColor: "#F2F2F2",
-	width: spacing.extraLarge - spacing.tiny,
-	height: spacing.extraLarge - spacing.tiny,
-	borderColor: "#fff",
-	borderWidth: 2,
-	position: "absolute",
-	left: -15,
-}
-
 const styles = StyleSheet.create({
 	dropdownTxt: {
 		color: "#282048",
@@ -469,16 +330,7 @@ const styles = StyleSheet.create({
 		flexDirection: "row",
 		width: "95%",
 	},
-	statusContainer: {
-		alignItems: "center",
-		backgroundColor: "#ECE8FC",
-		borderColor: "transparent",
-		height: 27,
-		marginRight: 6,
-		paddingHorizontal: 7,
-		width: 50,
-		zIndex: 1000,
-	},
+
 	times: {
 		alignItems: "center",
 		borderTopWidth: 1,
@@ -486,34 +338,7 @@ const styles = StyleSheet.create({
 		justifyContent: "space-between",
 		paddingTop: 16,
 	},
-	unasignedTaskTitle: {
-		color: "#282048",
-		fontFamily: typography.fonts.PlusJakartaSans.semiBold,
-		fontSize: 10,
-		width: "67%",
-	},
-	unassignedTaskContainer: {
-		alignItems: "center",
-		borderTopColor: "rgba(0, 0, 0, 0.06)",
-		borderTopWidth: 1,
-		flexDirection: "row",
-		justifyContent: "space-between",
-		paddingVertical: 12,
-		width: "100%",
-		zIndex: 1000,
-	},
-	wrapBugIcon: {
-		alignItems: "center",
-		backgroundColor: "#C24A4A",
-		borderRadius: 3,
-		height: 20,
-		justifyContent: "center",
-		marginRight: 3,
-		width: 20,
-	},
-	wrapTaskNumber: {
-		flexDirection: "row",
-	},
+
 	wrapTaskTitle: {
 		borderTopWidth: 1,
 		marginTop: 16,
