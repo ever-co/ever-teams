51743adb4d6ef63ef45ecc3555ff6287cf606edb
desperado1802 <mumun_ali@hotmail.com>
2023-09-18 21:32:27 +0300

    fixed task size fetch url in order to sync with web

--- apps/mobile/app/services/client/queries/task/task-sizes.ts
+++ apps/mobile/app/services/client/queries/task/task-sizes.ts
@@ -5,16 +5,20 @@ interface IGetTaskSizeParams {
 	authToken: string
 	tenantId: string
 	organizationId: string
+	activeTeamId: string
 }
 const fetchAllSizes = async (params: IGetTaskSizeParams) => {
-	const { organizationId, tenantId, authToken } = params
+	const { organizationId, tenantId, activeTeamId, authToken } = params
 	const { data } = await getAllTaskSizesRequest(
 		{
 			tenantId,
 			organizationId,
+			activeTeamId,
 		},
 		authToken,
 	)
+	console.log("api getting active team id:", activeTeamId)
+
 	return data
 }
 
--- apps/mobile/app/services/client/requests/task-size.ts
+++ apps/mobile/app/services/client/requests/task-size.ts
@@ -1,69 +1,73 @@
-import { PaginationResponse } from "../../interfaces/IDataResponse";
-import { ITaskSizeCreate, ITaskSizeItem } from "../../interfaces/ITaskSize";
-import { serverFetch } from "../fetch";
+import { PaginationResponse } from "../../interfaces/IDataResponse"
+import { ITaskSizeCreate, ITaskSizeItem } from "../../interfaces/ITaskSize"
+import { serverFetch } from "../fetch"
 
 export function createSizeRequest({
-    datas, tenantId, bearer_token
-}:
-    {
-        datas: ITaskSizeCreate,
-        bearer_token: string,
-        tenantId?: any
-    }
-) {
-    return serverFetch<ITaskSizeItem>({
-        path: '/task-sizes',
-        method: 'POST',
-        body: datas,
-        bearer_token,
-        tenantId,
-    });
+	datas,
+	tenantId,
+	bearer_token,
+}: {
+	datas: ITaskSizeCreate
+	bearer_token: string
+	tenantId?: any
+}) {
+	return serverFetch<ITaskSizeItem>({
+		path: "/task-sizes",
+		method: "POST",
+		body: datas,
+		bearer_token,
+		tenantId,
+	})
 }
 
 export function updateTaskSizesRequest({
-    id,
-    datas,
-    bearer_token,
-    tenantId,
+	id,
+	datas,
+	bearer_token,
+	tenantId,
 }: {
-    id: string | any;
-    datas: ITaskSizeCreate;
-    bearer_token: string;
-    tenantId?: any;
+	id: string | any
+	datas: ITaskSizeCreate
+	bearer_token: string
+	tenantId?: any
 }) {
-    return serverFetch<ITaskSizeItem>({
-        path: `/task-sizes/${id}`,
-        method: 'PUT',
-        body: datas,
-        bearer_token,
-        tenantId,
-    });
+	return serverFetch<ITaskSizeItem>({
+		path: `/task-sizes/${id}`,
+		method: "PUT",
+		body: datas,
+		bearer_token,
+		tenantId,
+	})
 }
 
 export function deleteTaskSizeRequest({
-    id,
-    bearer_token,
-    tenantId,
+	id,
+	bearer_token,
+	tenantId,
 }: {
-    id: string | any;
-    bearer_token: string | any;
-    tenantId?: any;
+	id: string | any
+	bearer_token: string | any
+	tenantId?: any
 }) {
-    return serverFetch<ITaskSizeItem>({
-        path: `/task-sizes/${id}`,
-        method: 'DELETE',
-        bearer_token,
-        tenantId,
-    });
+	return serverFetch<ITaskSizeItem>({
+		path: `/task-sizes/${id}`,
+		method: "DELETE",
+		bearer_token,
+		tenantId,
+	})
 }
 
 export function getAllTaskSizesRequest(
-    { organizationId, tenantId }: { tenantId: string; organizationId: string },
-    bearer_token: string
+	{
+		organizationId,
+		tenantId,
+		activeTeamId,
+	}: { tenantId: string; organizationId: string; activeTeamId: string },
+	bearer_token: string,
 ) {
-    return serverFetch<PaginationResponse<ITaskSizeItem>>({
-        path: `/task-sizes?tenantId=${tenantId}&organizationId=${organizationId}`,
-        method: 'GET',
-        bearer_token,
-    });
+	return serverFetch<PaginationResponse<ITaskSizeItem>>({
+		path: `/task-sizes?tenantId=${tenantId}&organizationId=${organizationId}&organizationTeamId=${activeTeamId}`,
+		method: "GET",
+		bearer_token,
+	})
 }
