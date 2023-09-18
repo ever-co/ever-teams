f484c8e58853b2a5bd4de1c5cebde10c71cfde2b
desperado1802 <mumun_ali@hotmail.com>
2023-09-18 20:52:11 +0300

    modified task priority api to to sync mobile and web priorities

--- apps/mobile/app/services/client/requests/task-priority.ts
+++ apps/mobile/app/services/client/requests/task-priority.ts
@@ -1,68 +1,73 @@
-import { PaginationResponse } from "../../interfaces/IDataResponse";
-import { ITaskPriorityCreate, ITaskPriorityItem } from "../../interfaces/ITaskPriority";
-import { serverFetch } from "../fetch";
+import { PaginationResponse } from "../../interfaces/IDataResponse"
+import { ITaskPriorityCreate, ITaskPriorityItem } from "../../interfaces/ITaskPriority"
+import { serverFetch } from "../fetch"
 
-export function createPriorityRequest(
-    { datas, bearer_token, tenantId }:
-    {
-        datas: ITaskPriorityCreate,
-        bearer_token: string,
-        tenantId?: any
-    }
-) {
-    return serverFetch<ITaskPriorityItem>({
-        path: '/task-priorities',
-        method: 'POST',
-        body: datas,
-        bearer_token,
-        tenantId,
-    });
+export function createPriorityRequest({
+	datas,
+	bearer_token,
+	tenantId,
+}: {
+	datas: ITaskPriorityCreate
+	bearer_token: string
+	tenantId?: any
+}) {
+	return serverFetch<ITaskPriorityItem>({
+		path: "/task-priorities",
+		method: "POST",
+		body: datas,
+		bearer_token,
+		tenantId,
+	})
 }
 
 export function updateTaskPriorityRequest({
-    id,
-    datas,
-    bearer_token,
-    tenantId,
+	id,
+	datas,
+	bearer_token,
+	tenantId,
 }: {
-    id: string | any;
-    datas: ITaskPriorityCreate;
-    bearer_token: string;
-    tenantId?: any;
+	id: string | any
+	datas: ITaskPriorityCreate
+	bearer_token: string
+	tenantId?: any
 }) {
-    return serverFetch<ITaskPriorityItem>({
-        path: `/task-priorities/${id}`,
-        method: 'PUT',
-        body: datas,
-        bearer_token,
-        tenantId,
-    });
+	return serverFetch<ITaskPriorityItem>({
+		path: `/task-priorities/${id}`,
+		method: "PUT",
+		body: datas,
+		bearer_token,
+		tenantId,
+	})
 }
 
 export function deleteTaskPriorityRequest({
-    id,
-    bearer_token,
-    tenantId,
+	id,
+	bearer_token,
+	tenantId,
 }: {
-    id: string | any;
-    bearer_token: string | any;
-    tenantId?: any;
+	id: string | any
+	bearer_token: string | any
+	tenantId?: any
 }) {
-    return serverFetch<ITaskPriorityItem>({
-        path: `/task-priorities/${id}`,
-        method: 'DELETE',
-        bearer_token,
-        tenantId,
-    });
+	return serverFetch<ITaskPriorityItem>({
+		path: `/task-priorities/${id}`,
+		method: "DELETE",
+		bearer_token,
+		tenantId,
+	})
 }
 
 export function getTaskAllPrioritiesRequest(
-    { organizationId, tenantId }: { tenantId: string; organizationId: string },
-    bearer_token: string
+	{
+		organizationId,
+		tenantId,
+		activeTeamId,
+	}: { tenantId: string; organizationId: string; activeTeamId: string },
+	bearer_token: string,
 ) {
-    return serverFetch<PaginationResponse<ITaskPriorityItem>>({
-        path: `/task-priorities?tenantId=${tenantId}&organizationId=${organizationId}`,
-        method: 'GET',
-        bearer_token,
-    });
+	return serverFetch<PaginationResponse<ITaskPriorityItem>>({
+		path: `/task-priorities?tenantId=${tenantId}&organizationId=${organizationId}&organizationTeamId=${activeTeamId}`,
+		method: "GET",
+		bearer_token,
+	})
 }
