9c8258b9788ab123dd36fe3ad49929c26e0a8bab
desperado1802 <mumun_ali@hotmail.com>
2023-09-19 20:08:58 +0300

    created the api fetch logic for issues

---
+++ apps/mobile/app/services/client/requests/issue-type.ts
@@ -0,0 +1,69 @@
+/* eslint-disable camelcase */
+import { IIssueTypesCreate, IIssueTypesItemList } from "../../interfaces/ITaskIssue"
+import { serverFetch } from "../fetch"
+
+export function createIssueTypeRequest(
+	datas: IIssueTypesCreate,
+	bearer_token: string,
+	tenantId?: any,
+) {
+	return serverFetch<IIssueTypesItemList>({
+		path: "/issue-types",
+		method: "POST",
+		body: datas,
+		bearer_token,
+		tenantId,
+	})
+}
+
+export function editIssueTypesRequest({
+	id,
+	datas,
+	bearer_token,
+	tenantId,
+}: {
+	id: string | any
+	datas: IIssueTypesCreate
+	bearer_token: string
+	tenantId?: any
+}) {
+	return serverFetch<IIssueTypesItemList>({
+		path: `/issue-types/${id}`,
+		method: "PUT",
+		body: datas,
+		bearer_token,
+		tenantId,
+	})
+}
+
+export function deleteIssueTypesRequest({
+	id,
+	bearer_token,
+	tenantId,
+}: {
+	id: string | any
+	bearer_token: string | any
+	tenantId?: any
+}) {
+	return serverFetch<IIssueTypesItemList>({
+		path: `/issue-types/${id}`,
+		method: "DELETE",
+		bearer_token,
+		tenantId,
+	})
+}
+
+export function getIssueTypesListRequest<IIssueTypesItemList>(
+	{
+		organizationId,
+		tenantId,
+		activeTeamId,
+	}: { tenantId: string; organizationId: string; activeTeamId: string | null },
+	bearer_token: string,
+) {
+	return serverFetch({
+		path: `/issue-types?tenantId=${tenantId}&organizationId=${organizationId}&organizationTeamId=${activeTeamId}`,
+		method: "GET",
+		bearer_token,
+	})
+}
---
+++ apps/mobile/app/services/interfaces/ITaskIssue.ts
@@ -0,0 +1,41 @@
+export interface IIssueTypesCreate {
+	id: string
+	description?: string
+	icon?: string
+	color?: string
+	projectId?: string
+	organizationId?: string
+	tenantId?: string | undefined | null
+	organizationTeamId?: string | undefined | null
+}
+
+export interface IIssueTypesItemList extends IIssueTypesCreate {
+	id: string
+	createdAt: string
+	updatedAt: string
+	tenantId: string
+	organizationId: string
+	value?: string
+	fullIconUrl?: string
+	is_system?: boolean
+	isSystem?: boolean
+}
+
+export interface IIssueType {
+	id: string
+	createdAt: string
+	updatedAt: string
+	tenantId: string
+	organizationId: string
+	name: string
+	value: string
+	description: string
+	icon: string
+	color: string
+	isSystem: boolean
+	imageId: string | null
+	projectId: string | null
+	organizationTeamId: string
+	image: string | null
+	fullIconUrl: string
+}
