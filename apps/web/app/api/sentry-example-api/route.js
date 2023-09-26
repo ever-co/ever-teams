31775faea63394627ed54be04e075c04b6e6f5dd
Badal Khatri <badal.khatri0924@gmail.com>
2023-09-26 09:57:52 +0530

    fix: Build

--- apps/web/app/api/sentry-example-api/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-// A faulty API route to test Sentry's error monitoring
-export function GET() {
-  throw new Error("Sentry Example API Route Error");
-  return NextResponse.json({ data: "Testing Sentry Error..." });
-}
