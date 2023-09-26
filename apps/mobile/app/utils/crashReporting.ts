f59f0e284aa0270df82ac63855cfd7d051f85865
desperado1802 <mumun_ali@hotmail.com>
2023-09-26 23:32:33 +0300

    setting debug key to false, as it's suggested for production

--- apps/mobile/app/utils/crashReporting.ts
+++ apps/mobile/app/utils/crashReporting.ts
@@ -26,7 +26,7 @@ export const initCrashReporting = () => {
 	Sentry.init({
 		dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
 		enableInExpoDevelopment: true,
-		debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+		debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
 	})
 	// Bugsnag.start("YOUR API KEY")
 }
