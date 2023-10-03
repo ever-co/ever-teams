f4e6deb383a9f808373deabbd5cde44477ab77d7
Ruslan Konviser <evereq@gmail.com>
2023-10-03 19:43:07 +0200

    fix: some formatting

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -43,7 +43,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 						? { ...jitsuConfiguration }
 						: {
 								disabled: true
-						  }
+							}
 				}
 			>
 				<RecoilRoot>
