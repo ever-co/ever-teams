a36d3f6d5e357f3335f8413f69f018d99843db31
desperado1802 <mumun_ali@hotmail.com>
2023-10-04 13:08:21 +0300

    made the sync api to fetch only when timer is running, amd the timer status api to fetch every 5 seconds

--- apps/mobile/app/services/client/queries/timer/timer.ts
+++ apps/mobile/app/services/client/queries/timer/timer.ts
@@ -4,20 +4,22 @@ import { ITimerTimeslotParams } from "../../../interfaces/ITimer"
 
 type IGetTimerStatusParams = ITimerTimeslotParams & { authToken: string }
 
-const fetchTimerStatus = async (params: IGetTimerStatusParams) => {
+const fetchTimerStatus = async (params: IGetTimerStatusParams, isTimerRunning: boolean) => {
 	const { tenantId, organizationId, logType, authToken, employeeId } = params
 
-	await syncTimeSlotRequest(
-		{
-			tenantId,
-			organizationId,
-			source: "MOBILE",
-			employeeId,
-			duration: 5,
-			logType,
-		},
-		authToken,
-	)
+	if (isTimerRunning) {
+		await syncTimeSlotRequest(
+			{
+				tenantId,
+				organizationId,
+				source: "MOBILE",
+				employeeId,
+				duration: 5,
+				logType,
+			},
+			authToken,
+		)
+	}
 
 	const { data } = await getTimerStatusRequest({ tenantId, organizationId }, authToken)
 
@@ -25,10 +27,13 @@ const fetchTimerStatus = async (params: IGetTimerStatusParams) => {
 }
 
 const useFetchTimerStatus = (IGetTimerStatusParams, isTimerRunning: boolean) =>
-	useQuery(["status-timer", IGetTimerStatusParams], () => fetchTimerStatus(IGetTimerStatusParams), {
-		enabled: true,
-		refetchInterval: 5000,
-		notifyOnChangeProps: ["data"], // Re-render only when data changes
-		notifyOnChangePropsExclusions: ["isFetching"],
-	})
+	useQuery(
+		["status-timer", IGetTimerStatusParams],
+		() => fetchTimerStatus(IGetTimerStatusParams, isTimerRunning),
+		{
+			refetchInterval: 5000,
+			notifyOnChangeProps: ["data"], // Re-render only when data changes
+			notifyOnChangePropsExclusions: ["isFetching"],
+		},
+	)
 export default useFetchTimerStatus
