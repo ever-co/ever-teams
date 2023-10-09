208e185988244781d6119896912c730fd9937cad
desperado1802 <mumun_ali@hotmail.com>
2023-10-09 15:19:11 +0300

    styled the time-progress part of timer

--- apps/mobile/app/components/TimerCard.tsx
+++ apps/mobile/app/components/TimerCard.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react-native/no-color-literals */
+/* eslint-disable camelcase */
 import React, { FC } from "react"
 import { View } from "react-native"
 import { ProgressBar, Text } from "react-native-paper"
@@ -39,13 +42,13 @@ const TimerCard: FC<Props> = observer(() => {
 	return (
 		<View style={[styles.mainContainer, { borderTopColor: colors.border }]}>
 			<View style={styles.horizontal}>
-				<View style={{ justifyContent: "space-around", width: "61%" }}>
+				<View style={styles.timeAndProgressBarWrapper}>
 					<Text style={[styles.timerText, { color: colors.primary }]}>
 						{pad(hours)}:{pad(minutes)}:{pad(seconds)}
-						<Text style={{ fontSize: 14 }}>:{pad(ms_p)}</Text>
+						<Text style={{ fontSize: 14, fontWeight: "600" }}>:{pad(ms_p)}</Text>
 					</Text>
 					<ProgressBar
-						style={{ backgroundColor: "#E9EBF8", width: "84%", height: 6, borderRadius: 3 }}
+						style={styles.progressBar}
 						progress={getTimePercentage()}
 						color={activeTask && activeTask.estimate > 0 ? "#27AE60" : "#F0F0F0"}
 					/>
@@ -70,16 +73,25 @@ const styles = EStyleSheet.create({
 		marginVertical: 4,
 		justifyContent: "center",
 		alignItems: "center",
-		width: "39%",
+		width: "34%",
 		borderLeftWidth: 2,
 		borderLeftColor: "rgba(0, 0, 0, 0.08)",
+		height: "100%",
+	},
+	progressBar: { backgroundColor: "#E9EBF8", width: "79%", height: 6, borderRadius: 3 },
+	timeAndProgressBarWrapper: {
+		flexDirection: "column",
+		justifyContent: "space-between",
+		width: "66%",
+		height: 70,
+		paddingBottom: 0,
 	},
 	horizontal: {
 		flexDirection: "row",
 		justifyContent: "space-between",
 		alignItems: "center",
 		width: "100%",
-		height: 60,
+		height: 70,
 	},
 	loading: {
 		position: "absolute",
@@ -88,8 +100,10 @@ const styles = EStyleSheet.create({
 	},
 	timerText: {
 		fontWeight: "600",
-		fontSize: "2rem",
+		fontSize: "2.3rem",
 		color: "#1B005D",
+		marginTop: 0,
+		paddingTop: 0,
 		fontFamily: typography.fonts.PlusJakartaSans.semiBold,
 	},
 })
