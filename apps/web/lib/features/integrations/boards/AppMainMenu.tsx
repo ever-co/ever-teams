6d3741a43b518a842351b8a9b6d40e6a69f1aba9
Paradoxe Ngwasi <paradoxngwasi@outlook.com>
2023-10-13 04:43:42 +0000

    chore: improve board menu

---
+++ apps/web/lib/features/integrations/boards/AppMainMenu.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { MainMenu } from '@excalidraw/excalidraw';
+
+export const AppMainMenu = React.memo(() => {
+	return (
+		<MainMenu>
+			<MainMenu.DefaultItems.LoadScene />
+			<MainMenu.DefaultItems.SaveToActiveFile />
+			<MainMenu.DefaultItems.Export />
+			<MainMenu.DefaultItems.SaveAsImage />
+
+			<MainMenu.DefaultItems.Help />
+			<MainMenu.DefaultItems.ClearCanvas />
+
+			<MainMenu.DefaultItems.ToggleTheme />
+			<MainMenu.DefaultItems.ChangeCanvasBackground />
+		</MainMenu>
+	);
+});
