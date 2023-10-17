6e78330a9b41e4c3c22315316127aeb9fbe451a1
desperado1802 <mumun_ali@hotmail.com>
2023-10-17 15:45:01 +0300

    added formatted timezones to the dropdown

--- apps/web/lib/settings/timezone-dropdown.tsx
+++ apps/web/lib/settings/timezone-dropdown.tsx
@@ -4,6 +4,7 @@ import timeZones from './timezones';
 import { mapTimezoneItems, TimezoneItem } from 'lib/features';
 import { useTimezoneSettings } from '@app/hooks';
 import { clsxm } from '@app/utils';
+import moment from 'moment-timezone';
 
 export const TimezoneDropDown = ({
 	currentTimezone,
@@ -14,7 +15,28 @@ export const TimezoneDropDown = ({
 }) => {
 	const { activeTimezone, setActiveTimezone } = useTimezoneSettings();
 
-	const timeZonesMap: string[] = timeZones; // TODO: we should import here from timeZones
+	const allTimezonesNames = moment.tz.names();
+	const allTimezonesWithUTC = allTimezonesNames.map((item) => {
+		const offset = moment.tz(item).format('Z');
+		return { name: item, offset: offset };
+	});
+
+	allTimezonesWithUTC.sort((a, b) => {
+		// Compare the offsets for sorting
+		if (a.offset < b.offset) {
+			return -1;
+		}
+		if (a.offset > b.offset) {
+			return 1;
+		}
+		return 0;
+	});
+
+	const sortedTimezones = allTimezonesWithUTC.map(
+		(item) => `${item.name} (UTC ${item.offset})`
+	);
+
+	const timeZonesMap: string[] = sortedTimezones; // TODO: we should import here from timeZones
 
 	const items = useMemo(() => mapTimezoneItems(timeZonesMap), [timeZonesMap]);
 
