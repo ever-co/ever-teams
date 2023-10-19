45ce7d2aaed673d7cb9b663b224869ef2968270e
desperado1802 <mumun_ali@hotmail.com>
2023-10-19 12:35:52 +0300

    added FlatLists to Profile section task cards

--- apps/mobile/app/screens/Authenticated/ProfileScreen/components/UserProfileTasks.tsx
+++ apps/mobile/app/screens/Authenticated/ProfileScreen/components/UserProfileTasks.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-color-literals */
 /* eslint-disable react-native/no-inline-styles */
-import { Dimensions, ScrollView, Text, TextStyle, View } from "react-native"
+import { Dimensions, FlatList, ScrollView, Text, TextStyle, View } from "react-native"
 import React, { FC, useMemo } from "react"
 import { IUserProfile } from "../logics/useProfileScreenLogic"
 import { ITaskFilter } from "../../../../services/hooks/features/useTaskFilters"
@@ -27,143 +27,166 @@ const UserProfileTasks: FC<IUserProfileTasks> = observer(({ profile, content }) 
 	}, [content, profile])
 
 	return (
-		<ScrollView
-			style={{
-				flex: 1,
-				paddingHorizontal: 20,
-				backgroundColor: dark ? "rgb(16,17,20)" : colors.background2,
-			}}
-			bounces={false}
-		>
-			{content.tab === "worked" &&
-			profile.activeUserTeamTask &&
-			(profile.member?.timerStatus === "running" ||
-				(profile.isAuthUser && timerStatus?.running)) ? (
-				<>
-					<View
-						style={{
-							flexDirection: "row",
-							justifyContent: "space-between",
-							marginVertical: 20,
-						}}
-					>
-						<Text style={[$textLabel, { color: colors.primary }]}>
-							{translate("tasksScreen.now")}
-						</Text>
-						<View
-							style={{
-								width: width / 1.8,
-								alignSelf: "center",
-								borderBottomWidth: 1,
-								borderBottomColor: colors.border,
-							}}
-						/>
-						<View style={{ flexDirection: "row", alignItems: "center" }}>
-							<Text
+		<>
+			{content.tab === "worked" && (
+				<ScrollView
+					style={{
+						flex: 1,
+						paddingHorizontal: 20,
+						backgroundColor: dark ? "rgb(16,17,20)" : colors.background2,
+					}}
+					bounces={false}
+				>
+					{content.tab === "worked" &&
+					profile.activeUserTeamTask &&
+					(profile.member?.timerStatus === "running" ||
+						(profile.isAuthUser && timerStatus?.running)) ? (
+						<>
+							<View
 								style={{
-									color: colors.primary,
-									fontSize: 12,
-									fontFamily: typography.secondary.medium,
+									flexDirection: "row",
+									justifyContent: "space-between",
+									marginVertical: 20,
 								}}
 							>
-								{translate("tasksScreen.totalTimeLabel")}:
-							</Text>
-							<Text
-								style={[
-									$textLabel,
-									{
-										marginLeft: 5,
-										color: colors.primary,
-										fontFamily: typography.primary.semiBold,
-										fontSize: 12,
-									},
-								]}
-							></Text>
-							<WorkedOnTaskHours
-								memberTask={profile.activeUserTeamTask}
-								containerStyle={{ alignItems: "center" }}
-								totalTimeText={{ color: colors.primary, fontSize: 12 }}
+								<Text style={[$textLabel, { color: colors.primary }]}>
+									{translate("tasksScreen.now")}
+								</Text>
+								<View
+									style={{
+										width: width / 1.8,
+										alignSelf: "center",
+										borderBottomWidth: 1,
+										borderBottomColor: colors.border,
+									}}
+								/>
+								<View style={{ flexDirection: "row", alignItems: "center" }}>
+									<Text
+										style={{
+											color: colors.primary,
+											fontSize: 12,
+											fontFamily: typography.secondary.medium,
+										}}
+									>
+										{translate("tasksScreen.totalTimeLabel")}:
+									</Text>
+									<Text
+										style={[
+											$textLabel,
+											{
+												marginLeft: 5,
+												color: colors.primary,
+												fontFamily: typography.primary.semiBold,
+												fontSize: 12,
+											},
+										]}
+									></Text>
+									<WorkedOnTaskHours
+										memberTask={profile.activeUserTeamTask}
+										containerStyle={{ alignItems: "center" }}
+										totalTimeText={{ color: colors.primary, fontSize: 12 }}
+									/>
+								</View>
+							</View>
+							<ListCardItem
+								task={profile.activeUserTeamTask}
+								isAuthUser={profile.isAuthUser}
+								isAssigned={true}
+								activeAuthTask={true}
+								profile={profile}
 							/>
-						</View>
-					</View>
-					<ListCardItem
-						task={profile.activeUserTeamTask}
-						isAuthUser={profile.isAuthUser}
-						isAssigned={true}
-						activeAuthTask={true}
-						profile={profile}
-					/>
-				</>
-			) : null}
+						</>
+					) : null}
 
-			{content.tab === "worked" && (
-				<View>
-					<View
-						style={{
-							flexDirection: "row",
-							justifyContent: "space-between",
-							marginVertical: 20,
-						}}
-					>
-						<Text style={[$textLabel, { color: colors.primary }]}>
-							{translate("tasksScreen.last24hours")} ({tasks.length})
-						</Text>
-						<View
-							style={{
-								width: width / 1.5,
-								alignSelf: "center",
-								top: 3,
-								borderBottomWidth: 1,
-								borderBottomColor: colors.border,
-							}}
+					{content.tab === "worked" && (
+						<View>
+							<View
+								style={{
+									flexDirection: "row",
+									justifyContent: "space-between",
+									marginVertical: 20,
+								}}
+							>
+								<Text style={[$textLabel, { color: colors.primary }]}>
+									{translate("tasksScreen.last24hours")} ({tasks.length})
+								</Text>
+								<View
+									style={{
+										width: width / 1.5,
+										alignSelf: "center",
+										top: 3,
+										borderBottomWidth: 1,
+										borderBottomColor: colors.border,
+									}}
+								/>
+							</View>
+							{tasks.map((task, index) => (
+								<View key={index.toString()} style={{ ...GS.mb4 }}>
+									<ListCardItem
+										task={task}
+										isAssigned={true}
+										isAuthUser={profile.isAuthUser}
+										activeAuthTask={activeTaskId === task?.id}
+										profile={profile}
+									/>
+								</View>
+							))}
+						</View>
+					)}
+				</ScrollView>
+			)}
+
+			{(content.tab === "assigned" || content.tab === "unassigned") && (
+				<View
+					style={{
+						flex: 1,
+						paddingHorizontal: 10,
+						backgroundColor: dark ? "rgb(16,17,20)" : colors.background2,
+					}}
+				>
+					{content.tab === "assigned" && (
+						<FlatList
+							data={content.tasksFiltered}
+							renderItem={({ item, index }) => (
+								<View
+									key={index.toString()}
+									style={{ ...GS.mb4, marginHorizontal: 10, marginTop: index === 0 && 20 }}
+								>
+									<ListCardItem
+										task={item}
+										isAssigned={true}
+										isAuthUser={profile.isAuthUser}
+										activeAuthTask={false}
+										profile={profile}
+									/>
+								</View>
+							)}
 						/>
-					</View>
-					{tasks.map((task, index) => (
-						<View key={index.toString()} style={{ ...GS.mb4 }}>
-							<ListCardItem
-								task={task}
-								isAssigned={true}
-								isAuthUser={profile.isAuthUser}
-								activeAuthTask={activeTaskId === task?.id}
-								profile={profile}
-							/>
-						</View>
-					))}
-				</View>
-			)}
+					)}
 
-			{content.tab === "assigned" && (
-				<View style={{ ...GS.mt4 }}>
-					{content.tasksFiltered.map((task, index) => (
-						<View key={index.toString()} style={{ ...GS.mb4 }}>
-							<ListCardItem
-								task={task}
-								isAssigned={true}
-								isAuthUser={profile.isAuthUser}
-								activeAuthTask={false}
-								profile={profile}
-							/>
-						</View>
-					))}
+					{content.tab === "unassigned" && (
+						<FlatList
+							data={content.tasksFiltered}
+							initialNumToRender={5}
+							renderItem={({ item, index }) => (
+								<View
+									key={index.toString()}
+									style={{ ...GS.mb4, marginHorizontal: 10, marginTop: index === 0 && 20 }}
+								>
+									<ListCardItem
+										task={item}
+										isAssigned={true}
+										isAuthUser={profile.isAuthUser}
+										activeAuthTask={false}
+										profile={profile}
+									/>
+								</View>
+							)}
+						/>
+					)}
 				</View>
 			)}
-
-			{content.tab === "unassigned" && (
-				<View style={{ ...GS.mt4 }}>
-					{content.tasksFiltered.map((task, index) => (
-						<View key={index.toString()} style={{ ...GS.mb4 }}>
-							<ListCardItem
-								task={task}
-								isAssigned={false}
-								isAuthUser={profile.isAuthUser}
-								activeAuthTask={false}
-								profile={profile}
-							/>
-						</View>
-					))}
-				</View>
-			)}
-		</ScrollView>
+		</>
 	)
 })
 
