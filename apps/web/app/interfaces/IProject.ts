c57e5592e51bd356aadca3e0c6b5d47a98130528
Badal Khatri <badal.khatri0924@gmail.com>
2023-10-22 11:29:35 +0530

    Feat: Updated Payload

--- apps/web/app/interfaces/IProject.ts
+++ apps/web/app/interfaces/IProject.ts
@@ -1,10 +1,26 @@
+export interface IProjectRepository {
+	id: string;
+	createdAt?: string;
+	updatedAt?: string;
+	isActive: boolean;
+	isArchived: boolean;
+	tenantId: string;
+	organizationId: string;
+	repositoryId: number;
+	name: string;
+	fullName: string;
+	owner: string;
+	integrationId: string;
+}
+
 export interface IProject {
 	id: string;
 	createdAt?: string;
 	updatedAt?: string;
 	tenantId: string;
 	organizationId: string;
-	externalRepositoryId?: number;
+	repositoryId?: number;
+	repository?: IProjectRepository;
 }
 
 export interface IProjectCreate {
