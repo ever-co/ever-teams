5f724340dc5afced58f2610443f06a25b2cd239f
Ndekocode <arickbulakali@gmail.com>
2023-10-23 00:07:04 +0200

    refactor: [internationalization] translation libs layout components

--- apps/web/lib/layout/footer.tsx
+++ apps/web/lib/layout/footer.tsx
@@ -1,23 +1,18 @@
 import { IClassName } from '@app/interfaces';
 import { clsxm } from '@app/utils';
 import { Text, ThemeToggler } from 'lib/components';
-import { useTranslation } from 'lib/i18n';
+import { useTranslation } from 'next-i18next';
 
 export function Footer({ className }: IClassName) {
-	const { trans } = useTranslation();
+	const { t } = useTranslation();
 
 	return (
-		<footer
-			className={clsxm(
-				'flex flex-col xs:flex-row justify-around items-center w-full py-6 px-3',
-				className
-			)}
-		>
+		<footer className={clsxm('flex flex-col xs:flex-row justify-around items-center w-full py-6 px-3', className)}>
 			<p className="text-neutral text-xs font-normal leading-4 -tracking-[-0.02em] text-center md:mb-2 mb-7">
-				© 2022-Present,{' '}
-				<Text.Link href="https://ever.team">{trans.TITLE}</Text.Link> by{' '}
-				<Text.Link href="https://ever.co/">Ever Co</Text.Link>.{' '}
-				{trans.layout.footer.RIGHTS_RESERVERD}
+				{t('layout.footer.COPY_RIGHT1', { date: new Date().getFullYear() })}{' '}
+				<Text.Link href="https://ever.team">{t('TITLE')}</Text.Link> {t('layout.footer.BY')}{' '}
+				<Text.Link href="https://ever.co/">{t('layout.footer.COPY_RIGHT4')}</Text.Link>{' '}
+				{t('layout.footer.RIGHTS_RESERVERD')}
 			</p>
 
 			<div className="">
