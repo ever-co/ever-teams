de6066342d34e9649c09369cab22e272dafdf12f
Ndekocode <arickbulakali@gmail.com>
2023-10-22 14:20:26 +0200

    refactor: [internationalization] translation of footer layout

--- apps/web/components/layout/footer/footer.tsx
+++ apps/web/components/layout/footer/footer.tsx
@@ -1,18 +1,19 @@
+import { useTranslation } from 'next-i18next';
 import ToggleThemeContainer from '../toggleThemeBtns';
-
 const Footer = () => {
+	const { t } = useTranslation();
 	return (
-		<footer className="py-2 flex flex-col md:flex-row items-center text-sm lg:text-base text-center sm:text-start justify-between font-light x-container-fluid">
-			<div className="text-light flex flex-col space-x-1 sm:flex-row md:flex-start sm:items-center  text-center md:flex-row">
-				<div className="flex items-center justify-between sm:justify-start sm:space-x-2 space-x-1">
-					<span className="px-1">@ 2022-Present,</span>
+		<footer className="flex flex-col items-center justify-between py-2 text-sm font-light text-center md:flex-row lg:text-base sm:text-start x-container-fluid">
+			<div className="flex flex-col space-x-1 text-center text-light sm:flex-row md:flex-start sm:items-center md:flex-row">
+				<div className="flex items-center justify-between space-x-1 sm:justify-start sm:space-x-2">
+					<span className="px-1">{t('layout.footer.COPY_RIGHT1', { date: new Date().getFullYear() })}</span>
 					<a
 						href="https://gauzy.team"
 						target="_blank"
 						className="text-primary dark:text-gray-300"
 						rel="noreferrer"
 					>
-						Gauzy Teams
+						{t('layout.footer.COPY_RIGHT3')}
 					</a>
 					<div>by</div>
 					<a
@@ -21,27 +22,27 @@ const Footer = () => {
 						className="text-primary dark:text-gray-300"
 						rel="noreferrer"
 					>
-						Ever Co. LTD.
+						{t('layout.footer.COMPANY_NAME')}
 					</a>{' '}
-					<div className="hidden xs:flex space-x-2 sm:hidden">
+					<div className="hidden space-x-2 xs:flex sm:hidden">
 						<ToggleThemeContainer />
 					</div>
 				</div>
-				<div>All rights reserved.</div>
-				<div className="hidden sm:flex space-x-2 md:hidden">
+				<div>{t('layout.footer.RIGHTS_RESERVERD')}</div>
+				<div className="hidden space-x-2 sm:flex md:hidden">
 					<ToggleThemeContainer />
 				</div>
 			</div>
 
-			<div className="flex flex-col xs:flex-row sm:flex-col items-center md:flex-row">
-				<div className="flex space-x-4 text-center w-full justify-center">
+			<div className="flex flex-col items-center xs:flex-row sm:flex-col md:flex-row">
+				<div className="flex justify-center w-full space-x-4 text-center">
 					<a
 						href="https://demo.gauzy.co/#/pages/legal/terms"
 						target="_blank"
 						className="text-primary dark:text-gray-300"
 						rel="noreferrer"
 					>
-						Terms Of Service
+						{t('layout.footer.TERMS')}
 					</a>
 					<a
 						href="https://demo.gauzy.co/#/pages/legal/privacy"
@@ -49,10 +50,10 @@ const Footer = () => {
 						className="text-primary dark:text-gray-300"
 						rel="noreferrer"
 					>
-						Privacy Policy
+						{t('layout.footer.PRIVACY_POLICY')}
 					</a>
 				</div>
-				<div className="space-x-2 px-4 flex xs:hidden md:flex">
+				<div className="flex px-4 space-x-2 xs:hidden md:flex">
 					<ToggleThemeContainer />
 				</div>
 			</div>
