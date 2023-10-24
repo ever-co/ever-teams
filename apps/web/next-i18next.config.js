0ad306b0b042bfa1ef6db50bef0f878687877baf
Ndekocode <arickbulakali@gmail.com>
2023-10-24 10:58:37 +0200

    refactor: [internationalization] change next-18next config

--- apps/web/next-i18next.config.js
+++ apps/web/next-i18next.config.js
@@ -3,29 +3,10 @@
 module.exports = {
 	debug: process.env.NODE_ENV === 'development',
 	i18n: {
-		locales: [
-			'en',
-			'fr',
-			'ar',
-			'bg',
-			'zh',
-			'nl',
-			'de',
-			'he',
-			'it',
-			'pl',
-			'pt',
-			'ru',
-			'es'
-		],
-		defaultLocale: 'en',
-		defaultNS: 'default',
-		localeExtension: 'json'
+		locales: ['en', 'fr', 'ar', 'bg', 'zh', 'nl', 'de', 'he', 'it', 'pl', 'pt', 'ru', 'es'],
+		defaultLocale: 'en'
 	},
 	/** To avoid issues when deploying to some paas (vercel...) */
-	localePath:
-		typeof window === 'undefined'
-			? require('path').resolve('./public/locales')
-			: '/locales',
+	localePath: typeof window === 'undefined' ? require('path').resolve('./public/locales') : '/locales',
 	reloadOnPrerender: process.env.NODE_ENV === 'development'
 };
