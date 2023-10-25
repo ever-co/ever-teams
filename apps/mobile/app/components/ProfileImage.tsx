c8ec4612990b9d98ad959652446296dea1aa12cf
desperado1802 <124465103+desperado1802@users.noreply.github.com>
2023-10-25 08:57:33 +0300

    Improve/profile avatar timer status (#1634)
    * added initial status icon and color
    
    * positioned the icon better, and removed unused code
    
    * updating icon bg color dynamically
    
    * added larger icons

--- apps/mobile/app/components/ProfileImage.tsx
+++ apps/mobile/app/components/ProfileImage.tsx
@@ -1,11 +1,22 @@
 /* eslint-disable react-native/no-unused-styles */
 /* eslint-disable react-native/no-color-literals */
+/* eslint-disable react-native/no-inline-styles */
 import React, { FC, useMemo } from "react"
 import { View, StyleSheet } from "react-native"
-import { Avatar, Badge } from "react-native-paper"
+import { Avatar } from "react-native-paper"
 import { IUser } from "../services/interfaces/IUserData"
 import { typography, useAppTheme } from "../theme"
 import { imgTitleProfileAvatar } from "../helpers/img-title-profile-avatar"
+import { useOrganizationTeam } from "../services/hooks/useOrganization"
+import { useTimer } from "../services/hooks/useTimer"
+import { getTimerStatusValue } from "../helpers/get-timer-status"
+import {
+	idleStatusIconLarge,
+	onlineAndTrackingTimeStatusIconLarge,
+	pauseStatusIconLarge,
+	suspendedStatusIconLarge,
+} from "./svgs/icons"
+import { SvgXml } from "react-native-svg"
 
 interface Props {
 	user: IUser
@@ -14,10 +25,37 @@ interface Props {
 const ProfileImage: FC<Props> = ({ user, size }) => {
 	const { colors } = useAppTheme()
 
+	const { currentTeam } = useOrganizationTeam()
+
+	const currentMember = currentTeam?.members.find(
+		(currentMember) => currentMember.employee.userId === user.id,
+	)
+
+	const { timerStatus } = useTimer()
+
+	const status = getTimerStatusValue(timerStatus, currentMember, currentTeam?.public)
+
 	const imageUrl = useMemo(
 		() => user?.image?.thumbUrl || user?.image?.fullUrl || user?.imageUrl,
 		[user?.image?.thumb, user],
 	)
+
+	let iconSvgXml = ""
+
+	switch (status) {
+		case "online":
+			iconSvgXml = onlineAndTrackingTimeStatusIconLarge
+			break
+		case "pause":
+			iconSvgXml = pauseStatusIconLarge
+			break
+		case "idle":
+			iconSvgXml = idleStatusIconLarge
+			break
+		case "suspended":
+			iconSvgXml = suspendedStatusIconLarge
+			break
+	}
 
 	return (
 		<View style={styles.container}>
@@ -25,7 +63,19 @@ const ProfileImage: FC<Props> = ({ user, size }) => {
 				{imageUrl ? (
 					<Avatar.Image
 						size={size - 6}
-						style={{ ...styles.profileImage, width: size, height: size }}
+						style={{
+							...styles.profileImage,
+							width: size,
+							height: size,
+							borderColor:
+								status === "online"
+									? "#6EE7B7"
+									: status === "pause"
+									? "#EFCF9E"
+									: status === "idle"
+									? "#F5BEBE"
+									: "#DCD6D6",
+						}}
 						source={{
 							uri: imageUrl,
 						}}
@@ -34,11 +84,46 @@ const ProfileImage: FC<Props> = ({ user, size }) => {
 					<Avatar.Text
 						label={imgTitleProfileAvatar(user?.name)}
 						size={size - 6}
-						style={{ ...styles.profileImage, width: size, height: size }}
+						style={{
+							...styles.profileImage,
+							width: size,
+							height: size,
+							borderColor:
+								status === "online"
+									? "#6EE7B7"
+									: status === "pause"
+									? "#EFCF9E"
+									: status === "idle"
+									? "#F5BEBE"
+									: "#DCD6D6",
+						}}
 						labelStyle={styles.prefix}
 					/>
 				)}
-				<Badge size={25} style={[styles.onlineIcon, { borderColor: colors.background }]} />
+				<View
+					style={{
+						right: 0,
+						bottom: 0,
+						borderRadius: 100,
+						width: 25,
+						height: 25,
+						position: "absolute",
+						backgroundColor:
+							status === "online"
+								? "#6EE7B7"
+								: status === "pause"
+								? "#EFCF9E"
+								: status === "idle"
+								? "#F5BEBE"
+								: "#DCD6D6",
+						alignItems: "center",
+						justifyContent: "center",
+						borderColor: colors.background,
+						borderWidth: 3,
+					}}
+				>
+					<SvgXml xml={iconSvgXml} />
+				</View>
 			</View>
 		</View>
 	)
@@ -50,20 +135,12 @@ const styles = StyleSheet.create({
 		alignItems: "center",
 		height: "10%",
 	},
-	onlineIcon: {
-		backgroundColor: "#27AE60",
-		borderWidth: 4,
-		bottom: 0,
-		position: "absolute",
-		right: 0,
-	},
 	prefix: {
 		fontFamily: typography.fonts.PlusJakartaSans.light,
 		fontSize: 42,
 		fontWeight: "200",
 	},
 	profileImage: {
-		borderColor: "#86DAA9",
 		borderRadius: 200,
 		borderWidth: 3,
 	},
