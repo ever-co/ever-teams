53d03c52d68f97d97c58e8d7e9238957dedb9b0f
yehualashet <yehualashetab@gmail.com>
2023-10-26 19:25:56 +0200

    Feat/table view for issues (#1648)
    * [feat] add table view for issues
    * create data table ui component
    * create re usable table component and used them for Data table UI component
    * refactor team members componen so that it can show card and table component
    * add a swtich button to change issue view
    
    * fix ts lint error and refactor team member table view
    
    * * add footer to team table view
    
    * * fix lint issue
    
    * address review comment
    
    * address review comment
    
    * fix ui related issue
    
    * fix: Background color
    
    ---------
    
    Co-authored-by: Ruslan Konviser <evereq@gmail.com>
    Co-authored-by: Badal Khatri <badal.khatri0924@gmail.com>

---
+++ apps/web/components/ui/data-table.tsx
@@ -0,0 +1,139 @@
+import React from 'react';
+import {
+	ColumnDef,
+	ColumnFiltersState,
+	SortingState,
+	VisibilityState,
+	flexRender,
+	getCoreRowModel,
+	getFacetedRowModel,
+	getFacetedUniqueValues,
+	getFilteredRowModel,
+	getPaginationRowModel,
+	getSortedRowModel,
+	useReactTable,
+} from "@tanstack/react-table"
+
+import {
+	Table,
+	TableHeader,
+	TableRow,
+	TableHead,
+	TableCell,
+	TableBody,
+	TableFooter,
+} from './table';
+
+interface DataTableProps<TData, TValue> {
+	columns: ColumnDef<TData, TValue>[]
+	data: TData[],
+	footerRows?: React.ReactNode[],
+	isError?: boolean;
+	noResultsMessage?: {
+		heading: string;
+		content: string;
+	}
+}
+
+function DataTable<TData, TValue>({
+	columns,
+	data,
+	footerRows,
+}: DataTableProps<TData, TValue>) {
+
+	const [rowSelection, setRowSelection] = React.useState({})
+	const [columnVisibility, setColumnVisibility] =
+		React.useState<VisibilityState>({})
+	const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+		[]
+	)
+	const [sorting, setSorting] = React.useState<SortingState>([])
+
+	const table = useReactTable({
+		data,
+		columns,
+		state: {
+			sorting,
+			columnVisibility,
+			rowSelection,
+			columnFilters,
+		},
+		enableRowSelection: true,
+		onRowSelectionChange: setRowSelection,
+		onSortingChange: setSorting,
+		onColumnFiltersChange: setColumnFilters,
+		onColumnVisibilityChange: setColumnVisibility,
+		getCoreRowModel: getCoreRowModel(),
+		getFilteredRowModel: getFilteredRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		getFacetedRowModel: getFacetedRowModel(),
+		getFacetedUniqueValues: getFacetedUniqueValues(),
+	})
+
+	return (
+		<Table className='border-transparent bg-light--theme-light dark:bg-dark--theme-light mt-8 w-full'>
+			<TableHeader>
+				{table.getHeaderGroups().map((headerGroup) => (
+					<TableRow key={headerGroup.id}>
+						{headerGroup.headers.map((header) => {
+							return (
+								<TableHead key={header.id}>
+									{header.isPlaceholder
+										? null
+										: flexRender(
+											header.column.columnDef.header,
+											header.getContext()
+										)}
+								</TableHead>
+							)
+						})}
+					</TableRow>
+				))}
+			</TableHeader>
+			<TableBody className="divide-y divide-gray-200">
+				{table.getRowModel().rows?.length ? (
+					table.getRowModel().rows.map((row) => (
+						<TableRow
+							key={row.id}
+							data-state={row.getIsSelected() && "selected"}
+							className='my-4'
+						>
+							{row.getVisibleCells().map((cell) => (
+								<TableCell key={cell.id}
+								className="rounded-[16px] my-4">
+
+									{flexRender(
+										cell.column.columnDef.cell,
+										cell.getContext()
+									)}
+								</TableCell>
+							))}
+						</TableRow>
+					))
+				) : (
+					<TableRow>
+						<TableCell
+							colSpan={columns.length}
+							className="h-24 text-center"
+						>
+							No results.
+						</TableCell>
+					</TableRow>
+				)}
+			</TableBody>
+			{
+				footerRows && footerRows?.length > 0 && (
+					<TableFooter className='bg-gray-50 dark:bg-gray-800'>
+						{footerRows.map((row, index) => (
+							<TableRow key={`footer-row-${index}}`}>{row}</TableRow>
+						))}
+					</TableFooter>
+				)
+			}
+
+		</Table>
+	);
+}
+
+export default DataTable;
---
+++ apps/web/lib/features/team-members-table-view.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import DataTable from '@components/ui/data-table';
+import { Column, ColumnDef } from '@tanstack/react-table';
+import { OT_Member } from '@app/interfaces';
+import { UserInfoCell, TaskCell, WorkedOnTaskCell, TaskEstimateInfoCell, ActionMenuCell } from './team-member-cell';
+import { useAuthenticateUser, useModal } from '@app/hooks';
+import { InviteUserTeamCard } from './team/invite/user-invite-card';
+import { InviteFormModal } from './team/invite/invite-form-modal';
+
+const TeamMembersTableView = ({
+	teamMembers,
+	publicTeam = false,
+	active = false
+}: {
+	teamMembers: OT_Member[];
+	publicTeam?: boolean;
+	active?: boolean;
+}) => {
+	const columns = React.useMemo<ColumnDef<OT_Member>[]>(
+		() => [
+			{
+				id: 'name',
+				header: 'Name',
+				cell: UserInfoCell,
+				meta: {
+					publicTeam
+				}
+			},
+			{
+				id: 'task',
+				header: 'Task',
+				cell: TaskCell
+			},
+			{
+				id: 'workedOnTask',
+				header: 'Worked on task',
+				cell: WorkedOnTaskCell
+			},
+			{
+				id: 'estimate',
+				header: 'Estimate',
+				cell: TaskEstimateInfoCell
+			},
+			{
+				id: 'action',
+				header: 'Action',
+				cell: ActionMenuCell,
+				meta: {
+					active
+				}
+			}
+		],
+		[]
+	);
+
+	const footerRows = React.useMemo<React.ReactNode[]>(() => [<Invite key={0} />], []);
+
+	return (
+		<DataTable
+			columns={columns as Column<OT_Member>[]}
+			data={teamMembers}
+			footerRows={footerRows}
+			noResultsMessage={{
+				heading: 'No team members found',
+				content: 'Try adjusting your search or filter to find what you’re looking for.'
+			}}
+		/>
+	);
+};
+
+function Invite() {
+	const { user } = useAuthenticateUser();
+	const { openModal, isOpen, closeModal } = useModal();
+
+	return (
+		<>
+			<InviteUserTeamCard active={user?.isEmailVerified} onClick={openModal} />
+			<InviteFormModal open={isOpen && !!user?.isEmailVerified} closeModal={closeModal} />
+		</>
+	);
+}
+
+export default TeamMembersTableView;
