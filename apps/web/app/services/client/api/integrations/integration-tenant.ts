995ed2efd4237e3e19c63a92aaa7d42774caa363
Badal Khatri <badal.khatri0924@gmail.com>
2023-11-08 15:57:58 +0530

    Added Delete Integration, WIP multiple API calling issue

--- apps/web/app/services/client/api/integrations/integration-tenant.ts
+++ apps/web/app/services/client/api/integrations/integration-tenant.ts
@@ -1,8 +1,12 @@
-import { IIntegrationTenant, PaginationResponse, CreateResponse } from '@app/interfaces';
+import { IIntegrationTenant, PaginationResponse, CreateResponse, DeleteResponse } from '@app/interfaces';
 import api from '../../axios';
 
 export function getIntegrationTenantAPI(name: string) {
 	return api.get<CreateResponse<PaginationResponse<IIntegrationTenant>>>(
 		`/integration-tenant/remember/state?name=${name}`
 	);
+}
+
+export function deleteIntegrationTenantAPI(integrationId: string) {
+	return api.delete<DeleteResponse>(`/integration-tenant/${integrationId}`);
 }
