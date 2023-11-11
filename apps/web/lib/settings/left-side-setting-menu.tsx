cc6f7262f9a93cf2eb050d42b757de18aaf0b1da
Badal Khatri <badal.khatri0924@gmail.com>
2023-11-11 14:33:16 +0530

    fix: Navigation Issue

--- apps/web/lib/settings/left-side-setting-menu.tsx
+++ apps/web/lib/settings/left-side-setting-menu.tsx
@@ -41,7 +41,6 @@ export const LeftSideSettingMenu = () => {
 
 	const onLinkClick = useCallback(
 		(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-			// if(router.pathname )
 			const url = new URL(e.currentTarget.href);
 			if (url.pathname !== router.pathname) {
 				return;
@@ -94,7 +93,7 @@ export const LeftSideSettingMenu = () => {
 						<div className="flex flex-col">
 							{PersonalAccordianData.map((ad, index) => {
 								return (
-									<Link onClick={onLinkClick} href={`/settings/personal${ad.href}`} key={index}>
+									<a onClick={onLinkClick} href={`/settings/personal${ad.href}`} key={index}>
 										<Text
 											className={`text-[${ad.color}] text-lg font-normal flex items-center p-4 pr-1 pl-5`}
 											key={index}
@@ -102,7 +101,7 @@ export const LeftSideSettingMenu = () => {
 										>
 											{ad.title}
 										</Text>
-									</Link>
+									</a>
 								);
 							})}
 						</div>
@@ -138,7 +137,7 @@ export const LeftSideSettingMenu = () => {
 							{TeamAccordianData.filter((ad) => (!isTeamManager && !ad.managerOnly) || isTeamManager).map(
 								(ad, index) => {
 									return (
-										<Link onClick={onLinkClick} href={`/settings/team${ad.href}`} key={index}>
+										<a onClick={onLinkClick} href={`/settings/team${ad.href}`} key={index}>
 											<Text
 												className={`text-[${ad.color}] text-lg font-normal flex items-center p-4 pr-1 pl-5`}
 												key={index}
@@ -146,7 +145,7 @@ export const LeftSideSettingMenu = () => {
 											>
 												{ad.title}
 											</Text>
-										</Link>
+										</a>
 									);
 								}
 							)}
