f77d6e6a4771de2233a0753e97a9dd4981e5c310
desperado1802 <mumun_ali@hotmail.com>
2023-11-11 16:44:21 +0200

    added some styling and improvements

--- apps/mobile/app/components/Task/DescrptionBlock/index.tsx
+++ apps/mobile/app/components/Task/DescrptionBlock/index.tsx
@@ -1,14 +1,15 @@
 /* eslint-disable react-native/no-inline-styles  */
 /* eslint-disable react-native/no-color-literals  */
 import { View, StyleSheet, ScrollView } from "react-native"
-import React from "react"
+import React, { RefObject } from "react"
 import Accordion from "../../Accordion"
 import QuillEditor, { QuillToolbar } from "react-native-cn-quill"
 import { useStores } from "../../../models"
 import { useAppTheme } from "../../../theme"
 
 const DescriptionBlock = () => {
-	const _editor = React.useRef()
+	const _editor: RefObject<QuillEditor> = React.useRef()
+
 	const {
 		TaskStore: { detailedTask: task },
 	} = useStores()
@@ -17,41 +18,58 @@ const DescriptionBlock = () => {
 
 	return (
 		<Accordion title="Description">
-			<View style={{ paddingHorizontal: 12 }}>
+			<View style={{ paddingBottom: 12 }}>
 				<QuillEditor
 					style={styles.editor}
 					ref={_editor}
-					initialHtml={
-						task?.description
-							? task?.description
-							: "<p>Quill Editor for react-native</p>"
-					}
+					initialHtml={task?.description ? task?.description : ""}
+					quill={{
+						placeholder: "asd",
+						modules: {
+							toolbar: false,
+						},
+					}}
 				/>
 				<ScrollView>
-					<QuillToolbar
-						editor={_editor}
-						styles={{
-							toolbar: {
-								provider: (provided) => ({
+					<View style={{ paddingHorizontal: 12 }}>
+						<View
+							style={{
+								width: "100%",
+								borderTopWidth: 1,
+								borderTopColor: "#F2F2F2",
+								marginBottom: 12,
+							}}
+						/>
+						<QuillToolbar
+							editor={_editor}
+							styles={{
+								toolbar: {
+									provider: (provided) => ({
+										...provided,
+										borderTopWidth: 0,
+										borderLeftWidth: 0,
+										borderRightWidth: 0,
+										borderBottomWidth: 0,
+
+										// backgroundColor: colors.background,
+									}),
+									root: (provided) => ({
+										...provided,
+										backgroundColor: colors.background,
+										width: "100%",
+										// height: "100%",
+									}),
+								},
+								separator: (provided) => ({
 									...provided,
-									borderTopWidth: 0,
-									borderLeftWidth: 0,
-									borderRightWidth: 0,
-									borderBottomWidth: 0,
+									width: "50%",
+									borderTopWidth: 5,
 								}),
-								root: (provided) => ({
-									...provided,
-									backgroundColor: colors.secondary2,
-								}),
-							},
-							separator: (provided) => ({
-								...provided,
-								display: "none",
-							}),
-						}}
-						options="full"
-						theme="light"
-					/>
+							}}
+							options="full"
+							theme="light"
+						/>
+					</View>
 				</ScrollView>
 			</View>
 		</Accordion>
