ea92da5a924c7d7c03c1495fe8849166049fadb0
Ruslan K <evereq@gmail.com>
2023-11-14 12:20:26 +0100

    Update sentry.client.config.ts

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -4,7 +4,7 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DNS = process.env.SENTRY_DNS || process.env.NEXT_PUBLIC_SENTRY_DNS;
+const SENTRY_DNS = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 if (SENTRY_DSN) {
 	Sentry.init({
