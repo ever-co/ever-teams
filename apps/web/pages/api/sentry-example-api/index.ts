147a46b827323e4a261775a05a14e967f824e20a
Ruslan K <evereq@gmail.com>
2023-11-14 11:16:46 +0100

    Delete apps/web/pages/api/sentry-example-api/index.ts

--- apps/web/pages/api/sentry-example-api/index.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	try {
-		throw new Error('Sentry Example API Route Error');
-	} catch (error) {
-		return res.status(500).json({ error: (error as Error)?.message });
-	}
-}
