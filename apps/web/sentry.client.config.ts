594ffd25933b62f3dc9602fb3b037713c34b16a2
Ruslan Konviser <evereq@gmail.com>
2023-11-14 12:35:26 +0100

    fix: DNS -> DSN

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -4,23 +4,23 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DNS = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 if (SENTRY_DSN) {
 	Sentry.init({
-		dsn: SENTRY_DNS,
+		dsn: SENTRY_DSN,
 		// Adjust this value in production, or use tracesSampler for greater control
 		tracesSampleRate: 1,
-	
-		// Setting this option to true will print useful information to the console while you're setting up Sentry.		
+
+		// Setting this option to true will print useful information to the console while you're setting up Sentry.
 		debug: process.env.NEXT_PUBLIC_SENTRY_DEBUG && process.env.NEXT_PUBLIC_SENTRY_DEBUG === 'true' ? true : false,
-	
+
 		replaysOnErrorSampleRate: 1.0,
-	
+
 		// This sets the sample rate to be 10%. You may want this to be 100% while
 		// in development and sample at a lower rate in production
 		replaysSessionSampleRate: 0.1,
-	
+
 		// Replay may only be enabled for the client-side
 		// You can remove this option if you're not planning to use the Sentry Session Replay feature:
 		integrations: [
--- apps/web/sentry.edge.config.ts
+++ apps/web/sentry.edge.config.ts
@@ -5,16 +5,15 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DNS = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-if (SENTRY_DNS) 
-{
+if (SENTRY_DSN) {
 	Sentry.init({
 		dsn: SENTRY_DSN,
-	
+
 		// Adjust this value in production, or use tracesSampler for greater control
 		tracesSampleRate: 1,
-	
+
 		// Setting this option to true will print useful information to the console while you're setting up Sentry.
 		debug: process.env.NEXT_PUBLIC_SENTRY_DEBUG && process.env.NEXT_PUBLIC_SENTRY_DEBUG === 'true' ? true : false
 	});
