200be3c16a2181a9af19cc662dae5165e4ad2f6d
Ruslan K <evereq@gmail.com>
2023-11-14 12:20:49 +0100

    Update sentry.edge.config.ts

--- apps/web/sentry.edge.config.ts
+++ apps/web/sentry.edge.config.ts
@@ -5,12 +5,12 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DNS = process.env.SENTRY_DNS || process.env.NEXT_PUBLIC_SENTRY_DNS;
+const SENTRY_DNS = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 if (SENTRY_DNS) 
 {
 	Sentry.init({
-		dsn: SENTRY_DNS,
+		dsn: SENTRY_DSN,
 	
 		// Adjust this value in production, or use tracesSampler for greater control
 		tracesSampleRate: 1,
