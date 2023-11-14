05e0d150d48e0cf486e1f237a5fb180baa589d9b
desperado1802 <mumun_ali@hotmail.com>
2023-11-14 17:00:43 +0200

    rendering the versions

--- apps/mobile/app/screens/Authenticated/TaskVersionScreen/index.tsx
+++ apps/mobile/app/screens/Authenticated/TaskVersionScreen/index.tsx
@@ -1,20 +1,47 @@
 /* eslint-disable react-native/no-color-literals */
 /* eslint-disable react-native/no-inline-styles */
-import React, { FC } from "react"
-import { View, Text, ViewStyle, TouchableOpacity, StyleSheet } from "react-native"
-import { AntDesign } from "@expo/vector-icons"
+import React, { FC, useRef, useState } from "react"
+import {
+	View,
+	Text,
+	ViewStyle,
+	TouchableOpacity,
+	StyleSheet,
+	ActivityIndicator,
+	FlatList,
+} from "react-native"
+import { AntDesign, Ionicons } from "@expo/vector-icons"
 import { Screen } from "../../../components"
 import { AuthenticatedDrawerScreenProps } from "../../../navigators/AuthenticatedNavigator"
 import { translate } from "../../../i18n"
-
 import { typography, useAppTheme } from "../../../theme"
 
 import Animated from "react-native-reanimated"
+import { useTaskVersion } from "../../../services/hooks/features/useTaskVersion"
+import { ITaskVersionItemList } from "../../../services/interfaces/ITaskVersion"
+import { BlurView } from "expo-blur"
+import VersionItem from "./components/VersionItem"
 
 export const TaskVersionScreen: FC<AuthenticatedDrawerScreenProps<"TaskVersion">> =
 	function AuthenticatedDrawerScreen(_props) {
-		const { colors } = useAppTheme()
+		const { colors, dark } = useAppTheme()
 		const { navigation } = _props
+
+		const { isLoading, versions, deleteTaskVersion, updateTaskVersion, createTaskVersion } =
+			useTaskVersion()
+
+		const [editMode, setEditMode] = useState(false)
+		const [itemToEdit, setItemToEdit] = useState<ITaskVersionItemList>(null)
+		const [isSheetOpen, setIsSheetOpen] = useState<boolean>(false)
+
+		const sheetRef = useRef(null)
+
+		const openForEdit = (item: ITaskVersionItemList) => {
+			setEditMode(true)
+			setIsSheetOpen(true)
+			setItemToEdit(item)
+			sheetRef.current.snapTo(0)
+		}
 
 		return (
 			<Screen
@@ -28,11 +55,76 @@ export const TaskVersionScreen: FC<AuthenticatedDrawerScreenProps<"TaskVersion">
 								<AntDesign name="arrowleft" size={24} color={colors.primary} />
 							</TouchableOpacity>
 							<Text style={[styles.title, { color: colors.primary }]}>
-								{translate("settingScreen.statusScreen.mainTitle")}
+								Task Versions
 							</Text>
 						</View>
 					</View>
+
+					<View style={{ width: "100%", padding: 20, height: "80%" }}>
+						<View>
+							<Text style={styles.title2}>List of Versions</Text>
+						</View>
+						<View
+							style={{
+								minHeight: 200,
+								justifyContent: "center",
+								alignItems: "center",
+							}}
+						>
+							{isLoading ? (
+								<ActivityIndicator size={"small"} color={"#3826A6"} />
+							) : null}
+							{!isLoading && versions?.total === 0 ? (
+								<Text style={{ ...styles.noStatusTxt, color: colors.primary }}>
+									{translate("settingScreen.statusScreen.noActiveStatuses")}
+								</Text>
+							) : null}
+
+							<FlatList
+								bounces={false}
+								showsVerticalScrollIndicator={false}
+								style={{ width: "100%" }}
+								data={versions?.items}
+								renderItem={({ item }) => (
+									<VersionItem
+										openForEdit={() => openForEdit(item)}
+										onDeleteTask={() => deleteTaskVersion(item.id)}
+										version={item}
+									/>
+								)}
+								keyExtractor={(_, index) => index.toString()}
+								ListFooterComponent={() => <View style={{ marginBottom: 40 }} />}
+							/>
+						</View>
+					</View>
+					<TouchableOpacity
+						style={{
+							...styles.createButton,
+							borderColor: dark ? "#6755C9" : "#3826A6",
+						}}
+						onPress={() => {
+							setEditMode(false)
+							setIsSheetOpen(true)
+							sheetRef.current.snapTo(0)
+						}}
+					>
+						<Ionicons name="add" size={24} color={dark ? "#6755C9" : "#3826A6"} />
+						<Text style={{ ...styles.btnText, color: dark ? "#6755C9" : "#3826A6" }}>
+							Create new version
+						</Text>
+					</TouchableOpacity>
 				</Animated.View>
+				{isSheetOpen && (
+					<BlurView
+						intensity={15}
+						tint="dark"
+						style={{
+							position: "absolute",
+							width: "100%",
+							height: "100%",
+						}}
+					/>
+				)}
 			</Screen>
 		)
 	}
@@ -55,17 +147,45 @@ const $headerContainer: ViewStyle = {
 }
 
 const styles = StyleSheet.create({
+	btnText: {
+		color: "#3826A6",
+		fontFamily: typography.primary.semiBold,
+		fontSize: 18,
+		fontStyle: "normal",
+	},
 	container: {
 		alignItems: "center",
 		flexDirection: "row",
 		width: "100%",
 	},
-
+	createButton: {
+		alignItems: "center",
+		alignSelf: "center",
+		borderColor: "#3826A6",
+		borderRadius: 12,
+		borderWidth: 2,
+		flexDirection: "row",
+		justifyContent: "center",
+		marginTop: 24,
+		padding: 16,
+		width: "90%",
+	},
+	noStatusTxt: {
+		color: "#7E7991",
+		fontFamily: typography.primary.semiBold,
+		fontSize: 16,
+	},
 	title: {
 		alignSelf: "center",
 		fontFamily: typography.primary.semiBold,
 		fontSize: 16,
 		textAlign: "center",
 		width: "80%",
+	},
+	title2: {
+		color: "#7E7991",
+		fontFamily: typography.primary.semiBold,
+		fontSize: 16,
+		marginBottom: 8,
 	},
 })
