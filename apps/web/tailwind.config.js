1b16345f0f751b3ee672b9cd8040d321c964b22f
maceteligolden <maceteligolden@gmail.com>
2023-11-21 23:55:23 +0100

    feat:add color to header

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -33,6 +33,7 @@ module.exports = {
 				transparent: 'transparent',
 				current: 'currentColor',
 				neutral: '#7E7991',
+				transparentWhite: 'rgba(255, 255, 255, 0.30)',
 				default: {
 					DEFAULT: '#282048'
 				},
