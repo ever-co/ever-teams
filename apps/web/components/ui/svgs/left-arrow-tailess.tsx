bb9abee157d6776b9e4f7092f4049cfb05005d50
maceteligolden <maceteligolden@gmail.com>
2023-11-21 23:55:35 +0100

    feat:add tailess left arrow arrow

---
+++ apps/web/components/ui/svgs/left-arrow-tailess.tsx
@@ -0,0 +1,7 @@
+export default function LeftArrowTailessIcon() {
+    return (
+        <>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14 7l-5 5m0 0l5 5"/></svg>
+        </>
+    )
+}
