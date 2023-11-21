9a2d98a22b5f9a542b480058e7a3415020e47013
Ndekocode <arickbulakali@gmail.com>
2023-11-21 17:48:18 +0200

    refactor(web): [danger-zone] change the text button content and "add delete all data" button

--- apps/web/lib/settings/danger-zone-personal.tsx
+++ apps/web/lib/settings/danger-zone-personal.tsx
@@ -4,21 +4,40 @@ import { Button, Text } from 'lib/components';
 import { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { RemoveModal } from './remove-modal';
+type RemoveModalType = 'REMOVE' | 'DELETE' | 'DELETE_ALL' | string;
+type ActionFunction = () => void;
 
 export const DangerZone = () => {
 	const { t } = useTranslation();
 	const { isOpen, closeModal, openModal } = useModal();
-	const [removeModalType, setRemoveModalType] = useState<'REMOVE' | 'DELETE' | null>(null);
+	const [removeModalType, setRemoveModalType] = useState<RemoveModalType>('');
 
 	const { deleteUser, deleteUserLoading } = useUser();
 	const { user } = useAuthenticateUser();
-	const { removeUserFromAllTeam, removeUserFromAllTeamLoading } = useOrganizationTeams();
 
+	const { removeUserFromAllTeam, removeUserFromAllTeamLoading } = useOrganizationTeams();
 	const handleRemoveUser = useCallback(() => {
 		if (user) {
 			return removeUserFromAllTeam(user.id);
 		}
 	}, [user, removeUserFromAllTeam]);
+
+	const handleDeleteAllUserData = useCallback(() => {
+		console.log(user);
+	}, [user]);
+
+	const actionTypes: Record<RemoveModalType, ActionFunction> = {
+		DELETE: deleteUser,
+		DELETE_ALL: handleDeleteAllUserData,
+		REMOVE: handleRemoveUser
+	};
+	const modalMessages: Record<RemoveModalType, string> = {
+		REMOVE: t('pages.settingsPersonal.ABOUT_TO_REMOVE_FROM_ALL_TEAMS'),
+		DELETE: t('pages.settingsPersonal.ABOUT_TO_DELETE_ACCOUNT'),
+		DELETE_ALL: t('pages.settingsPersonal.ABOUT_TO_DELETE_ALL_ACCOUNT_DATA')
+	};
+	const messageToShow = modalMessages[removeModalType] || '';
+	const onAction = actionTypes[removeModalType] || handleRemoveUser;
 
 	return (
 		<>
@@ -67,18 +86,35 @@ export const DangerZone = () => {
 								</Button>
 							</div>
 						</div>
+						<div className="flex flex-col items-center justify-between w-full gap-6 mt-5 sm:flex-row">
+							<div className="flex-auto w-64"></div>
+							<div className="flex-auto sm:w-64">
+								<Text className="font-normal text-center text-gray-400 text-md sm:text-left">
+									{t('alerts.ALERT_REMOVE_ALL_DATA')}
+								</Text>
+							</div>
+							<div className="flex-auto w-32">
+								<Button
+									variant="danger"
+									type="submit"
+									className="float-right w-full bg-[#DE5536]"
+									onClick={() => {
+										setRemoveModalType('DELETE_ALL');
+										openModal();
+									}}
+								>
+									{t('common.DELETE_ALL_DATA')}
+								</Button>
+							</div>
+						</div>
 					</div>
 				</div>
 
 				<RemoveModal
 					open={removeModalType && isOpen ? true : false}
 					close={closeModal}
-					title={
-						removeModalType === 'DELETE'
-							? t('pages.settingsPersonal.ABOUT_TO_DELETE_ACCOUNT')
-							: t('pages.settingsPersonal.ABOUT_TO_REMOVE_ACCOUNT')
-					}
-					onAction={removeModalType === 'DELETE' ? deleteUser : handleRemoveUser}
+					title={messageToShow}
+					onAction={onAction}
 					loading={deleteUserLoading || removeUserFromAllTeamLoading}
 				/>
 			</div>
