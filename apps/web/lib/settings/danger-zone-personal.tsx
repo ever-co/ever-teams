c9108750cce1fa7062074eff005eb28f1123698d
Ndekocode <arickbulakali@gmail.com>
2023-11-21 18:33:20 +0200

    refactor(web): [danger-zone] add image description for danger-zone

--- apps/web/lib/settings/danger-zone-personal.tsx
+++ apps/web/lib/settings/danger-zone-personal.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 import { useAuthenticateUser, useModal, useOrganizationTeams, useUser } from '@app/hooks';
 import { Button, Text } from 'lib/components';
+import Image from 'next/image';
 import { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import dangerZoneImage from '../../public/assets/svg/danger-zones.svg';
 import { RemoveModal } from './remove-modal';
 type RemoveModalType = 'REMOVE' | 'DELETE' | 'DELETE_ALL' | string;
 type ActionFunction = () => void;
@@ -44,67 +46,71 @@ export const DangerZone = () => {
 			<div className="flex flex-col items-center justify-between">
 				<div className="w-full mt-5">
 					<div className="">
-						<div className="flex flex-col items-center justify-between w-full gap-6 sm:flex-row">
-							<div className="flex-auto sm:w-64"></div>
-							<div className="flex-auto sm:w-64">
-								<Text className="font-normal text-gray-400 text-md">
-									{t('alerts.ALERT_DELETE_ACCOUNT')}
-								</Text>
+						<div className="flex items-center justify-between w-full gap-6">
+							<div className="flex items-center justify-center flex-auto opacity-50 sm:w-32">
+								<Image alt="Danger zone" src={dangerZoneImage} width={150} height={150} />
 							</div>
-							<div className="flex-auto w-32">
-								<Button
-									variant="danger"
-									type="submit"
-									className="float-right w-full !bg-[#FF9B50]"
-									onClick={() => {
-										setRemoveModalType('REMOVE');
-										openModal();
-									}}
-								>
-									{t('common.REMOVE_EVERYWHERE')}
-								</Button>
-							</div>
-						</div>
-						<div className="flex flex-col items-center justify-between w-full gap-6 mt-5 sm:flex-row">
-							<div className="flex-auto w-64"></div>
-							<div className="flex-auto sm:w-64">
-								<Text className="font-normal text-center text-gray-400 text-md sm:text-left">
-									{t('alerts.ALERT_ACCOUNT_PERMANENT_DELETE')}
-								</Text>
-							</div>
-							<div className="flex-auto w-32">
-								<Button
-									variant="danger"
-									type="submit"
-									className="float-right w-full bg-[#DE5536]"
-									onClick={() => {
-										setRemoveModalType('DELETE');
-										openModal();
-									}}
-								>
-									{t('common.DELETE_ACCOUNT')}
-								</Button>
-							</div>
-						</div>
-						<div className="flex flex-col items-center justify-between w-full gap-6 mt-5 sm:flex-row">
-							<div className="flex-auto w-64"></div>
-							<div className="flex-auto sm:w-64">
-								<Text className="font-normal text-center text-gray-400 text-md sm:text-left">
-									{t('alerts.ALERT_REMOVE_ALL_DATA')}
-								</Text>
-							</div>
-							<div className="flex-auto w-32">
-								<Button
-									variant="danger"
-									type="submit"
-									className="float-right w-full bg-[#DE5536]"
-									onClick={() => {
-										setRemoveModalType('DELETE_ALL');
-										openModal();
-									}}
-								>
-									{t('common.DELETE_ALL_DATA')}
-								</Button>
+							<div className="flex-auto sm:w-96">
+								<div className="flex flex-col items-center justify-between w-full gap-6 sm:flex-row">
+									<div className="flex-auto sm:w-64">
+										<Text className="font-normal text-gray-400 text-md">
+											{t('alerts.ALERT_DELETE_ACCOUNT')}
+										</Text>
+									</div>
+									<div className="flex-auto w-32">
+										<Button
+											variant="danger"
+											type="submit"
+											className="float-right w-full !bg-[#FF9B50]"
+											onClick={() => {
+												setRemoveModalType('REMOVE');
+												openModal();
+											}}
+										>
+											{t('common.REMOVE_EVERYWHERE')}
+										</Button>
+									</div>
+								</div>
+								<div className="flex flex-col items-center justify-between w-full gap-6 mt-5 sm:flex-row">
+									<div className="flex-auto sm:w-64">
+										<Text className="font-normal text-center text-gray-400 text-md sm:text-left">
+											{t('alerts.ALERT_ACCOUNT_PERMANENT_DELETE')}
+										</Text>
+									</div>
+									<div className="flex-auto w-32">
+										<Button
+											variant="danger"
+											type="submit"
+											className="float-right w-full bg-[#DE5536]"
+											onClick={() => {
+												setRemoveModalType('DELETE');
+												openModal();
+											}}
+										>
+											{t('common.DELETE_ACCOUNT')}
+										</Button>
+									</div>
+								</div>
+								<div className="flex flex-col items-center justify-between w-full gap-6 mt-5 sm:flex-row">
+									<div className="flex-auto sm:w-64">
+										<Text className="font-normal text-center text-gray-400 text-md sm:text-left">
+											{t('alerts.ALERT_REMOVE_ALL_DATA')}
+										</Text>
+									</div>
+									<div className="flex-auto w-32">
+										<Button
+											variant="danger"
+											type="submit"
+											className="float-right w-full bg-[#DE5536]"
+											onClick={() => {
+												setRemoveModalType('DELETE_ALL');
+												openModal();
+											}}
+										>
+											{t('common.DELETE_ALL_DATA')}
+										</Button>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
