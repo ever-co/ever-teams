8ad78bc83db0306720972a0194b75ce5c7b45e9f
maceteligolden <maceteligolden@gmail.com>
2023-11-27 15:26:01 +0100

    feat:add priority  icon

---
+++ apps/web/components/ui/svgs/priority-icon.tsx
@@ -0,0 +1,9 @@
+export default function PriorityIcon() {
+    return (
+        <>
+            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 12 12" fill="none">
+                <path d="M9.95906 7.5249L6.69906 4.2649C6.31406 3.8799 5.68406 3.8799 5.29906 4.2649L2.03906 7.5249" stroke="#EE6C4D" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </>
+    )
+}
