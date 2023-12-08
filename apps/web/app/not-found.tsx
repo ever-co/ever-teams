0fc2f80bfa0486e36eebb77301668005a5095ec9
Badal Khatri <badal.khatri0924@gmail.com>
2023-12-08 19:03:10 +0530

    Minor Fixes

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -1,19 +1,12 @@
-"use client"
+'use client';
 import NotFound from '@components/pages/404';
 import { AuthLayout } from 'lib/layout';
-import { useTranslation } from 'react-i18next';
 
-
-const NotFoundPage =  () => {
-	
-	const { t } = useTranslation();
-
+const NotFoundPage = () => {
+	// TODO:
+	// Fix localisation issue
 	return (
-	
-		<AuthLayout 
-			title={t('pages.page404.HEADING_TITLE')} 
-			isAuthPage={false}
-			>
+		<AuthLayout title={'Page not found !'} isAuthPage={false}>
 			<NotFound />
 		</AuthLayout>
 	);
--- apps/web/components/pages/404/index.tsx
+++ apps/web/components/pages/404/index.tsx
@@ -1,28 +1,25 @@
-"use client"
+'use client';
 import SadCry from '@components/ui/svgs/sad-cry';
 import { Button, Text } from 'lib/components';
 import Link from 'next/link';
-import { useTranslation } from 'react-i18next';
+
 function NotFound() {
-	const { t } = useTranslation();
 	return (
 		<div className="mt-28 flex flex-col gap-7 items-center">
 			<div className="m-auto relative flex justify-center items-center gap-4 text-center ">
-				<SadCry width={97} height={97}/>
+				<SadCry width={97} height={97} />
 				<Text className="text-[78px] font-semibold text-chetwodeBlue">404!</Text>
 			</div>
 
-				<Text className="text-[40px] font-bold text-center text-[#282048] dark:text-light--theme">
-					{t('pages.page404.HEADING_TITLE')}
-				</Text>
+			<Text className="text-[40px] font-bold text-center text-[#282048] dark:text-light--theme">
+				Page not found !
+			</Text>
 			<div className="flex flex-col gap-4">
 				<Text className="text-[20px] font-normal text-center text-gray-400">
-					{t('pages.page404.HEADING_DESCRIPTION')}
+					{`We looked, but can't find it ....`}
 				</Text>
 				<Link href="/">
-					<Button className="m-auto mt-10 font-normal rounded-lg pl-7 pr-7">
-						{t('pages.page404.LINK_LABEL')}
-					</Button>
+					<Button className="m-auto mt-10 font-normal rounded-lg pl-7 pr-7">Home</Button>
 				</Link>
 			</div>
 		</div>
