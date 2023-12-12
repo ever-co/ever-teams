d09c59adc323c56a4fcd2b7c90035da7933401e0
maceteligolden <maceteligolden@gmail.com>
2023-12-12 14:37:47 +0100

    feat:add kanban icon

---
+++ apps/web/components/ui/svgs/kanaban.tsx
@@ -0,0 +1,9 @@
+export default function KanbanIcon({
+    className
+}: {
+    className?: string
+}) {
+    return (
+        <svg className={className} xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15"><path fill="currentColor" d="M0 1h7V0H0zm8 0h7V0H8zM.5 3a.5.5 0 0 0-.5.5v11a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5v-11a.5.5 0 0 0-.5-.5zm8 0a.5.5 0 0 0-.5.5v6a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5v-6a.5.5 0 0 0-.5-.5z"/></svg>
+    )
+}
