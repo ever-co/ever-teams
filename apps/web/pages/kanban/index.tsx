1b5a7e6f24e5febb54c93416a8ba82d082224dfb
maceteligolden <maceteligolden@gmail.com>
2023-12-13 18:51:10 +0100

    feat:add kanban header

--- apps/web/pages/kanban/index.tsx
+++ apps/web/pages/kanban/index.tsx
@@ -1,8 +1,10 @@
 import { useKanban } from "@app/hooks/features/useKanban";
 import KanbanBoardSkeleton from "@components/shared/skeleton/KanbanBoardSkeleton";
+import VerticalLine from "@components/ui/svgs/vertificalline";
 import { withAuthentication } from "lib/app/authenticator";
+import { Breadcrumb } from "lib/components";
 import { KanbanView } from "lib/features/team-members-kanban-view"
-import { MainLayout } from "lib/layout";
+import { MainHeader, MainLayout } from "lib/layout";
 
 const Kanban= () => {
   
@@ -11,6 +13,21 @@ const Kanban= () => {
     return (
         <>
         <MainLayout>
+            <MainHeader>
+				<Breadcrumb paths={['Dashboard', 'Team Page']} className="text-sm" />
+                <div className="flex flex-row items-center justify-between mt-[24px]">
+                    <div>
+                        <h1 className="text-[35px] font-bold text-[#282048] dark:text-white">Kanban Board</h1>
+                    </div>
+                    <div className="flex flex-row items-center gap-[12px]">
+                        <p>08:00 ( UTC +04:30 )</p>
+                        <VerticalLine/>
+                        <p>08:00 ( UTC +04:30 )</p>
+                        <VerticalLine/>
+                        <p>08:00 ( UTC +04:30 )</p>
+                    </div>
+                </div>
+			</MainHeader>
            {Object.keys(data).length > 0 ? 
             <KanbanView kanbanBoardTasks={data}/>
             :
