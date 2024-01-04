3cc6fb14d0da005fc92bff1f1656a11301622a11
maceteligolden <maceteligolden@gmail.com>
2024-01-04 17:25:41 +0100

    fix:add skeleton to imageoverlapper

--- apps/web/lib/components/image-overlapper.tsx
+++ apps/web/lib/components/image-overlapper.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Tooltip } from 'lib/components';
+import Skeleton from "react-loading-skeleton";
 
 export interface IImageOverlapper {
     id: string;
@@ -33,37 +34,41 @@ export default function ImageOverlapper({
 
     return (
         <div className="relative ">
-            <div className="flex h-fit flex-row justify-end items-center relative"
-                style={{
-                    width: `${totalLength}px`
-                }}
-            >
-                {images.map((image: IImageOverlapper, index: number) => {
-                    if (index < numberOfImagesDisplayed) {
-                    return (
-                            <Link href={`/profile/${image.id}`} className="relative w-[40px] h-[40px]" key={index}>
-                                <Tooltip label={image.alt} placement="bottom-end">
-                                    <Image
-                                        src={image.url}
-                                        alt={`${image.alt} avatar`}
-                                        fill={true}
-                                        className="absolute rounded-full border-2 border-white"
-                                        style={stackImages(index, imageLength)}
-                                    />
-                                </Tooltip>
-                            </Link>
-                    );
-                    }
-                })}
-                {images.length > numberOfImagesDisplayed && (
-                    <div
-                        className="flex flex-row text-sm text-[#282048] dark:text-white font-semibold items-center justify-center absolute h-[40px] w-[40px] rounded-full border-2 border-[#0000001a] dark:border-white bg-white dark:bg-[#191A20]"
-                        style={stackImages(numberOfImagesDisplayed, imageLength)}
-                    >
-                        {(imageLength - numberOfImagesDisplayed) < 100 ? (imageLength - numberOfImagesDisplayed) : 99}+
-                    </div>
-                )}
-            </div>
+            {imageLength > 0 ?
+                <div className="flex h-fit flex-row justify-end items-center relative"
+                    style={{
+                        width: `${totalLength}px`
+                    }}
+                >
+                    {images.map((image: IImageOverlapper, index: number) => {
+                        if (index < numberOfImagesDisplayed) {
+                        return (
+                                <Link href={`/profile/${image.id}`} className="relative w-[40px] h-[40px]" key={index}>
+                                    <Tooltip label={image.alt} placement="bottom-end">
+                                        <Image
+                                            src={image.url}
+                                            alt={`${image.alt} avatar`}
+                                            fill={true}
+                                            className="absolute rounded-full border-2 border-white"
+                                            style={stackImages(index, imageLength)}
+                                        />
+                                    </Tooltip>
+                                </Link>
+                        );
+                        }
+                    })}
+                    {images.length > numberOfImagesDisplayed && (
+                        <div
+                            className="flex flex-row text-sm text-[#282048] dark:text-white font-semibold items-center justify-center absolute h-[40px] w-[40px] rounded-full border-2 border-[#0000001a] dark:border-white bg-white dark:bg-[#191A20]"
+                            style={stackImages(numberOfImagesDisplayed, imageLength)}
+                        >
+                            {(imageLength - numberOfImagesDisplayed) < 100 ? (imageLength - numberOfImagesDisplayed) : 99}+
+                        </div>
+                    )}
+                </div>
+            : 
+                <Skeleton height={40} width={40} borderRadius={100} className="rounded-full dark:bg-[#353741]" />
+            }
 		</div>
     )
 }
