db7d92c03230869421393b172c8f5229c7b24533
cedric karungu <ckarungu921@gmail.com>
2024-01-06 22:23:54 +0200

    feat: move organisation-team-join route to app

---
+++ apps/web/app/api/organization-team-join/[id]/[action]/route.ts
@@ -0,0 +1,25 @@
+import { IRequestToJoinActionEnum } from '@app/interfaces';
+import { authenticatedGuard } from '@app/services/server/guards/authenticated-guard-app';
+import { acceptRejectRequestToJoinRequest } from '@app/services/server/requests';
+import { NextResponse } from 'next/server';
+
+export async function PUT(req: Request) {
+	const res = new NextResponse();
+	const { $res, user, access_token, tenantId } = await authenticatedGuard(req, res);
+	if (!user) return $res('unauthorized');
+
+	const { searchParams } = new URL(req.url);
+
+	const { id, action } = searchParams as unknown as { id: string; action: string };
+
+	if (id) {
+		return $res(
+			await acceptRejectRequestToJoinRequest({
+				id: id as string,
+				bearer_token: access_token,
+				tenantId,
+				action: action as IRequestToJoinActionEnum
+			})
+		);
+	}
+}
---
+++ apps/web/app/api/organization-team-join/route.ts
@@ -0,0 +1,21 @@
+import { IRequestToJoinCreate } from '@app/interfaces';
+import { authenticatedGuard } from '@app/services/server/guards/authenticated-guard-app';
+import { getRequestToJoinRequest, requestToJoinRequest } from '@app/services/server/requests';
+import { NextResponse } from 'next/server';
+
+export async function GET(req: Request) {
+	const res = new NextResponse();
+	const { access_token, tenantId, organizationId } = await authenticatedGuard(req, res);
+
+	const requestToJoinData = await getRequestToJoinRequest({
+		bearer_token: access_token,
+		tenantId,
+		organizationId
+	});
+	return NextResponse.json(requestToJoinData.data);
+}
+
+export async function POST(req: Request) {
+	const body = (await req.json()) as IRequestToJoinCreate;
+	return NextResponse.json(await requestToJoinRequest(body));
+}
