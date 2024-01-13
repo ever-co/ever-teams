e01f1874d5a0f32091f4a7828283989ecae20fb9
cedric karungu <ckarungu921@gmail.com>
2024-01-13 13:01:22 +0200

    feat: use translation files for Texts and Titles

--- apps/web/lib/features/activity/components/screenshot-details.tsx
+++ apps/web/lib/features/activity/components/screenshot-details.tsx
@@ -64,32 +64,32 @@ const ScreenshotDetailsModal = ({
 					))}
 				</div>
 				<div>
-					<h4 className="text-lg font-semibold">Other details</h4>
+					<h4 className="text-lg font-semibold">{t('timer.OTHER_DETAILS')}</h4>
 					<div className="flex gap-2">
 						<p>
-							<span className="font-semibold mx-2">Keyboard</span>
+							<span className="font-semibold mx-2">{t('timer.KEYBOARD')}</span>
 							<span>
-								{slot.keyboard}times : {slot.keyboardPercentage}%
+								{t('timer.TIMES')} : {slot.keyboard} {slot.keyboardPercentage}%
 							</span>
 						</p>
 						<p>
-							<span className="font-semibold mx-2">Mouse</span>
+							<span className="font-semibold mx-2">{t('timer.MOUSE')}</span>
 							<span>
-								{slot.mouse}times : {slot.mousePercentage}%
+								{t('timer.TIMES')} : {slot.mouse} {slot.mousePercentage}%
 							</span>
 						</p>
 						<p className="rounded-lg px-1 mb-1 text-white ">
 							{slot.isActive ? (
-								<span className=" bg-green-600 rounded-lg px-2 m-1">Active</span>
+								<span className=" bg-green-600 rounded-lg px-2 m-1">{t('timer.ACTIVE')}</span>
 							) : (
-								<span className=" bg-red-600 rounded-lg px-2 m-1">Inactive</span>
+								<span className=" bg-red-600 rounded-lg px-2 m-1">{t('timer.INACTIVE')}</span>
 							)}
 						</p>
 						<p>
 							{slot.isArchived ? (
-								<span className=" bg-gray-600 rounded-lg px-2 m-1">Archived</span>
+								<span className=" bg-gray-600 rounded-lg px-2 m-1">{t('timer.ARCHIVED')}</span>
 							) : (
-								<span className=" bg-blue-600 rounded-lg px-2 m-1">Not archived</span>
+								<span className=" bg-blue-600 rounded-lg px-2 m-1">{t('timer.NOT_ARCHIVED')}</span>
 							)}
 						</p>
 					</div>
