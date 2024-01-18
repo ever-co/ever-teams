d25254a0237c47bc27b562df1673041e231ad352
Paradoxe Ngwasi <paradoxngwasi@outlook.com>
2024-01-18 07:22:23 +0000

    implement PUT organization-projects/setting/:id api on frontend

--- apps/web/app/services/client/api/organization-projects.ts
+++ apps/web/app/services/client/api/organization-projects.ts
@@ -1,11 +1,9 @@
 import { IProject } from '@app/interfaces';
-import api from '../axios';
+import api, { put } from '../axios';
 
 export function editOrganizationProjectSettingAPI(id: string, data: any, tenantId?: string) {
-	return api.put<any>(`/organization-projects/setting/${id}`, data, {
-		headers: {
-			'Tenant-Id': tenantId
-		}
+	return put<any>(`/organization-projects/setting/${id}`, data, {
+		tenantId
 	});
 }
 
