dce24f86b4eabbdd8720b0d538c3253c3a18144f
cedric karungu <ckarungu921@gmail.com>
2024-01-21 15:55:32 +0200

    feat add translation

--- apps/web/components/pages/maintenance/index.tsx
+++ apps/web/components/pages/maintenance/index.tsx
@@ -1,16 +1,20 @@
 'use client';
 import { RocketIcon } from '@components/ui/svgs/rocket';
 import { Text } from 'lib/components';
+import { useTranslations } from 'next-intl';
 function Maintenance() {
+	const t = useTranslations();
 	return (
 		<div className="mt-28 flex flex-col gap-7 jutify-center items-center">
 			<div className="m-auto relative flex justify-center items-center gap-4 text-center ">
 				<RocketIcon width={97} height={97} />
-				<Text className="text-[78px] font-semibold text-chetwodeBlue">We&apos;re Under Maintenance</Text>
+				<Text className="text-[78px] font-semibold text-chetwodeBlue">
+					{t('pages.maintenance.HEADING_TITLE')}
+				</Text>
 			</div>
 
 			<Text className="text-[40px] font-bold text-center text-[#282048] dark:text-light--theme">
-				We&apos;re currently updating our website to serve you better. Please check back later.
+				{t('pages.maintenance.HEADING_DESCRIPTION')}
 			</Text>
 		</div>
 	);
