b9a4a8a1b72345b491aa819c7f3062d066b34d20
Paradoxe Ngwasi <paradoxngwasi@outlook.com>
2024-01-23 06:38:26 +0000

    implement PUT /task-related-issue-types/:id api frontend

--- apps/web/app/services/client/api/task-related-issue-type.ts
+++ apps/web/app/services/client/api/task-related-issue-type.ts
@@ -4,7 +4,7 @@ import {
 	ITaskRelatedIssueTypeItemList,
 	PaginationResponse
 } from '@app/interfaces';
-import api, { get, post, put } from '../axios';
+import { deleteApi, get, post, put } from '../axios';
 
 export function createTaskRelatedIssueTypeAPI(data: ITaskRelatedIssueTypeCreate, tenantId?: string) {
 	return post<ITaskRelatedIssueTypeCreate>('/task-related-issue-types', data, {
@@ -19,7 +19,7 @@ export function editTaskRelatedIssueTypeAPI(id: string, data: ITaskRelatedIssueT
 }
 
 export function deleteTaskRelatedIssueTypeAPI(id: string) {
-	return api.delete<DeleteResponse>(`/task-related-issue-types/${id}`);
+	return deleteApi<DeleteResponse>(`/task-related-issue-types/${id}`);
 }
 
 export async function getTaskRelatedIssueTypeList(
