5934dae96bac37acaab775afa778d24b82ab5d62
cedric karungu <ckarungu921@gmail.com>
2024-01-23 19:35:11 +0200

    feat: add transaltions

--- apps/web/components/pages/error/index.tsx
+++ apps/web/components/pages/error/index.tsx
@@ -15,16 +15,16 @@ function ErrorPage({ error, reset }: { error: Error & { digest?: string }; reset
 		<div className="mt-28 flex flex-col gap-7 items-center">
 			<div className="m-auto relative flex justify-center items-center gap-4 text-center ">
 				<SadCry width={97} height={97} />
-				<Text className="text-[78px] font-semibold text-chetwodeBlue">Error!</Text>
+				<Text className="text-[78px] font-semibold text-chetwodeBlue">{t('pages.error.HEADING_TITLE')}</Text>
 			</div>
 
 			<Text className="text-[40px] font-bold text-center text-[#282048] dark:text-light--theme">
-				{t('pages.offline.HEADING_TITLE')}
+				{t('pages.error.HEADING_TITLE')}
 				{/* Something went wrong! */}
 			</Text>
 			<div className="flex flex-col gap-4">
 				<Text className="text-[20px] font-normal text-center text-gray-400">
-					{t('pages.offline.HEADING_DESCRIPTION')}
+					{t('pages.error.HEADING_DESCRIPTION')}
 				</Text>
 				<button onClick={() => reset()}>Try again</button>
 			</div>
