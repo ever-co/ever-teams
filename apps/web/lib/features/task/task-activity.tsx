e59849c78742ebdbcc6f221a7ed2db69619a4cca
cedric karungu <ckarungu921@gmail.com>
2024-01-26 23:20:23 +0200

    refact: user Activity on Task

--- apps/web/lib/features/task/task-activity.tsx
+++ apps/web/lib/features/task/task-activity.tsx
@@ -1,10 +1,15 @@
-import { clsxm } from '@app/utils';
-import { Tab } from '@headlessui/react';
 import { Card } from 'lib/components';
-import { ActivityFilters } from '@app/constants';
 import React from 'react';
+import { UserTaskActivity } from './activity/user-task-activity';
+import { ITeamTask } from '@app/interfaces';
 
-export function TaskActivity() {
+export function TaskActivity({ task }: { task: ITeamTask }) {
+	// get users tasks
+	const users = task.members;
+	console.log(users);
+	// filters user Task and get This one
+	// push to activity arr
+	// order activity arr by Time
 	return (
 		<Card
 			className="w-full p-4 md:px-4 dark:bg-[#25272D] flex flex-col gap-6 border border-[#00000014] dark:border-[#26272C]"
@@ -30,39 +35,3 @@ export function TaskActivity() {
 		</Card>
 	);
 }
-
-const UserTaskActivity = () => {
-	return (
-		<div className="shadow-md rounded-md  p-4 my-4 bg-[#00000014] dark:bg-[#26272C]">
-			<div className="flex items-center gap-2 my-2">
-				<h4 className="text-lg">{'Cedric medium'}</h4>
-				<span>{'05:30'}</span>
-			</div>
-			<Tab.Group>
-				<Tab.List className="w-full flex rounded-xl bg-gray-200 dark:bg-[#FFFFFF14] p-2">
-					{Object.values(ActivityFilters).filter(filter => filter !== 'Tasks').map((filter: string) => (
-                        <Tab
-                            key={filter}
-                            className={({ selected }) =>
-                                clsxm(
-                                    'w-full rounded-lg py-2.5 text-sm font-medium leading-5',
-                                    ' focus:outline-none focus:ring-2',
-                                    selected
-                                        ? 'bg-white dark:bg-dark text-blue-700 shadow'
-                                        : ' hover:bg-white/[0.50]'
-                                )
-                            }
-                        >
-                            {filter}
-                        </Tab>
-                    ))}
-				</Tab.List>
-				<Tab.Panels>
-					<Tab.Panel className="w-full mx-4 py-4">{'Screenshoot Team Tab'}</Tab.Panel>
-					<Tab.Panel className="w-full mx-4 py-4">{'Apps Tab'}</Tab.Panel>
-					<Tab.Panel className="w-full mx-4 py-4">{'VisitedSites Tab'}</Tab.Panel>
-				</Tab.Panels>
-			</Tab.Group>
-		</div>
-	);
-};
