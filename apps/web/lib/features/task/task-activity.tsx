bad131666d11276861e3867d06c068c5b58a6be3
cedric karungu <ckarungu921@gmail.com>
2024-01-28 16:04:38 +0200

    fix: font sizes

--- apps/web/lib/features/task/task-activity.tsx
+++ apps/web/lib/features/task/task-activity.tsx
@@ -12,7 +12,6 @@ export function TaskActivity({ task }: { task: ITeamTask }) {
 	const { getTaskTimesheets, taskTimesheets } = useTaskTimeSheets(task?.id);
 	// order activity arr by Time
 	const groupedData = groupByTime(taskTimesheets);
-	console.log(groupedData);
 
 	React.useEffect(() => {
 		getTaskTimesheets();
@@ -22,33 +21,17 @@ export function TaskActivity({ task }: { task: ITeamTask }) {
 			className="w-full p-4 md:px-4 dark:bg-[#25272D] flex flex-col gap-6 border border-[#00000014] dark:border-[#26272C]"
 			shadow="bigger"
 		>
-			{/* TO DELETE: start */}
-			<div className="shadow-lg rounded-lg p-4 bg-light dark:bg-dark border border-[#00000014] dark:border-[#26272C]">
-				<h3 className="text-xl font-semibold py-2">{'05.01.2024'}</h3>
-				<UserTaskActivity />
-				<UserTaskActivity />
-			</div>
-			{/* TO DELETE: end */}
 			{groupedData.map((timesheet, i) => (
 				<div
 					key={i}
 					className="shadow-lg rounded-lg p-4 bg-light dark:bg-dark border border-[#00000014] dark:border-[#26272C]"
 				>
-					<h3 className="text-xl font-semibold py-2">{timesheet.date}</h3>
+					<h3 className="text-base font-semibold py-2">{timesheet.date}</h3>
 					{timesheet.items.map((item) => (
-						<UserTaskActivity key={item.id} />
+						<UserTaskActivity key={item.id} timesheet={item} />
 					))}
 				</div>
 			))}
-
-			{/* TO DELETE: start */}
-			<div className="shadow-lg rounded-lg p-4 bg-light dark:bg-dark border border-[#00000014] dark:border-[#26272C]">
-				<h3 className="text-xl font-semibold py-2">{'03.01.2024'}</h3>
-				<UserTaskActivity />
-				<UserTaskActivity />
-				<UserTaskActivity />
-			</div>
-			{/* TO DELETE: end */}
 		</Card>
 	);
 }
