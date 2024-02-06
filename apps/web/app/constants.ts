54d18e6cb942c2b875fd0463c074a4e8427e42d0
Ruslan Konviser <evereq@gmail.com>
2024-02-06 13:39:55 +0100

    chore: switching to Ever Teams APIs (WIP)

--- apps/web/app/constants.ts
+++ apps/web/app/constants.ts
@@ -36,7 +36,8 @@ export const RECAPTCHA_SITE_KEY = getNextPublicEnv(
 export const RECAPTCHA_SECRET_KEY = process.env.CAPTCHA_SECRET_KEY;
 
 // Gauzy Server URL
-export const GAUZY_API_SERVER_URL = process.env.GAUZY_API_SERVER_URL || 'https://api.gauzy.co/api';
+export const GAUZY_API_SERVER_URL = process.env.GAUZY_API_SERVER_URL || 'https://api.ever.team/api';
+
 export const GAUZY_API_BASE_SERVER_URL = getNextPublicEnv(
 	'NEXT_PUBLIC_GAUZY_API_SERVER_URL',
 	process.env.NEXT_PUBLIC_GAUZY_API_SERVER_URL
