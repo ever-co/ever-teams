239caafe2710d5486a58360e824ad6160ac5cecd
cedric karungu <ckarungu921@gmail.com>
2024-02-07 14:43:12 +0200

    feat: add translations text

--- apps/web/components/pages/unauthorized/index.tsx
+++ apps/web/components/pages/unauthorized/index.tsx
@@ -13,21 +13,15 @@ function UnauthorizedPage() {
 		<div className="mt-28 flex flex-col gap-7 items-center">
 			<div className="m-auto relative flex justify-center items-center gap-4 text-center ">
 				<LockClosedIcon width={97} height={97} fill="#8C7AE4" className="text-[#8C7AE4]" />
-				<Text className="text-[78px] font-semibold text-chetwodeBlue">
-					{/* {t('pages.error.TITLE')} */}
-					Unauthorized
-				</Text>
+				<Text className="text-[78px] font-semibold text-chetwodeBlue">{t('pages.unauthorized.TITLE')}</Text>
 			</div>
 
 			<Text className="text-[40px] font-bold text-center text-[#282048] dark:text-light--theme">
-				{/* {t('pages.error.HEADING_TITLE')} */}
-				You are not authorized to access this page
+				{t('pages.unauthorized.HEADING_TITLE')}
 			</Text>
 			<div className="flex flex-col gap-4">
 				<Text className="text-[20px] container !max-w-5xl leading-8 font-normal text-center text-gray-400">
-					{t('pages.error.HEADING_DESCRIPTION')}
-					We apologize for the inconvenience, but you are not authorized to access to this page. If you
-					believe this is an error. Please login
+					{t('pages.unauthorized.HEADING_DESCRIPTION')}
 				</Text>
 				<Link href="/auth/login">
 					<Button className="m-auto mt-10 font-normal rounded-lg pl-7 pr-7">Login</Button>
