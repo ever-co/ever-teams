a662959be7f83c9643cd4662eb1c5d49cc2453f6
Paradoxe Ngwasi <paradoxngwasi@outlook.com>
2024-02-22 10:18:14 +0000

    add generateStaticParams api/public/team

--- apps/web/app/api/public/team/[profileLink]/[teamId]/route.ts
+++ apps/web/app/api/public/team/[profileLink]/[teamId]/route.ts
@@ -1,27 +1,36 @@
+import { INextParams } from '@app/interfaces';
 import {
 	getPublicOrganizationTeamMiscDataRequest,
 	getPublicOrganizationTeamRequest
 } from '@app/services/server/requests/public-organization-team';
 import { NextResponse } from 'next/server';
 
-export async function GET(req: Request, { params }: { params: { profileLink: string; teamId: string } }) {
+export async function GET(req: Request, { params }: INextParams) {
 	const { searchParams } = new URL(req.url);
 
-	const { profileLink, teamId } = params;
-	const { type } = searchParams as unknown as { type: string };
+	if (!params.profileLink || !params.teamId) {
+		return;
+	}
+
+	const type = searchParams.get('type') as string;
 
 	if (type === 'misc') {
 		const response = await getPublicOrganizationTeamMiscDataRequest({
-			profileLink: profileLink,
-			teamId
+			profileLink: params.profileLink,
+			teamId: params.teamId
 		});
 
 		return NextResponse.json(response.data);
 	}
 
 	const response = await getPublicOrganizationTeamRequest({
-		profileLink: profileLink,
-		teamId
+		profileLink: params.profileLink,
+		teamId: params.teamId
 	});
+
 	return NextResponse.json(response.data);
+}
+
+export async function generateStaticParams() {
+	return [{ profileLink: '', teamId: '' }];
 }
