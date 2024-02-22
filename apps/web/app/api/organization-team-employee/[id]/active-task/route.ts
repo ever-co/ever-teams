b4d8e5d7738a3601e4425da243df956aa1ea00aa
Paradoxe Ngwasi <paradoxngwasi@outlook.com>
2024-02-22 07:42:11 +0000

    add generateStaticParams api/organization-projects/setting

--- apps/web/app/api/organization-team-employee/[id]/active-task/route.ts
+++ apps/web/app/api/organization-team-employee/[id]/active-task/route.ts
@@ -1,24 +1,29 @@
-import { IOrganizationTeamEmployeeUpdate } from '@app/interfaces';
+import { INextParams, IOrganizationTeamEmployeeUpdate } from '@app/interfaces';
 import { authenticatedGuard } from '@app/services/server/guards/authenticated-guard-app';
 import { updateOrganizationTeamEmployeeActiveTaskRequest } from '@app/services/server/requests';
 import { NextResponse } from 'next/server';
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(req: Request, { params }: INextParams) {
 	const res = new NextResponse();
+	if (!params.id) {
+		return;
+	}
+
 	const { $res, user, access_token, tenantId } = await authenticatedGuard(req, res);
 	if (!user) return NextResponse.json({ error: 'unauthorized' }, { status: 401 });
 
-	const { id } = params;
 	const body = (await req.json()) as IOrganizationTeamEmployeeUpdate;
 
-	if (id) {
-		const response = await updateOrganizationTeamEmployeeActiveTaskRequest({
-			id: id as string,
-			bearer_token: access_token,
-			tenantId,
-			body
-		});
+	const response = await updateOrganizationTeamEmployeeActiveTaskRequest({
+		id: params.id,
+		bearer_token: access_token,
+		tenantId,
+		body
+	});
 
-		return $res(response.data);
-	}
+	return $res(response.data);
+}
+
+export async function generateStaticParams() {
+	return [{ id: '' }];
 }
