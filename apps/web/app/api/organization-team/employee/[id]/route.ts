6ef24c713a34c071d4139c79be13f58776421771
Paradoxe Ngwasi <paradoxngwasi@outlook.com>
2024-02-22 09:16:38 +0000

    add generateStaticParams api/organization-team/employee

--- apps/web/app/api/organization-team/employee/[id]/route.ts
+++ apps/web/app/api/organization-team/employee/[id]/route.ts
@@ -1,3 +1,4 @@
+import { INextParams } from '@app/interfaces';
 import { authenticatedGuard } from '@app/services/server/guards/authenticated-guard-app';
 import {
 	addEmployeeOrganizationTeamOrderRequest,
@@ -5,48 +6,53 @@ import {
 } from '@app/services/server/requests';
 import { NextResponse } from 'next/server';
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(req: Request, { params }: INextParams) {
 	const res = new NextResponse();
-	const { $res, user, access_token, tenantId } = await authenticatedGuard(req, res);
-	if (!user) return NextResponse.json({}, { status: 401 });
 
-	const { id } = params;
-
-	if (!id) {
+	if (!params.id) {
 		return NextResponse.json({}, { status: 405 });
 	}
+
+	const { $res, user, access_token, tenantId } = await authenticatedGuard(req, res);
+
+	if (!user) return NextResponse.json({}, { status: 401 });
 
 	const response = await removeEmployeeOrganizationTeamRequest({
 		bearer_token: access_token,
 		tenantId,
-		employeeId: id.toString()
+		employeeId: params.id
 	});
 
 	return $res(response.data);
 }
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(req: Request, { params }: INextParams) {
 	const res = new NextResponse();
+
+	if (!params.id) {
+		return NextResponse.json({}, { status: 405 });
+	}
+
 	const { $res, user, access_token, tenantId } = await authenticatedGuard(req, res);
+
 	if (!user) return NextResponse.json({}, { status: 401 });
 
 	const body = await req.json();
 
-	const { id } = params;
 	const order = body.order;
-
-	if (!id) {
-		return NextResponse.json({}, { status: 405 });
-	}
 
 	const response = await addEmployeeOrganizationTeamOrderRequest({
 		bearer_token: access_token,
+		employeeId: params.id,
 		tenantId,
-		employeeId: id.toString(),
 		order,
 		organizationTeamId: body.organizationTeamId,
 		organizationId: body.organizationId
 	});
 
 	return $res(response.data);
+}
+
+export async function generateStaticParams() {
+	return [{ id: '' }];
 }
