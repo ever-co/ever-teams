a925fff9136b033722b39031ef9797df66e031eb
Paradoxe Ngwasi <paradoxngwasi@outlook.com>
2024-02-23 09:59:59 +0000

    add generateStaticParams [locale]/meet

--- apps/web/app/[locale]/[...rest]/page.tsx
+++ apps/web/app/[locale]/[...rest]/page.tsx
@@ -3,3 +3,7 @@ import { notFound } from 'next/navigation';
 export default function CatchAllPage() {
 	notFound();
 }
+
+export async function generateStaticParams() {
+	return [{ locale: 'en', rest: [] }];
+}
