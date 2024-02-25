6534e6033ed4f3d4dc523890925e274c5c541db1
Paradoxe Ng <paradoxngwasi@outlook.com>
2024-02-25 08:46:33 +0100

    Feat/Static build (#2233)
    * add generateStaticParams
    
    * add generateStaticParams api/invite
    
    * add generateStaticParams api/organization-projects/setting
    
    * add generateStaticParams api/organization-team-join
    
    * add generateStaticParams api/organization-team-employee
    
    * add generateStaticParams api/organization-team/employee
    
    * add generateStaticParams api/roles
    
    * add generateStaticParams api/public/team
    
    * add generateStaticParams api/task-priorities
    
    * add generateStaticParams api/task-related-issue-types
    
    * add generateStaticParams api/tasks/employee
    
    * add generateStaticParams api/task-versions
    
    * add generateStaticParams auth/team/page
    
    * fixed build error
    
    * add generateStaticParams auth/passcode
    
    * add generateStaticParams [locale]/meet
    
    * add generateStaticParams [locale]/permissions
    
    * default application language
    
    * remove generateStaticParams from api routes
    
    * tiny changes

--- apps/web/app/api/tasks/employee/[employeeId]/route.ts
+++ apps/web/app/api/tasks/employee/[employeeId]/route.ts
@@ -1,45 +1,56 @@
+import { INextParams } from '@app/interfaces';
 import { authenticatedGuard } from '@app/services/server/guards/authenticated-guard-app';
 import { deleteEmployeeFromTasksRequest, getEmployeeTasksRequest } from '@app/services/server/requests';
 import { NextResponse } from 'next/server';
 
-export async function GET(req: Request, { params }: { params: { employeeId: string } }) {
+export async function GET(req: Request, { params }: INextParams) {
 	const res = new NextResponse();
+
+	if (!params.employeeId) {
+		return;
+	}
+
 	const { $res, user, tenantId, access_token: bearer_token } = await authenticatedGuard(req, res);
+
 	if (!user) return $res('Unauthorized');
 
 	const { searchParams } = new URL(req.url);
 
-	const { employeeId } = params;
 	const { organizationTeamId } = searchParams as unknown as {
 		organizationTeamId: string;
 	};
 
 	const response = await getEmployeeTasksRequest({
-		tenantId,
-		employeeId,
+		employeeId: params.employeeId,
 		organizationTeamId,
+		tenantId,
 		bearer_token
 	});
 
 	return $res(response.data);
 }
 
-export async function DELETE(req: Request, { params }: { params: { employeeId: string } }) {
+export async function DELETE(req: Request, { params }: INextParams) {
 	const res = new NextResponse();
+
+	if (!params.employeeId) {
+		return;
+	}
+
 	const { $res, user, tenantId, access_token: bearer_token } = await authenticatedGuard(req, res);
-	if (!user) return $res('Unauthorized');
+
+	if (!user) {
+		return $res('Unauthorized');
+	}
 
 	const { searchParams } = new URL(req.url);
 
-	const { employeeId } = params;
-	const { organizationTeamId } = searchParams as unknown as {
-		organizationTeamId: string;
-	};
+	const organizationTeamId = searchParams.get('organizationTeamId') as string;
 
 	const response = await deleteEmployeeFromTasksRequest({
-		tenantId,
-		employeeId,
+		employeeId: params.employeeId,
 		organizationTeamId,
+		tenantId,
 		bearer_token
 	});
 
