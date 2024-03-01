d548413c11878f7001413af62aef2bf3a8fe48f5
Cedric Karungu <ckarungu921@gmail.com>
2024-03-01 14:24:21 +0200

    fix: horizontal scroll on Invitation Table

--- apps/web/app/[locale]/settings/layout.tsx
+++ apps/web/app/[locale]/settings/layout.tsx
@@ -24,32 +24,30 @@ const SettingsLayout = ({ children }: { children: JSX.Element }) => {
 		return <SettingsPersonalSkeleton />;
 	} else {
 		return (
-			<div className="">
-				<MainLayout
-					className="items-start pb-1  h-auto w-full"
-					childrenClassName="flex h-[calc(100vh-_300px)] flex w-full items-start"
-				>
-					<div className="flex flex-col">
-						<div className="pt-12 w-full pb-4 bg-white dark:bg-dark--theme">
-							<Container fullWidth={fullWidth}>
-								<div className="flex flex-row items-center justify-start gap-8">
-									<Link href="/">
-										<ArrowLeft className="w-6 h-6" />
-									</Link>
+			<MainLayout
+				className="items-start pb-1 overflow-hidden w-full"
+				childrenClassName="flex h-[calc(100vh-_300px)] flex w-full items-start"
+			>
+				<div className="flex flex-col">
+					<div className="pt-12 w-full pb-4 bg-white dark:bg-dark--theme">
+						<Container fullWidth={fullWidth}>
+							<div className="flex flex-row items-center justify-start gap-8">
+								<Link href="/">
+									<ArrowLeft className="w-6 h-6" />
+								</Link>
 
-									<Breadcrumb paths={breadcrumb} className="text-sm" />
-								</div>
-							</Container>
-						</div>
-						<div className="flex">
-							<div className="w-96 ml-8">
-								<LeftSideSettingMenu />
+								<Breadcrumb paths={breadcrumb} className="text-sm" />
 							</div>
+						</Container>
+					</div>
+					<div className="flex">
+						<div className="w-96 ml-8">
+							<LeftSideSettingMenu />
 						</div>
 					</div>
-					<div className="h-[calc(100vh-_186px)] mt-3 p-10 overflow-y-auto  w-full">{children}</div>
-				</MainLayout>
-			</div>
+				</div>
+				<div className="h-[calc(100vh-_186px)] mt-3 p-10 overflow-y-auto  w-full">{children}</div>
+			</MainLayout>
 		);
 	}
 };
