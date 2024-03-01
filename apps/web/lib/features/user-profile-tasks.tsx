3282d7e506bdd68e7e79f5f5ce1f12ce1e9959fc
Cedric Karungu <ckarungu921@gmail.com>
2024-03-01 14:28:17 +0200

    fix: spell words

--- apps/web/lib/features/user-profile-tasks.tsx
+++ apps/web/lib/features/user-profile-tasks.tsx
@@ -31,7 +31,7 @@ export function UserProfileTask({ profile, tabFiltered }: Props) {
 		profile.member?.running == true ? t.id !== profile.activeUserTeamTask?.id : t
 	);
 	const { nextOffset, data } = useInfinityScrolling(otherTasks);
-	const dataTodisplay = otherTasks.length < 10 ? otherTasks : data;
+	const dataToDisplay = otherTasks.length < 10 ? otherTasks : data;
 
 	// const { total, onPageChange, itemsPerPage, itemOffset, endOffset, setItemsPerPage, currentItems } =
 	// 	usePagination(otherTasks);
@@ -86,7 +86,7 @@ export function UserProfileTask({ profile, tabFiltered }: Props) {
 			)}
 
 			<ul className="flex flex-col gap-6">
-				{dataTodisplay.map((task, index) => {
+				{dataToDisplay.map((task, index) => {
 					return (
 						<li key={task.id}>
 							<ObserverComponent isLast={index === data.length - 1} getNextData={nextOffset} />
