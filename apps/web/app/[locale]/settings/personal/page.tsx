c12a2b18a2d3d4a247cb65f3614f9bc6387e06e3
cedric karungu <83704005+Cedric921@users.noreply.github.com>
2024-03-01 20:52:52 +0200

    Fix: Errors (#2254)
    * fix: Email Input on Invite Pop Up
    
    * fix: Tasks Tab on Profile Page
    
    * fix: horizontal scroll on Invitation Table
    
    * fix: spell words
    
    * fix: Settings Header Page
    
    * fix: restore AutoCompleteDropdown for emails in Invite

--- apps/web/app/[locale]/settings/personal/page.tsx
+++ apps/web/app/[locale]/settings/personal/page.tsx
@@ -12,40 +12,37 @@ const Personal = () => {
 
 	return (
 		<>
-
-								<Link href={'/settings/team'} className="w-full">
-									<button className="w-full lg:hidden hover:bg-white rounded-xl border border-dark text-dark p-4 mt-2">
-										Go to Team settings
-									</button>
-								</Link>
-								<Accordian
-									title={t('pages.settingsPersonal.HEADING_TITLE')}
-									className="w-full max-w-[96vw] overflow-y-hidden p-4 mt-8 dark:bg-dark--theme"
-									id="general"
-								>
-									{/* <Text className="text-base font-normal text-center text-gray-400 sm:text-left">
-										{t('pages.settings.HEADING_DESCRIPTION')}
-									</Text> */}
-									<ProfileAvatar />
-									<PersonalSettingForm />
-								</Accordian>
-								<Accordian
-									title={t('pages.settingsPersonal.DATA_SYNCHRONIZATION')}
-									className="p-4 mt-4 dark:bg-dark--theme"
-									id="sync-zone"
-								>
-									<SyncZone />
-								</Accordian>
-								<Accordian
-									title={t('pages.settings.DANDER_ZONE')}
-									className="p-4 mt-4 dark:bg-dark--theme"
-									isDanger={true}
-									id="danger-zone"
-								>
-									<DangerZone />
-								</Accordian>
-
-
+			<Link href={'/settings/team'} className="w-full">
+				<button className="w-full lg:hidden hover:bg-white rounded-xl border border-dark text-dark p-4 mt-2">
+					Go to Team settings
+				</button>
+			</Link>
+			<Accordian
+				title={t('pages.settingsPersonal.HEADING_TITLE')}
+				className="w-full max-w-[96vw] overflow-y-hidden p-4 mt-8 dark:bg-dark--theme"
+				id="general"
+			>
+				{/* <Text className="text-base font-normal text-center text-gray-400 sm:text-left">
+					{t('pages.settings.HEADING_DESCRIPTION')}
+				</Text> */}
+				<ProfileAvatar />
+				<PersonalSettingForm />
+			</Accordian>
+			<Accordian
+				title={t('pages.settingsPersonal.DATA_SYNCHRONIZATION')}
+				className="p-4 mt-4 dark:bg-dark--theme"
+				id="sync-zone"
+			>
+				<SyncZone />
+			</Accordian>
+			<Accordian
+				title={t('pages.settings.DANDER_ZONE')}
+				className="p-4 mt-4 dark:bg-dark--theme"
+				isDanger={true}
+				id="danger-zone"
+			>
+				<DangerZone />
+			</Accordian>
 		</>
 	);
 };
