8194a59f45920cda5978566bea4aa5165f2c4f6f
Cedric Karungu <ckarungu921@gmail.com>
2024-03-05 18:31:18 +0200

    feat: add transition when Switch FullWidth mode

--- apps/web/lib/components/container.tsx
+++ apps/web/lib/components/container.tsx
@@ -15,7 +15,9 @@ export function Container({ children, className, fullWidth }: PropsWithChildren<
 			leaveTo="opacity-0"
 			className="w-full p-0 m-0"
 		>
-			<div className={clsxm(!fullWidth && 'x-container', fullWidth && 'mx-8', className)}>{children}</div>
+			<div className={clsxm('transition-all', !fullWidth && 'x-container', fullWidth && 'mx-8', className)}>
+				{children}
+			</div>
 		</Transition>
 	);
 }
