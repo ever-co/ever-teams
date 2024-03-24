76893cc44a0edb1bfab7887ea01f2e45a907f0b5
Cedric Karungu <ckarungu921@gmail.com>
2024-03-24 23:14:37 +0200

    fix: infinty scroll on profile page

--- apps/web/app/hooks/useInfinityFetch.ts
+++ apps/web/app/hooks/useInfinityFetch.ts
@@ -2,30 +2,34 @@
 
 import React from 'react';
 
-export const getPartData = ({ offset = 0, limit = 10, arr = [] }: { offset?: number; limit?: number; arr: any[] }) =>
-	arr.slice(0, offset * limit + limit);
+export const getPartData = ({ offset = 0, limit = 10, arr = [] }: { offset?: number; limit?: number; arr: any[] }) => {
+	const startIndex = offset * limit;
+	if (startIndex > arr.length) return arr;
+	return arr.slice(startIndex, Math.min(startIndex + limit, arr.length));
+};
 
 export const useInfinityScrolling = <T>(arr: T[], lim?: number) => {
 	const limit = lim ?? 10;
-	const [offset, setOffset] = React.useState(0);
-	const [data, setData] = React.useState<T[]>(arr);
+	const [offset, setOffset] = React.useState(1);
+	const [data, setData] = React.useState<T[]>([]);
 
 	const getSomeTasks = React.useCallback(
 		(offset: number) => {
-			setData(getPartData({ arr, limit, offset }));
+			setData((prevData) => {
+				const newData = getPartData({ arr, limit, offset });
+				return newData;
+			});
 		},
 		[arr, limit]
 	);
 
 	const nextOffset = React.useCallback(() => {
 		setOffset((prev) => prev + 1);
-		setData((prev) => getPartData({ arr: prev, limit, offset }));
-	}, [limit, offset]);
+	}, []);
 
 	React.useEffect(() => {
 		getSomeTasks(offset);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [offset]);
+	}, [getSomeTasks, offset]);
 
 	return {
 		offset,
