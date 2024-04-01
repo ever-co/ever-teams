5a49a0ad85af9b0ca435132c0c83fb5b8622ef13
anishali2 <ansihali1221@gmail.com>
2024-04-01 23:22:10 +0500

    Linting and Warnings

--- apps/web/env.js
+++ apps/web/env.js
@@ -1,30 +1,30 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 require('dotenv').config();
 
-// const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3030;
 
-// console.log(`NextJs is starting on Port 3030. You can open https://localhost:${port} to view it in the browser.`);
+console.log(`NextJs is starting on Port 3030. You can open https://localhost:${port} to view it in the browser.`);
 
-// const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
-// const isSentryEnabled = isProduction && process.env.SENTRY_DSN;
+const isSentryEnabled = isProduction && process.env.SENTRY_DSN;
 
-// console.log(`isProduction: ${isProduction}`);
+console.log(`isProduction: ${isProduction}`);
 
-// console.log(`isSentryEnabled: ${isSentryEnabled}`);
+console.log(`isSentryEnabled: ${isSentryEnabled}`);
 
-// if (process.env.GAUZY_API_SERVER_URL) {
-// 	console.log(
-// 		`Using Gauzy API from NextJs backend defined by GAUZY_API_SERVER_URL: ${process.env.GAUZY_API_SERVER_URL}`
-// 	);
-// } else {
-// 	console.warn(`GAUZY_API_SERVER_URL not defined`);
-// }
+if (process.env.GAUZY_API_SERVER_URL) {
+	console.log(
+		`Using Gauzy API from NextJs backend defined by GAUZY_API_SERVER_URL: ${process.env.GAUZY_API_SERVER_URL}`
+	);
+} else {
+	console.warn(`GAUZY_API_SERVER_URL not defined`);
+}
 
-// if (process.env.NEXT_PUBLIC_GAUZY_API_SERVER_URL) {
-// 	console.log(
-// 		`Using Gauzy API directly from frontend defined by NEXT_PUBLIC_GAUZY_API_SERVER_URL: ${process.env.NEXT_PUBLIC_GAUZY_API_SERVER_URL}`
-// 	);
-// } else {
-// 	console.warn(`Using Gauzy API via backend API 'proxy' because env NEXT_PUBLIC_GAUZY_API_SERVER_URL not defined`);
-// }
+if (process.env.NEXT_PUBLIC_GAUZY_API_SERVER_URL) {
+	console.log(
+		`Using Gauzy API directly from frontend defined by NEXT_PUBLIC_GAUZY_API_SERVER_URL: ${process.env.NEXT_PUBLIC_GAUZY_API_SERVER_URL}`
+	);
+} else {
+	console.warn(`Using Gauzy API via backend API 'proxy' because env NEXT_PUBLIC_GAUZY_API_SERVER_URL not defined`);
+}
