b1ef11270faa7f7154f8251cf9ee7068d5b04daf
GedeonTS <gedeontshobohwa1@gmail.com>
2024-05-02 22:58:05 +0200

    Fix type error

--- apps/web/app/[locale]/auth/passcode/component.tsx
+++ apps/web/app/[locale]/auth/passcode/component.tsx
@@ -129,7 +129,7 @@ function PasscodeScreen({ form, className }: { form: TAuthenticationPasscode } &
 	const t = useTranslations();
 	const inputsRef = useRef<Array<HTMLInputElement>>([]);
 
-	const formatTime = (seconds) => {
+	const formatTime = (seconds: number) => {
 		const minutes = Math.floor(seconds / 60);
 		const remainingSeconds = seconds % 60;
 		return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
