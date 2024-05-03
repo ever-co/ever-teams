c153df54aed1bddedeafcfe540ebeb5ac252ea7a
JoeMakuta <makutajosue@gmail.com>
2024-05-03 15:41:28 +0200

    Feat : light mode design of not found page

--- apps/web/components/pages/404/index.tsx
+++ apps/web/components/pages/404/index.tsx
@@ -1,28 +1,30 @@
 'use client';
-import SadCry from '@components/ui/svgs/sad-cry';
+// import SadCry from '@components/ui/svgs/sad-cry';
 import { Button, Text } from 'lib/components';
 import Link from 'next/link';
 
 function NotFound() {
 	return (
 		<div className=" w-screen h-screen flex flex-col  items-center justify-center">
-			<div className="flex flex-col items-center justify-center gap-4">
-				<div className="m-auto relative flex justify-center items-center gap-4 text-center ">
-					<SadCry width={97} height={97} />
-					<Text className="text-[78px] font-semibold text-chetwodeBlue">404!</Text>
+			<div className="flex flex-col items-center justify-center gap-6">
+				<div className=" w-52 h-52 rounded-full bg-black/10 flex justify-center items-center gap-4 text-center  ">
+					{/* <SadCry width={97} height={97} /> */}
+					<Text className="text-6xl font-semibold text-primary">404</Text>
 				</div>
 
-				<Text className="text-[40px] font-bold text-center text-[#282048] dark:text-light--theme">
-					Page not found !
-				</Text>
+				<div className="flex flex-col justify-center items-center gap-4">
+					<Text className="text-[40px] font-bold text-center text-[#282048] dark:text-light--theme">
+						Not found !
+					</Text>
 
-				<Text className="text-[20px] font-normal text-center text-gray-400">
-					{`We looked, but can't find it ....`}
-				</Text>
+					<Text className=" font-light text-center text-gray-400">
+						Resource you are looking for is not found!
+					</Text>
 
-				<Button className="m-auto font-normal rounded-lg ">
-					<Link href="/">Go back to home</Link>
-				</Button>
+					<Button className="m-auto font-normal rounded-lg ">
+						<Link href="/">Go back to home</Link>
+					</Button>
+				</div>
 			</div>
 		</div>
 	);
