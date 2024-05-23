7570c45d6e5ec31bbc0ff7ec913a208815eceb95
Gloire Mutaliko (Salva) <86450367+GloireMutaliko21@users.noreply.github.com>
2024-05-23 18:04:51 +0200

    Integrate next-auth for support of social logins (#2525)
    * chore: init next-auth library configurations
    
    * feat: config social media logins
    
    * style: add social login buttons
    
    * feat: next-auth signin methods
    
    * feat: jwt and session callbacks
    
    * feat: get gauzy user from next-auth jwt callback
    
    * feat: load session gauzy data on next-auth session
    
    * feat: load session cookies from oauth
    
    * feat: redirect to workspaces page after signin
    
    * feat: logged user on workspaces page
    
    * feat: init next-auth provider
    
    * feat: signin to workspaces using oauth
    
    * fix: improve naming update function
    
    * feat: oauth signout

---
+++ apps/web/auth.ts
@@ -0,0 +1,110 @@
+import NextAuth from 'next-auth';
+import type { Provider } from 'next-auth/providers';
+import Facebook from 'next-auth/providers/facebook';
+import Google from 'next-auth/providers/google';
+import Github from 'next-auth/providers/github';
+import Twitter from 'next-auth/providers/twitter';
+import { signWithSocialLoginsRequest } from '@app/services/server/requests';
+import { getUserOrganizationsRequest, signInWorkspaceAPI } from '@app/services/client/api/auth/invite-accept';
+
+const providers: Provider[] = [Facebook, Google, Github, Twitter];
+
+export const mappedProviders = providers.map((provider) => {
+	if (typeof provider === 'function') {
+		const providerData = provider();
+		return { id: providerData.id, name: providerData.name };
+	} else return { id: provider.id, name: provider.name };
+});
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+	providers: providers,
+	callbacks: {
+		async signIn({ user }) {
+			try {
+				const { email } = user;
+				const gauzyLoginUser = await signWithSocialLoginsRequest(email ?? '');
+				const data = await signInWorkspaceAPI(
+					gauzyLoginUser?.data.confirmed_email,
+					gauzyLoginUser?.data.workspaces[0].token
+				);
+				const tenantId = data.user?.tenantId || '';
+				const access_token = data.token;
+				const userId = data.user?.id;
+
+				const { data: organizations } = await getUserOrganizationsRequest({
+					tenantId,
+					userId,
+					token: access_token
+				});
+
+				const organization = organizations?.items[0];
+
+				if (!organization) {
+					return false;
+				}
+
+				return !!gauzyLoginUser && !!organization;
+			} catch (error) {
+				return false;
+			}
+		},
+
+		async jwt({ token, user, trigger, session }) {
+			if (user) {
+				const { email } = user;
+				const gauzyLoginUser = await signWithSocialLoginsRequest(email ?? '');
+				const data = await signInWorkspaceAPI(
+					gauzyLoginUser?.data.confirmed_email,
+					gauzyLoginUser?.data.workspaces[0].token
+				);
+				const tenantId = data.user?.tenantId || '';
+				const access_token = data.token;
+				const userId = data.user?.id;
+
+				const { data: organizations } = await getUserOrganizationsRequest({
+					tenantId,
+					userId,
+					token: access_token
+				});
+
+				const organization = organizations?.items[0];
+
+				if (!organization) {
+					return Promise.reject({
+						errors: {
+							email: 'Your account is not yet ready to be used on the Ever Teams Platform'
+						}
+					});
+				}
+
+				token.authCookie = {
+					access_token,
+					refresh_token: {
+						token: data.refresh_token
+					},
+					teamId: gauzyLoginUser?.data.workspaces[0].current_teams[0].team_id,
+					tenantId,
+					organizationId: organization?.organizationId,
+					languageId: 'en',
+					noTeamPopup: true,
+					userId,
+					workspaces: gauzyLoginUser?.data.workspaces,
+					confirmed_mail: gauzyLoginUser?.data.confirmed_email
+				};
+			}
+
+			if (trigger === 'update' && session) {
+				token = { ...token, authCookie: session };
+			}
+
+			return token;
+		},
+		session({ session, token }) {
+			session.user = token.authCookie as any;
+			return session;
+		}
+	},
+	pages: {
+		error: '/auth/error'
+	}
+});
