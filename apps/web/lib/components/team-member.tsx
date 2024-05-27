ea1d033cffb324dfd4793124953b37f6af55ab76
Ushindi Gedeon <gedeontshobohwa1@gmail.com>
2024-05-27 23:36:02 +0200

    Add check marck for assigned members and the possibility to uncheck

--- apps/web/lib/components/team-member.tsx
+++ apps/web/lib/components/team-member.tsx
@@ -1,22 +1,31 @@
 import { UserInfo } from 'lib/features/team/user-team-card/user-info';
 import { useTeamMemberCard } from '@app/hooks';
-import { useModal } from '@app/hooks';
+import { FaCheck } from "react-icons/fa6";
 
 export default function TeamMember({ member, item }: { member: any; item: any }) {
 	const memberInfo = useTeamMemberCard(member);
 	const { assignTask } = useTeamMemberCard(member);
-	const { closeModal } = useModal();
+
+
+	const checkUser = item.members.some(el => el.id === member.employeeId);
+	const assignMember = () => {
+		if (checkUser) {
+			memberInfo.unassignTask(item);
+		} else {
+			assignTask(item);
+		}
+	}
 
 	return (
 		<div
 			onClick={() => {
-				assignTask(item);
-				closeModal();
+				assignMember();
 			}}
 
-            className="w-100 cursor-pointer"
+			className="w-100 cursor-pointer flex items-center"
 		>
 			<UserInfo memberInfo={memberInfo} className="2xl:w-[20.625rem] w-100 pointer-events-none" />
+			{checkUser ? (<FaCheck size={17} fill="#3826a6" />) : (<></>)}
 		</div>
 	);
 }
