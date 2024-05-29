b850a3f27f18bab2c51bbf632acbb830e9e7724b
Ushindi Gedeon <gedeontshobohwa1@gmail.com>
2024-05-29 10:24:19 +0200

    Update team-member cell

--- apps/web/lib/components/team-member.tsx
+++ apps/web/lib/components/team-member.tsx
@@ -2,18 +2,17 @@ import { UserInfo } from 'lib/features/team/user-team-card/user-info';
 import { useTeamMemberCard } from '@app/hooks';
 import { FaCheck } from "react-icons/fa6";
 
-export default function TeamMember({ member, item }: { member: any; item: any }) {
+export default function TeamMember(
+	{ member, item, onCheckMember, membersList }
+		:c
+		{ member: any; item: any; onCheckMember: any; membersList: any }
+) {
 	const memberInfo = useTeamMemberCard(member);
-	const { assignTask } = useTeamMemberCard(member);
+	// const { assignTask } = useTeamMemberCard(member);
+	const checkAssign = membersList.assignedMembers.some(el => el.id === member.employeeId);
 
-
-	const checkUser = item.members.some(el => el.id === member.employeeId);
 	const assignMember = () => {
-		if (checkUser) {
-			memberInfo.unassignTask(item);
-		} else {
-			assignTask(item);
-		}
+		onCheckMember(member.employee);
 	}
 
 	return (
@@ -25,7 +24,7 @@ export default function TeamMember({ member, item }: { member: any; item: any })
 			className="w-100 cursor-pointer flex items-center"
 		>
 			<UserInfo memberInfo={memberInfo} className="2xl:w-[20.625rem] w-100 pointer-events-none" />
-			{checkUser ? (<FaCheck size={17} fill="#3826a6" />) : (<></>)}
+			{checkAssign ? (<FaCheck size={17} fill="#3826a6" />) : (<></>)}
 		</div>
 	);
 }
