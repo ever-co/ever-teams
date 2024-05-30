047733b1812eae594114b8edc3307094bdfd9327
Ushindi Gedeon <gedeontshobohwa1@gmail.com>
2024-05-30 11:23:01 +0200

    [Improve]Refactor The Assign Task to Team Members Popup (#2556)
    * Add functionnality to show assigned members with tasks avatar and select many
    
    * Add check marck for assigned members and the possibility to uncheck
    
    * Install react-icons
    
    * Update the functionnality to select multiple assignee and assign all on validate
    
    * Update team-member cell
    
    * Update task avatars
    
    * Update task assign validation to many member
    
    * Update task assign for a team member
    
    * Refactor code to clean
    
    * Fix unsafe null check in useState
    
    * Update package.json to remove error
    
    * Add type to unassigned members array
    
    * Update dependency arrays
    
    * Add Interface member
    
    * Add Interface to members list
    
    * Update image overlapper
    
    * Add Iemployee interface to member
    
    * Update task avatar to require only members
    
    * Update image overlaper to show the arrow
    
    * Add type IEmployee when checking member
    
    * Update task card to show the arrow

--- apps/web/lib/features/task/task-item.tsx
+++ apps/web/lib/features/task/task-item.tsx
@@ -1,6 +1,6 @@
 import { imgTitle } from '@app/helpers';
 import { useTeamTasks } from '@app/hooks';
-import { IClassName, ITaskStatus, ITeamTask } from '@app/interfaces';
+import { IClassName, ITaskStatus, IEmployee, ITeamTask } from '@app/interfaces';
 import { clsxm, isValidUrl } from '@app/utils';
 import clsx from 'clsx';
 import { Avatar, ConfirmDropdown, SpinnerLoader, Tooltip } from 'lib/components';
@@ -118,9 +118,10 @@ export function TaskItem({ task, selected, onClick, className }: Props) {
 	);
 }
 
-export function TaskAvatars({ task, limit = 2 }: { task: ITeamTask; limit?: number }) {
-	const members = task.members;
+type PartialITeamTask = Partial<ITeamTask> & { members: IEmployee[] };
 
+export function TaskAvatars({ task, limit = 2 }: { task: PartialITeamTask; limit?: number }) {
+	const members = task.members;
 	const taskAssignee: ImageOverlapperProps[] = members.map((member: any) => {
 		return {
 			id: member.user.id,
