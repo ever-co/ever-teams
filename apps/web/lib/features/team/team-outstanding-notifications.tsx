bd674da61333d2e85acb0616393fc0c3904c80e7
GloireMutaliko21 <mufunyig@gmail.com>
2024-06-29 16:07:49 +0200

    feat: user notification for outstanding

---
+++ apps/web/lib/features/team/team-outstanding-notifications.tsx
@@ -0,0 +1,89 @@
+'use client';
+import { useAuthenticateUser, useDailyPlan } from '@app/hooks';
+import { IDailyPlan, IUser } from '@app/interfaces';
+import { Cross2Icon, EyeOpenIcon } from '@radix-ui/react-icons';
+import { Tooltip } from 'lib/components';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+export function TeamOutstandingNotifications() {
+	const { getEmployeeDayPlans, outstandingPlans } = useDailyPlan();
+
+	const { user } = useAuthenticateUser();
+
+	useEffect(() => {
+		getEmployeeDayPlans(user?.employee.id || '');
+	}, [getEmployeeDayPlans, user?.employee.id]);
+
+	return (
+		<>
+			{outstandingPlans && outstandingPlans.length > 0 && (
+				<UserOutstandingNotification outstandingTasks={outstandingPlans} user={user} />
+			)}
+		</>
+	);
+}
+
+function UserOutstandingNotification({ outstandingTasks, user }: { outstandingTasks: IDailyPlan[]; user?: IUser }) {
+	// Notification will be displayed 6 hours after the user closed it
+	const REAPPEAR_INTERVAL = 6 * 60 * 60 * 1000; // 6 hours in milliseconds;
+	const DISMISSAL_TIMESTAMP_KEY = 'user-saw-notif';
+
+	const name = user?.name || user?.firstName || user?.lastName || user?.username;
+
+	const [visible, setVisible] = useState(false);
+	const tasks = outstandingTasks.flatMap((plan) => plan.tasks);
+
+	useEffect(() => {
+		const checkNotification = () => {
+			const alreadySeen = window && parseInt(window?.localStorage.getItem(DISMISSAL_TIMESTAMP_KEY) || '0', 10);
+			const currentTime = new Date().getTime();
+
+			if (!alreadySeen || currentTime - alreadySeen > REAPPEAR_INTERVAL) {
+				setVisible(true);
+			}
+		};
+
+		checkNotification();
+		const intervalId = setInterval(checkNotification, REAPPEAR_INTERVAL);
+		return () => clearInterval(intervalId);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	const onClose = () => {
+		window && window?.localStorage.setItem(DISMISSAL_TIMESTAMP_KEY, new Date().getTime().toString());
+		setVisible(false);
+	};
+
+	return (
+		<>
+			{visible && (
+				<div className="rounded-2xl dark:border-dark--theme-light border py-2 px-6 flex justify-between items-center text-xs mb-2">
+					<div>
+						You have {tasks?.length} uncompleted tasks, please check in{' '}
+						<span className="font-medium">Outstanding View</span>
+					</div>
+					<div className="flex items-center gap-5">
+						<div>
+							<Link
+								href={`/profile/${user?.id}?name=${name || ''}`}
+								className="bg-primary text-white py-2 px-4 flex gap-2 items-center rounded-xl"
+								onClick={() => {
+									onClose();
+									window && window.localStorage.setItem('task-tab', 'dailyplan');
+									window && window.localStorage.setItem('daily-plan-tab', 'Outstanding');
+								}}
+							>
+								<EyeOpenIcon />
+								<span>View</span>
+							</Link>
+						</div>
+						<Tooltip label="Close">
+							<Cross2Icon className="text-xl cursor-pointer" onClick={onClose} />
+						</Tooltip>
+					</div>
+				</div>
+			)}
+		</>
+	);
+}
