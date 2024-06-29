0337cceeb822affa00dbd9b6850c45d66a1002ad
GloireMutaliko21 <mufunyig@gmail.com>
2024-06-29 20:05:25 +0200

    feat: manager notification for outstanding users tasks

--- apps/web/lib/features/team/team-outstanding-notifications.tsx
+++ apps/web/lib/features/team/team-outstanding-notifications.tsx
@@ -8,20 +8,27 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 export function TeamOutstandingNotifications() {
-	const { getEmployeeDayPlans, outstandingPlans } = useDailyPlan();
+	const { getAllDayPlans, dailyPlan, getEmployeeDayPlans, outstandingPlans } = useDailyPlan();
 
 	const { user } = useAuthenticateUser();
 
 	useEffect(() => {
+		getAllDayPlans();
 		getEmployeeDayPlans(user?.employee.id || '');
-	}, [getEmployeeDayPlans, user?.employee.id]);
+	}, [getAllDayPlans, getEmployeeDayPlans, user?.employee.id]);
+
+	console.log(dailyPlan);
 
 	return (
-		<>
+		<div className="flex flex-col gap-4">
 			{outstandingPlans && outstandingPlans.length > 0 && (
 				<UserOutstandingNotification outstandingTasks={outstandingPlans} user={user} />
 			)}
-		</>
+
+			{dailyPlan.items && dailyPlan.items.length > 0 && (
+				<ManagerOutstandingUsersNotification outstandingTasks={dailyPlan.items} />
+			)}
+		</div>
 	);
 }
 
@@ -83,6 +90,73 @@ function UserOutstandingNotification({ outstandingTasks, user }: { outstandingTa
 								<EyeOpenIcon />
 								<span>{t('pages.home.OUTSTANDING_NOTIFICATIONS.VIEW_BUTTON')}</span>
 							</Link>
+						</div>
+						<Tooltip label={t('common.CLOSE')}>
+							<Cross2Icon className="text-xl cursor-pointer" onClick={onClose} />
+						</Tooltip>
+					</div>
+				</div>
+			)}
+		</>
+	);
+}
+
+function ManagerOutstandingUsersNotification({ outstandingTasks }: { outstandingTasks: IDailyPlan[] }) {
+	const t = useTranslations();
+
+	// Notification will be displayed 6 hours after the user closed it
+	const REAPPEAR_INTERVAL = 6 * 60 * 60 * 1000; // 6 hours in milliseconds;
+	const DISMISSAL_TIMESTAMP_KEY = 'manager-saw-outstanding-notif';
+
+	const [visible, setVisible] = useState(false);
+
+	const employeeWithOutstanding = outstandingTasks.map((plan) => plan.employee);
+
+	useEffect(() => {
+		const checkNotification = () => {
+			const alreadySeen = window && parseInt(window?.localStorage.getItem(DISMISSAL_TIMESTAMP_KEY) || '0', 10);
+			const currentTime = new Date().getTime();
+
+			if (!alreadySeen || currentTime - alreadySeen > REAPPEAR_INTERVAL) {
+				setVisible(true);
+			}
+		};
+
+		checkNotification();
+		const intervalId = setInterval(checkNotification, REAPPEAR_INTERVAL);
+		return () => clearInterval(intervalId);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	const onClose = () => {
+		window && window?.localStorage.setItem(DISMISSAL_TIMESTAMP_KEY, new Date().getTime().toString());
+		setVisible(false);
+	};
+	return (
+		<>
+			{visible && (
+				<div className="rounded-2xl dark:border-dark--theme-light border py-2 px-6 flex justify-between items-center text-xs mb-2">
+					<div>
+						{t('pages.home.OUTSTANDING_NOTIFICATIONS.SUBJECT')} {employeeWithOutstanding?.length}{' '}
+						{t('pages.home.OUTSTANDING_NOTIFICATIONS.USER_LABEL')}{' '}
+						<span className="font-medium">
+							{t('pages.home.OUTSTANDING_NOTIFICATIONS.OUTSTANDING_VIEW')}
+						</span>
+					</div>
+					<div className="flex items-center gap-5">
+						<div>
+							{/* <Link
+								href={`/profile/${user?.id}?name=${name || ''}`}
+								className="bg-primary text-white py-2 px-4 flex gap-2 items-center rounded-xl"
+								onClick={() => {
+									onClose();
+									window && window.localStorage.setItem('task-tab', 'dailyplan');
+									window && window.localStorage.setItem('daily-plan-tab', 'Outstanding');
+								}}
+							>
+								<EyeOpenIcon />
+								<span>{t('pages.home.OUTSTANDING_NOTIFICATIONS.VIEW_BUTTON')}</span>
+							</Link> */}
 						</div>
 						<Tooltip label={t('common.CLOSE')}>
 							<Cross2Icon className="text-xl cursor-pointer" onClick={onClose} />
