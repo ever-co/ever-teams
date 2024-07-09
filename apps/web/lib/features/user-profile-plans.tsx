aa05abcd0f673ff3fef00bf1792ab7c5e34405be
Innocent-akim <gentilakili98@gmail.com>
2024-07-09 15:47:17 +0200

    fix/dailyplan/deleteplan

--- apps/web/lib/features/user-profile-plans.tsx
+++ apps/web/lib/features/user-profile-plans.tsx
@@ -5,7 +5,7 @@ import { useRecoilValue } from 'recoil';
 import { useCanSeeActivityScreen, useDailyPlan, useUserProfilePage } from '@app/hooks';
 import { TaskCard } from './task/task-card';
 import { IDailyPlan } from '@app/interfaces';
-import { Container, NoData, ProgressBar, VerticalSeparator } from 'lib/components';
+import { AlertPopup, Container, NoData, ProgressBar, VerticalSeparator } from 'lib/components';
 import { clsxm } from '@app/utils';
 import { fullWidthState } from '@app/stores/fullWidth';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@components/ui/accordion';
@@ -131,6 +131,7 @@ function AllPlans({ profile, currentTab = 'All Tasks' }: { profile: any; current
 	// Filter plans
 	let filteredPlans: IDailyPlan[] = [];
 	const { deleteDailyPlan, deleteDailyPlanLoading, sortedPlans, todayPlan } = useDailyPlan();
+	const [popupOpen, setPopupOpen] = useState(false)
 
 	filteredPlans = sortedPlans;
 	if (currentTab === 'Today Tasks') filteredPlans = todayPlan;
@@ -199,6 +200,44 @@ function AllPlans({ profile, currentTab = 'All Tasks' }: { profile: any; current
 													)}
 													Delete this plan
 												</Button>
+												<AlertPopup
+													open={popupOpen}
+													children={
+														<>
+															{/*button confirm*/}
+															<Button
+																disabled={deleteDailyPlanLoading}
+																onClick={() => deleteDailyPlan(plan.id ?? '')}
+																variant="destructive"
+																className="flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-red-600 rounded-md hover:bg-red-700 disabled:bg-red-400"
+															>
+																{deleteDailyPlanLoading && (
+																	<ReloadIcon className="animate-spin mr-2 h-4 w-4" />
+																)}
+																Delete
+															</Button>
+															{/*button cancel*/}
+															<Button
+																onClick={() => setPopupOpen(false)}
+																variant="outline"
+																className="px-4 py-2 text-sm font-medium text-red-600 border border-red-600 rounded-md bg-light--theme-light dark:!bg-dark--theme-light"
+															>
+																Cancel
+															</Button>
+														</>
+													}
+													buttonOpen={
+														//button open popup
+														<Button
+															onClick={() => setPopupOpen(true)}
+															variant="outline"
+															className="px-4 py-2 text-sm font-medium text-red-600 border border-red-600 rounded-md bg-light--theme-light dark:!bg-dark--theme-light"
+														>
+															Delete this plan
+														</Button>
+													}
+												/>
+
 											</div>
 										) : (
 											<></>
