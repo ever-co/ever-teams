d77cf390d9b34641b9dd47c42987bcad3db260dd
Gloire Mutaliko (Salva) <86450367+GloireMutaliko21@users.noreply.github.com>
2024-07-13 17:39:50 +0200

    [Feature] 'Plan's' views UI (#2721)
    * feat: render daily plan view tabs
    
    * feat: daily plan colum and tile view
    
    * style: outstanding input filter
    
    * fix: typos, components naming etc
    
    * fix: typos, components naming etc

--- apps/web/lib/features/task/daily-plan/outstanding-date.tsx
+++ apps/web/lib/features/task/daily-plan/outstanding-date.tsx
@@ -3,12 +3,18 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@c
 import { EmptyPlans, PlanHeader } from 'lib/features/user-profile-plans';
 import { TaskCard } from '../task-card';
 import { useDailyPlan } from '@app/hooks';
+import { HorizontalSeparator } from 'lib/components';
+import TaskBlockCard from '../task-block-card';
+import { clsxm } from '@app/utils';
+import { useRecoilValue } from 'recoil';
+import { dailyPlanViewHeaderTabs } from '@app/stores/header-tabs';
 
 interface IOutstandingFilterDate {
-	profile: any
+	profile: any;
 }
 export function OutstandingFilterDate({ profile }: IOutstandingFilterDate) {
 	const { outstandingPlans } = useDailyPlan();
+	const view = useRecoilValue(dailyPlanViewHeaderTabs);
 	return (
 		<div className="flex flex-col gap-6">
 			{outstandingPlans?.length > 0 ? (
@@ -21,11 +27,14 @@ export function OutstandingFilterDate({ profile }: IOutstandingFilterDate) {
 						<AccordionItem
 							value={plan.date.toString().split('T')[0]}
 							key={plan.id}
-							className="dark:border-slate-600"
+							className="dark:border-slate-600 !border-none"
 						>
-							<AccordionTrigger className="hover:no-underline">
-								<div className="text-lg">
-									{formatDayPlanDate(plan.date.toString())} ({plan.tasks?.length})
+							<AccordionTrigger className="!min-w-full text-start hover:no-underline">
+								<div className="flex items-center justify-between gap-3 w-full">
+									<div className="text-lg min-w-max">
+										{formatDayPlanDate(plan.date.toString())} ({plan.tasks?.length})
+									</div>
+									<HorizontalSeparator />
 								</div>
 							</AccordionTrigger>
 							<AccordionContent className="bg-light--theme border-none dark:bg-dark--theme pb-12">
@@ -33,21 +42,32 @@ export function OutstandingFilterDate({ profile }: IOutstandingFilterDate) {
 								<PlanHeader plan={plan} planMode="Outstanding" />
 
 								{/* Plan tasks list */}
-								<ul className="flex flex-col gap-2">
-									{plan.tasks?.map((task) => (
-										<TaskCard
-											key={`${task.id}${plan.id}`}
-											isAuthUser={true}
-											activeAuthTask={true}
-											viewType={'dailyplan'}
-											task={task}
-											profile={profile}
-											type="HORIZONTAL"
-											taskBadgeClassName={`rounded-sm`}
-											taskTitleClassName="mt-[0.0625rem]"
-											planMode="Outstanding"
-										/>
-									))}
+								<ul
+									className={clsxm(
+										view === 'CARDS' && 'flex-col',
+										view === 'TABLE' && 'flex-wrap',
+										'flex gap-2 pb-[1.5rem]',
+										view === 'BLOCKS' && 'overflow-x-scroll'
+									)}
+								>
+									{plan.tasks?.map((task) =>
+										view === 'CARDS' ? (
+											<TaskCard
+												key={`${task.id}${plan.id}`}
+												isAuthUser={true}
+												activeAuthTask={true}
+												viewType={'dailyplan'}
+												task={task}
+												profile={profile}
+												type="HORIZONTAL"
+												taskBadgeClassName={`rounded-sm`}
+												taskTitleClassName="mt-[0.0625rem]"
+												planMode="Outstanding"
+											/>
+										) : (
+											<TaskBlockCard key={task.id} task={task} />
+										)
+									)}
 								</ul>
 							</AccordionContent>
 						</AccordionItem>
