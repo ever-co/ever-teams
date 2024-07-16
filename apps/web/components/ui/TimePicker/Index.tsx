30833fe9a0ea7477979efd52476fa7e27561ce4a
Ushindi Gedeon <gedeontshobohwa1@gmail.com>
2024-07-16 18:09:08 +0200

    Update style and fix bug when selecting 0h

--- apps/web/components/ui/TimePicker/Index.tsx
+++ apps/web/components/ui/TimePicker/Index.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import TimeKeeper from 'react-timekeeper';
-import { FaClock, FaCaretDown } from 'react-icons/fa';
+import { FaClock } from 'react-icons/fa';
 import { FaCheck } from "react-icons/fa";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
-const CustomTimePicker = ({ selectedTime, containerStyle }:
-    { selectedTime: (timeVal: string) => void; containerStyle: string }) => {
+const CustomTimePicker = ({ selectedTime, containerStyle, disabledTimeRange }:
+    { selectedTime: (timeVal: string) => void; containerStyle: string; disabledTimeRange?: { from: string, to: string } | null }) => {
     const getCurrentTime = (): string => {
         const now = new Date();
         const hours = String(now.getHours()).padStart(2, '0');
@@ -15,37 +16,16 @@ const CustomTimePicker = ({ selectedTime, containerStyle }:
     const [value, setValue] = useState(getCurrentTime());
     const [isOpen, setIsOpen] = useState(false);
 
-    const handleChange = (newValue: { minute: number, hour: number, meridiem: string }) => {
-
-        const { min, h } = roundTimeToNearest10({ h: newValue.hour, min: newValue.minute });
-
-        setValue(`${h}:${min}`);
+    const formatTime = (time:String) => {
+        const [hours, minutes] = time.split(':').map((part:any) => part.padStart(2, '0'));
+        return `${hours}:${minutes}`;
     };
 
-    const roundTimeToNearest10 = ({ h, min }: { h: number; min: number }) => {
-        const roundedMinute = Math.round(min / 10) * 10;
-
-        let hour = `${h % 24}`; let minute = `${roundedMinute}`;
-        if (roundedMinute === 60) {
-            if (h === 23) {
-                hour = "00";
-            } else {
-                hour = `${(h + 1) % 24}`;
-            }
-            minute = "00";
-        } else if (h === 24) {
-            hour = "00";
-        } else if(min === 0){
-            minute = "00";
-        }else {
-            hour = `${h % 24}`;
-            minute = `${roundedMinute}`;
-        }
-        return {
-            h: hour,
-            min: minute
-        };
-    }
+    const handleChange = (newValue: { minute: number, hour: number }) => {
+        const formatedTime = formatTime(`${newValue.hour}:${newValue.minute}`)
+        setValue(formatedTime);
+        selectedTime(formatedTime);
+    };
 
     useEffect(() => {
         selectedTime(value);
@@ -54,12 +34,12 @@ const CustomTimePicker = ({ selectedTime, containerStyle }:
     return (
         <div className={`relative ${containerStyle}`}>
             <div
-                className="flex items-center justify-between p-2 border border-gray-300 rounded-md cursor-pointer"
+                className="flex items-center justify-between p-2 border border-gray-300 rounded-[10px] cursor-pointer"
                 onClick={() => setIsOpen(!isOpen)}
             >
                 <FaClock className="text-gray-500" />
-                <span className="ml-2">{value.replace(':', 'h')}</span>
-                <FaCaretDown className="text-gray-500 ml-auto" />
+                <span className="ml-2 text-[13px] font-bold">{value.replace(':', 'h')}</span>
+                <RiArrowDropDownLine size={30} className="text-gray-500 ml-auto" />
             </div>
             {isOpen && (
                 <div className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg">
@@ -68,10 +48,13 @@ const CustomTimePicker = ({ selectedTime, containerStyle }:
                         onChange={(newTime) => handleChange(newTime)}
                         switchToMinuteOnHourSelect
                         closeOnMinuteSelect
+                        forceCoarseMinutes
                         coarseMinutes={10}
+                        disabledTimeRange={disabledTimeRange? {...disabledTimeRange} : null}
                         doneButton={() => <button
+                            className="flex items-center border-[1px] border-gray-300 rounded-[12px] px-[12px] font-bold text-[#25b15d]"
                             onClick={() => setIsOpen(false)}>
-                            <FaCheck fill={"#"} /> Done</button>
+                            <FaCheck fill={"#25b15d"} size={20} className="mr-[8px]" /> Done</button>
                         }
                     />
                 </div>
