12474f53306ae8da11f8b7f0c5b933e191a2f224
CREDO23 <thierrybakera12@gmail.com>
2024-07-23 19:42:44 +0200

    feat: connect the calendar activity component with the api

--- apps/web/lib/features/activity/calendar.tsx
+++ apps/web/lib/features/activity/calendar.tsx
@@ -1,40 +1,24 @@
 'use client';
-import { ResponsiveCalendar } from '@nivo/calendar';
 
-const data = [
-	{ value: 23, day: '2024-01-01' },
-	{ value: 78, day: '2024-01-15' },
-	{ value: 41, day: '2024-02-05' },
-	{ value: 60, day: '2024-02-20' },
-	{ value: 12, day: '2024-03-10' },
-	{ value: 87, day: '2024-03-25' },
-	{ value: 35, day: '2024-04-05' },
-	{ value: 94, day: '2024-04-20' },
-	{ value: 17, day: '2024-05-01' },
-	{ value: 68, day: '2024-05-15' },
-	{ value: 52, day: '2024-06-05' },
-	{ value: 81, day: '2024-06-20' },
-	{ value: 29, day: '2024-07-10' },
-	{ value: 73, day: '2024-07-25' },
-	{ value: 45, day: '2024-08-05' },
-	{ value: 88, day: '2024-08-20' },
-	{ value: 8, day: '2024-09-01' },
-	{ value: 57, day: '2024-09-15' },
-	{ value: 63, day: '2024-10-05' },
-	{ value: 37, day: '2024-10-20' },
-	{ value: 91, day: '2024-11-10' },
-	{ value: 19, day: '2024-11-25' },
-	{ value: 74, day: '2024-12-05' },
-	{ value: 5, day: '2024-12-20' },
-	{ value: 23, day: '2024-01-01' }
-	// Add more objects as needed
-];
+import { useTimeLogs } from '@app/hooks/features/useTimeLogs';
+import { ResponsiveCalendar } from '@nivo/calendar';
+import { useEffect, useState } from 'react';
+import { CalendarDatum } from '@nivo/calendar';
 
 export function ActivityCalendar() {
+	const { timerLogsDailyReport } = useTimeLogs();
+	const [calendarData, setCalendarData] = useState<CalendarDatum[]>([]);
+
+	useEffect(() => {
+		setCalendarData(
+			timerLogsDailyReport.map((el) => ({ value: Number((el.sum / 3600).toPrecision(2)), day: el.date }))
+		);
+	}, [timerLogsDailyReport]);
+
 	return (
-		<div className=" h-40 w-full text-red-700">
+		<div className=" h-40 w-full">
 			<ResponsiveCalendar
-				data={data}
+				data={calendarData}
 				from="2024-01-01"
 				to="2024-12-12"
 				emptyColor="#ffffff"
