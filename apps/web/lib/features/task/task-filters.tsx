718aefe2957d37519c4f62d4435d17f74ec36d63
Innocent-akim <gentilakili98@gmail.com>
2024-07-25 14:16:44 +0200

    fix: some bug on modal add time

--- apps/web/lib/features/task/task-filters.tsx
+++ apps/web/lib/features/task/task-filters.tsx
@@ -24,13 +24,13 @@ import { SettingFilterIcon } from 'assets/svg';
 import { DailyPlanFilter } from './daily-plan/daily-plan-filter';
 import { Modal, Divider } from 'lib/components';
 import api from '@app/services/client/axios';
-import { IoIosTimer } from "react-icons/io";
 import { DatePicker } from '@components/ui/DatePicker';
 import { PencilSquareIcon } from '@heroicons/react/20/solid';
 import { FaRegCalendarAlt } from "react-icons/fa";
 import { useDateRange } from '@app/hooks/useDateRange';
 import { TaskDatePickerWithRange } from './task-date-range';
 import { format } from 'date-fns';
+import { HiMiniClock } from "react-icons/hi2";
 import '../../../styles/style.css'
 
 
@@ -356,7 +356,7 @@ function InputFilters({ hook, profile }: Props) {
 
 		const hours = Math.floor(diffMinutes / 60);
 		const minutes = diffMinutes % 60;
-		setTimeDifference(`${hours} Hours ${minutes} Minutes`);
+		setTimeDifference(`${String(hours).padStart(2, '0')}h ${String(minutes).padStart(2, '0')}m`);
 	};
 
 	useEffect(() => {
@@ -446,15 +446,15 @@ function InputFilters({ hook, profile }: Props) {
 				className="bg-light--theme-light dark:bg-dark--theme-light py-5 rounded-xl w-full md:min-w-[25vw] md:max-w-fit h-[auto] justify-start"
 				titleClass="text-[16px] font-bold"
 			>
-				<Divider className="mt-4" />
+				{/* <Divider className="mt-4" /> */}
 				<form onSubmit={handleSubmit} className="text-[13px] w-[90%]">
 					<div className="mb-4">
 						<label className="block text-gray-500 mb-1">Date<span className="text-[#de5505e1] ml-1">*</span></label>
-						<div className="w-full p-2 border border-gray-100 rounded-[10px]">
+						<div className="w-full p-2 border border-slate-100 dark:border-slate-600 rounded-[10px]">
 							<DatePicker
 								buttonVariant={'link'}
 								className='dark:bg-dark--theme-light'
-								buttonClassName={'py-3 decoration-transparent h-[0.875rem] w-full flex items-center'}
+								buttonClassName={'decoration-transparent h-[0.875rem] w-full flex items-center'}
 								customInput={
 									<div
 										className={clsxm(
@@ -485,12 +485,12 @@ function InputFilters({ hook, profile }: Props) {
 					<div className="mb-4 flex items-center">
 						<label className="block text-gray-500 mr-2">Billable</label>
 						<div
-							className={`w-12 h-6 flex items-center bg-[#3726a662] rounded-full p-1 cursor-pointer `}
+							className={`w-12 h-6 flex items-center bg-[#6c57f4b7] rounded-full p-1 cursor-pointer `}
 							onClick={() => setIsBillable(!isBillable)}
-							style={isBillable ? { background: 'linear-gradient(to right, #3726a662, transparent)' } : { background: '#3726a662' }}
+							style={isBillable ? { background: 'linear-gradient(to right, #9d91efb7, #8a7bedb7)' } : { background: '#6c57f4b7' }}
 						>
 							<div
-								className={`bg-[#3826A6] w-4 h-4 rounded-full shadow-md transform transition-transform ${isBillable ? 'translate-x-6' : 'translate-x-0'}`}
+								className={` bg-[#3826A6] w-4 h-4 rounded-full shadow-md transform transition-transform ${isBillable ? 'translate-x-6' : 'translate-x-0'}`}
 							/>
 						</div>
 					</div>
@@ -501,7 +501,7 @@ function InputFilters({ hook, profile }: Props) {
 								type="time"
 								value={startTime}
 								onChange={(e) => setStartTime(e.target.value)}
-								className="w-full p-2 border text-[13px] font-bold border-gray-100 rounded-[10px] dark:bg-dark--theme-light"
+								className="w-full p-2 border text-[13px] font-bold border-slate-100 dark:border-slate-600 rounded-[10px] dark:bg-dark--theme-light"
 								required
 							/>
 						</div>
@@ -512,7 +512,7 @@ function InputFilters({ hook, profile }: Props) {
 								type="time"
 								value={endTime}
 								onChange={(e) => setEndTime(e.target.value)}
-								className="w-full p-2 border text-[13px] font-bold border-gray-100 rounded-[10px] dark:bg-dark--theme-light"
+								className="w-full p-2 border text-[13px] font-bold border-slate-100 dark:border-slate-600 rounded-[10px] dark:bg-dark--theme-light"
 								required
 							/>
 						</div>
@@ -521,10 +521,12 @@ function InputFilters({ hook, profile }: Props) {
 					<div className="mb-4 flex items-center">
 						<label className="block text-gray-500 mb-1">Added hours: </label>
 						<div
-							className="ml-[10px] p-[10px] flex items-center font-bold border-[#410a504e] rounded-[10px]"
-							style={{ background: 'linear-gradient(to right, #0085c8a4, #410a504e )' }}
+							className="ml-[10px] p-1 flex items-center font-bold dark:border-regal-rose border rounded-[10px] pr-3"
+						// style={{ background: 'linear- (to right, #0085c8a4, #410a504e )' }}
 						>
-							<IoIosTimer size={20} className="mr-[10px]" />
+							<div className='mr-[10px] bg-gradient-to-tl from-[#3826A6] dark:from-regal-rose to-regal-blue rounded-full '>
+								<HiMiniClock size={20} className="bg-gradient-to-tl from-regal-rose to-regal-blue rounded-full text-[#8a7bedb7] dark:text-to-regal-rose" />
+							</div>
 							{timeDifference}
 						</div>
 					</div>
@@ -534,7 +536,9 @@ function InputFilters({ hook, profile }: Props) {
 						<SelectItems items={members!}
 							onValueChange={(value) => setTeam(value)}
 							itemId={(member) => (member.id)}
-							itemToString={(member: any) => member?.employee?.user?.firstName} />
+							itemToString={(member: any) => member?.employee?.user?.firstName}
+							triggerClassName='border-slate-100 dark:border-slate-600'
+						/>
 					</div>
 
 					<div className="mb-4">
@@ -542,7 +546,9 @@ function InputFilters({ hook, profile }: Props) {
 						<SelectItems items={tasks}
 							onValueChange={(value) => setTask(value)}
 							itemId={(task) => (task.id)}
-							itemToString={(task) => task.title} />
+							itemToString={(task) => task.title}
+							triggerClassName='border-slate-100 dark:border-slate-600'
+						/>
 					</div>
 
 					<div className="mb-4">
@@ -551,7 +557,7 @@ function InputFilters({ hook, profile }: Props) {
 							value={description}
 							placeholder="What worked on?"
 							onChange={(e) => setDescription(e.target.value)}
-							className="w-full p-2 border border-gray-100 rounded-[10px] dark:bg-dark--theme-light"
+							className="w-full p-2 border border-slate-100 dark:border-slate-600 rounded-[10px] dark:bg-dark--theme-light"
 						/>
 					</div>
 
@@ -560,12 +566,12 @@ function InputFilters({ hook, profile }: Props) {
 						<textarea
 							value={reason}
 							onChange={(e) => setReason(e.target.value)}
-							className="w-full p-2 border border-gray-100 rounded-[10px] dark:bg-dark--theme-light"
+							className="w-full p-2 border border-slate-100 dark:border-slate-600 rounded-[10px] dark:bg-dark--theme-light"
 						/>
 					</div>
 
 					<div className="flex justify-between items-center w-full">
-						<Button variant='outline' type="button" className="text-[#3826A6] font-bold p-[12px] h-11 rounded-[10px] border-[1px] dark:bg-dark--theme-light">View timesheet</Button>
+						<Button variant='outline' type="button" className="dark:text-primary border-slate-100 dark:border-slate-600 font-bold p-[12px] h-11 rounded-[10px] border-[0.5px] dark:bg-dark--theme-light">View timesheet</Button>
 						<Button loading={loading1} disabled={loading1} type="submit" className="bg-[#3826A6] font-bold  flex items-center text-white p-[12px] rounded-[10px]">
 							Add time
 						</Button>
