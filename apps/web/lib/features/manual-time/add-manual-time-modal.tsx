7aa6d1cffaf9ef2d12498052a9df277d488c5e21
CREDO23 <thierrybakera12@gmail.com>
2024-07-30 23:01:27 +0200

    feat: add predefined reson to the select input

--- apps/web/lib/features/manual-time/add-manual-time-modal.tsx
+++ apps/web/lib/features/manual-time/add-manual-time-modal.tsx
@@ -5,7 +5,7 @@ import { clsxm } from '@app/utils';
 import { DatePicker } from '@components/ui/DatePicker';
 import { PencilSquareIcon } from '@heroicons/react/20/solid';
 import { format } from 'date-fns';
-import { useState, useEffect, FormEvent } from 'react';
+import { useState, useEffect, FormEvent, useCallback } from 'react';
 import { Button, SelectItems, Modal } from 'lib/components';
 import { FaRegCalendarAlt } from 'react-icons/fa';
 import { HiMiniClock } from 'react-icons/hi2';
@@ -22,18 +22,17 @@ export function AddManualTimeModal(props: IAddManualTimeModalProps) {
 	const t = useTranslations();
 	const [isBillable, setIsBillable] = useState<boolean>(false);
 	const [description, setDescription] = useState<string>('');
-	const [reason] = useState<string>('');
+	const [reason, setReason] = useState<string>('');
 	const [errorMsg, setError] = useState<string>('');
-	const [loading1, setLoading1] = useState<boolean>(false);
+	const [loading, setLoading] = useState<boolean>(false);
 	const [endTime, setEndTime] = useState<string>('');
 	const [date, setDate] = useState<Date>(new Date());
 	const [startTime, setStartTime] = useState<string>('');
-	const [team, setTeam] = useState<string>('');
-	const [task, setTask] = useState<string>('');
+	const [teamId, setTeamId] = useState<string>('');
+	const [taskId, setTaskId] = useState<string>('');
 	const [timeDifference, setTimeDifference] = useState<string>('');
-	const { tasks } = useTeamTasks();
-	const { activeTeam } = useOrganizationTeams();
-	const members = activeTeam?.members;
+	const { activeTeamTask, tasks, activeTeamId } = useTeamTasks();
+	const { teams } = useOrganizationTeams();
 
 	useEffect(() => {
 		const now = new Date();
@@ -44,44 +43,47 @@ export function AddManualTimeModal(props: IAddManualTimeModalProps) {
 		setEndTime(currentTime);
 	}, []);
 
-	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
+	const handleSubmit = useCallback(
+		(e: FormEvent<HTMLFormElement>) => {
+			e.preventDefault();
 
-		const timeObject = {
-			date,
-			isBillable,
-			startTime,
-			endTime,
-			team,
-			task,
-			description,
-			reason,
-			timeDifference
-		};
-
-		if (date && startTime && endTime && team && task) {
-			setLoading1(true);
-			setError('');
-			const postData = async () => {
-				try {
-					const response = await api.post('/add_time', timeObject);
-					if (response.data.message) {
-						setLoading1(false);
-						closeModal();
-					}
-				} catch (err) {
-					setError('Failed to post data');
-					setLoading1(false);
-				}
+			const timeObject = {
+				date,
+				isBillable,
+				startTime,
+				endTime,
+				teamId,
+				taskId,
+				description,
+				reason,
+				timeDifference
 			};
 
-			postData();
-		} else {
-			setError(`Please complete all required fields with a ${'*'}`);
-		}
-	};
+			if (date && startTime && endTime && teamId && taskId) {
+				setLoading(true);
+				setError('');
+				const postData = async () => {
+					try {
+						const response = await api.post('/add_time', timeObject);
+						if (response.data.message) {
+							setLoading(false);
+							closeModal();
+						}
+					} catch (err) {
+						setError('Failed to post data');
+						setLoading(false);
+					}
+				};
 
-	const calculateTimeDifference = () => {
+				postData();
+			} else {
+				setError(`Please complete all required fields with a ${'*'}`);
+			}
+		},
+		[closeModal, date, description, endTime, isBillable, reason, startTime, taskId, teamId, timeDifference]
+	);
+
+	const calculateTimeDifference = useCallback(() => {
 		const [startHours, startMinutes] = startTime.split(':').map(Number);
 		const [endHours, endMinutes] = endTime.split(':').map(Number);
 
@@ -96,36 +98,35 @@ export function AddManualTimeModal(props: IAddManualTimeModalProps) {
 		const hours = Math.floor(diffMinutes / 60);
 		const minutes = diffMinutes % 60;
 		setTimeDifference(`${String(hours).padStart(2, '0')}h ${String(minutes).padStart(2, '0')}m`);
-	};
-
-	useEffect(() => {
-		calculateTimeDifference();
 	}, [endTime, startTime]);
 
 	useEffect(() => {
-		if (task == '') {
-			setTask(tasks[0]?.id);
+		calculateTimeDifference();
+	}, [calculateTimeDifference, endTime, startTime]);
+
+	useEffect(() => {
+		if (activeTeamTask) {
+			setTaskId(activeTeamTask.id);
 		}
-		if (team == '') {
-			members && setTeam(members[0].id);
+		if (activeTeamId) {
+			setTeamId(activeTeamId);
 		}
-	}, [tasks, members]);
+	}, [activeTeamTask, activeTeamId]);
 
 	return (
 		<Modal
 			isOpen={isOpen}
 			closeModal={closeModal}
 			title={'Add time'}
-			className="bg-light--theme-light dark:bg-dark--theme-light py-5 rounded-xl w-full md:min-w-[25vw] md:max-w-fit h-[auto] justify-start shadow-xl"
-			titleClass="text-[16px] font-bold"
+			className="bg-light--theme-light dark:bg-dark--theme-light p-5 rounded-xl w-full md:w-40 md:min-w-[24rem] h-[auto] justify-start shadow-xl"
+			titleClass="font-bold"
 		>
-			{/* <Divider className="mt-4" /> */}
-			<form onSubmit={handleSubmit} className="text-[13px] w-[90%]">
-				<div className="mb-4">
+			<form onSubmit={handleSubmit} className="text-sm w-[90%] md:w-full  flex flex-col justify-between gap-4">
+				<div className="">
 					<label className="block text-gray-500 mb-1">
 						Date<span className="text-[#de5505e1] ml-1">*</span>
 					</label>
-					<div className="w-full p-2 border border-slate-100 dark:border-slate-600 rounded-[10px]">
+					<div className="w-full p-2 border border-slate-100 dark:border-slate-600 rounded-md">
 						<DatePicker
 							buttonVariant={'link'}
 							className="dark:bg-dark--theme-light"
@@ -160,7 +161,7 @@ export function AddManualTimeModal(props: IAddManualTimeModalProps) {
 					</div>
 				</div>
 
-				<div className="mb-4 flex items-center">
+				<div className=" flex items-center">
 					<label className="block text-gray-500 mr-2">Billable</label>
 					<div
 						className={`w-12 h-6 flex items-center bg-[#6c57f4b7] rounded-full p-1 cursor-pointer `}
@@ -177,7 +178,7 @@ export function AddManualTimeModal(props: IAddManualTimeModalProps) {
 					</div>
 				</div>
 				<div className="flex items-center">
-					<div className="mb-4 w-[48%] mr-[4%]">
+					<div className=" w-[48%] mr-[4%]">
 						<label className="block text-gray-500 mb-1">
 							Start time<span className="text-[#de5505e1] ml-1">*</span>
 						</label>
@@ -185,12 +186,12 @@ export function AddManualTimeModal(props: IAddManualTimeModalProps) {
 							type="time"
 							value={startTime}
 							onChange={(e) => setStartTime(e.target.value)}
-							className="w-full p-2 border text-[13px] font-bold border-slate-100 dark:border-slate-600 rounded-[10px] dark:bg-dark--theme-light"
+							className="w-full p-2 border font-bold border-slate-100 dark:border-slate-600 dark:bg-dark--theme-light rounded-md"
 							required
 						/>
 					</div>
 
-					<div className="mb-4 w-[48%]">
+					<div className=" w-[48%]">
 						<label className="block text-gray-500 mb-1">
 							End time<span className="text-[#de5505e1] ml-1">*</span>
 						</label>
@@ -199,18 +200,15 @@ export function AddManualTimeModal(props: IAddManualTimeModalProps) {
 							type="time"
 							value={endTime}
 							onChange={(e) => setEndTime(e.target.value)}
-							className="w-full p-2 border text-[13px] font-bold border-slate-100 dark:border-slate-600 rounded-[10px] dark:bg-dark--theme-light"
+							className="w-full p-2 border font-bold border-slate-100 dark:border-slate-600 dark:bg-dark--theme-light rounded-md"
 							required
 						/>
 					</div>
 				</div>
 
-				<div className="mb-4 flex items-center">
+				<div className=" flex items-center">
 					<label className="block text-gray-500 mb-1">Added hours: </label>
-					<div
-						className="ml-[10px] p-1 flex items-center font-bold dark:border-regal-rose border rounded-[10px] pr-3"
-						// style={{ background: 'linear- (to right, #0085c8a4, #410a504e )' }}
-					>
+					<div className="ml-[10px] p-1 flex items-center font-bold dark:border-regal-rose rounded-md border pr-3">
 						<div className="mr-[10px] bg-gradient-to-tl from-[#3826A6] dark:from-regal-rose to-regal-blue rounded-full ">
 							<HiMiniClock
 								size={20}
@@ -221,67 +219,66 @@ export function AddManualTimeModal(props: IAddManualTimeModalProps) {
 					</div>
 				</div>
 
-				<div className="mb-4">
+				<div className="">
 					<label className="block text-gray-500 mb-1">
 						Team<span className="text-[#de5505e1] ml-1">*</span>
 					</label>
 					<SelectItems
-						items={members!}
-						onValueChange={(value) => setTeam(value)}
-						itemId={(member) => member.id}
-						itemToString={(member: any) => member?.employee?.user?.firstName}
+						items={teams}
+						onValueChange={(value) => setTeamId(value)}
+						itemId={(team) => team.id}
+						itemToString={(team) => team.name}
 						triggerClassName="border-slate-100 dark:border-slate-600"
 					/>
 				</div>
 
-				<div className="mb-4">
+				<div className="">
 					<label className="block text-gray-500 mb-1">
 						Task<span className="text-[#de5505e1] ml-1">*</span>
 					</label>
 					<SelectItems
 						items={tasks}
-						onValueChange={(value) => setTask(value)}
+						onValueChange={(value) => setTaskId(value)}
 						itemId={(task) => task.id}
 						itemToString={(task) => task.title}
 						triggerClassName="border-slate-100 dark:border-slate-600"
 					/>
 				</div>
 
-				<div className="mb-4">
-					<label className="block text-gray-500 mb-1">Description</label>
+				<div className="flex flex-col">
+					<label className="block text-gray-500 shrink-0">Description</label>
 					<textarea
 						value={description}
 						placeholder="What worked on?"
 						onChange={(e) => setDescription(e.target.value)}
-						className="w-full p-2 border border-slate-100 dark:border-slate-600 rounded-[10px] dark:bg-dark--theme-light"
+						className="w-full p-2 grow border border-slate-100 dark:border-slate-600 dark:bg-dark--theme-light rounded-md"
 					/>
 				</div>
 
-				<div className="mb-4">
+				<div className="">
 					<label className="block text-gray-500 mb-1">Reason</label>
-					<select defaultValue={t('manualTime.reasons.DEFAULT')} name="reason" id="reason">
-						<option>{t('manualTime.reasons.DEFAULT')}</option>
-						{manualTimeReasons.map((reason, index) => (
-							<option value={reason} key={index}>
-								{t(`manualTime.reasons.${reason}`)}
-							</option>
-						))}
-					</select>
+					<SelectItems
+						items={manualTimeReasons.map((reason) => t(`manualTime.reasons.${reason}`))}
+						onValueChange={(reason) => setReason(reason)}
+						itemId={(reason) => reason}
+						itemToString={(reason) => reason}
+						triggerClassName="border-slate-100 dark:border-slate-600"
+					/>
 				</div>
 
-				<div className="flex justify-between items-center w-full">
+				<div className="flex justify-between items-center w-full pt-6">
 					<Button
 						variant="outline"
 						type="button"
-						className="dark:text-primary border-slate-100 dark:border-slate-600 font-bold p-[12px] h-11 rounded-[10px] border-[0.5px] dark:bg-dark--theme-light"
+						className="dark:text-primary border-slate-100 dark:border-slate-600 font-bold  dark:bg-dark--theme-light"
 					>
 						View timesheet
 					</Button>
 					<Button
-						loading={loading1}
-						disabled={loading1}
+						loading={loading}
+						disabled={loading}
 						type="submit"
-						className="bg-[#3826A6] font-bold  flex items-center text-white p-[12px] rounded-[10px]"
+						className="bg-[#3826A6] font-bold  flex items-center text-white "
 					>
 						Add time
 					</Button>
