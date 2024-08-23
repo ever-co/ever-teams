6c6718dcaa120dea0e583cc981576e2ba666ebce
Innocent-akim <gentilakili98@gmail.com>
2024-08-23 22:50:28 +0200

    feat: add badget status, select filter, button delete

--- apps/web/lib/features/integrations/calendar/setup-time-sheet.tsx
+++ apps/web/lib/features/integrations/calendar/setup-time-sheet.tsx
@@ -1,9 +1,12 @@
 "use client"
 
 import React from 'react'
-import { DataTableTimeSheet } from './table-time-sheet'
+import { DataTableTimeSheet, SelectFilter } from './table-time-sheet'
 import { HeadTimeSheet } from '@app/[locale]/calendar/component'
-import { timesheetCalendar } from './helper-calendar'
+import { statusOptions, timesheetCalendar } from './helper-calendar'
+import { StatusBadge } from './confirm-change-status'
+import { RiDeleteBinLine } from "react-icons/ri";
+
 
 interface ISetupTimeSheetProps {
     timesheet?: timesheetCalendar
@@ -17,7 +20,24 @@ export function SetupTimeSheet({ timesheet }: ISetupTimeSheetProps) {
                 <div className='border border-gray-100 dark:border-gray-700 w-full'></div>
                 <HeadTimeSheet timesheet={timesheet} />
             </div>
-            <div className='flex h-[780px] border border-gray-200 dark:border-gray-700 rounded-lg bg-white dark:bg-dark--theme-light p-4 shadow-lg shadow-gray-100  dark:shadow-gray-700'>
+            <div className='flex flex-col h-[780px] border border-gray-200 dark:border-gray-700 rounded-lg bg-white dark:bg-dark--theme-light p-4 shadow-lg shadow-gray-100  dark:shadow-gray-700'>
+                <div className='w-full pb-3 flex items-center gap-x-4 justify-between'>
+                    <div className='flex gap-x-4'>
+                        <span className='font-medium'>123 Logs Selected</span>
+                        <SelectFilter selectedStatus='Rejected' />
+                        <button className='border flex items-center gap-2 border-gray-200 dark:border-gray-700 text-red-500 h-8 px-2 rounded-md' >
+                            <RiDeleteBinLine />
+                            <span className='!font-normal text-sm'>Delete</span>
+                        </button>
+                    </div>
+                    <div className='flex gap-x-2 h-6'>
+                        <StatusBadge selectedStatus='77:00h' />
+                        {statusOptions.map((itens, index) => (
+                            <StatusBadge key={index} selectedStatus={itens.value} filterNumber={`${index + 2}`} />
+
+                        ))}
+                    </div>
+                </div>
                 <DataTableTimeSheet />
             </div>
         </div>
