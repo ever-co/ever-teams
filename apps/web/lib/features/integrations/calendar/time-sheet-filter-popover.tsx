f1ead7ae3f0905f0f0e27fa8462364e479722b7b
Innocent-akim <gentilakili98@gmail.com>
2024-08-29 22:40:45 +0200

    feat: modal for filter and improv mult-select

--- apps/web/lib/features/integrations/calendar/time-sheet-filter-popover.tsx
+++ apps/web/lib/features/integrations/calendar/time-sheet-filter-popover.tsx
@@ -21,15 +21,15 @@ export function TimeSheetFilter({ closeModal, isOpen }: TimeSheetFilterProps) {
                 isOpen={isOpen}
                 closeModal={closeModal}
                 title={"Filters"}
-                className="bg-light--theme-light dark:bg-dark--theme-light p-5 rounded-xl !w-[400px]  md:w-40 md:min-w-[24rem] h-[auto] justify-start shadow-xl"
+                className="bg-light--theme-light dark:bg-dark--theme-light p-5 rounded-xl !w-[350px]  md:w-40 md:min-w-[24rem] h-[auto] justify-start shadow-xl"
                 titleClass="font-bold"
             >
                 <div className="w-full flex flex-col gap-5 ">
-                    <div className="grid gap-3">
+                    <div className="grid gap-5">
 
                         <div className="">
-                            <label className="block text-gray-500 mb-1">
-                                Team<span className="text-[#de5505e1] ml-1">*</span>
+                            <label className="block text-gray-600 mb-1 text-sm">
+                                <span className="text-sm">Team</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
                             </label>
                             <SelectItems
                                 defaultValue={activeTeam}
@@ -37,36 +37,60 @@ export function TimeSheetFilter({ closeModal, isOpen }: TimeSheetFilterProps) {
                                 onValueChange={(team) => setTeamId(team ? team.id : '')}
                                 itemId={(team) => (team ? team.id : '')}
                                 itemToString={(team) => (team ? team.name : teamId)}
-                                triggerClassName="border-slate-100 dark:border-slate-600"
+                                triggerClassName="border border-gray-200 dark:border-gray-600"
                             />
                         </div>
                         <div className="">
-                            <label className="block text-gray-500 mb-1">
-                                Employee<span className="text-[#de5505e1] ml-1">*</span>
+                            <label className="block text-gray-600 mb-1 text-sm">
+                                <span className="text-sm">Employee</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
                             </label>
                             <SelectItems
                                 items={activeTeam?.members ?? []}
                                 onValueChange={(members) => setMembersId(members ? members.id : '')}
                                 itemId={(members) => (members ? members.id : membersId)}
                                 itemToString={(members) => (members ? members.employee.fullName : '')}
-                                triggerClassName="border-slate-100 dark:border-slate-600"
+                                triggerClassName="border border-gray-200 dark:border-gray-600"
                             />
                         </div>
                         <div className="">
-                            <label className="block text-gray-500 mb-1">
-                                Status<span className="text-[#de5505e1] ml-1">*</span>
+                            <label className="block text-gray-600 mb-1 text-sm">
+                                <span className="text-sm">Status</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
                             </label>
                             <SelectItems
                                 items={statusOptions}
                                 onValueChange={(value) => setStatus(value.value ? value.value : status)}
                                 itemId={(value) => value.label}
                                 itemToString={(value) => (value.label)}
-                                triggerClassName="border-slate-100 dark:border-slate-600"
+                                triggerClassName="border border-gray-200 dark:border-gray-600"
                             />
                         </div>
                         <div className="">
-                            <label className="block text-gray-500 mb-1">
-                                Task<span className="text-[#de5505e1] ml-1">*</span>
+                            <label className="block text-gray-600 mb-1 text-sm">
+                                <span className="text-sm">Status</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
+                            </label>
+                            <SelectItems
+                                items={statusOptions}
+                                onValueChange={(value) => setStatus(value.value ? value.value : status)}
+                                itemId={(value) => value.label}
+                                itemToString={(value) => (value.label)}
+                                triggerClassName="border border-gray-200 dark:border-gray-600"
+                            />
+                        </div>
+                        <div className="">
+                            <label className="block text-gray-600 mb-1 text-sm">
+                                <span className="text-sm">Status</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
+                            </label>
+                            <SelectItems
+                                items={statusOptions}
+                                onValueChange={(value) => setStatus(value.value ? value.value : status)}
+                                itemId={(value) => value.label}
+                                itemToString={(value) => (value.label)}
+                                triggerClassName="border border-gray-200 dark:border-gray-600"
+                            />
+                        </div>
+                        <div className="">
+                            <label className="block text-gray-600 mb-1 text-sm">
+                                <span className="text-sm">Status</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
                             </label>
                             <SelectItems
                                 defaultValue={activeTeamTask}
@@ -74,18 +98,18 @@ export function TimeSheetFilter({ closeModal, isOpen }: TimeSheetFilterProps) {
                                 onValueChange={(task) => setTaskId(task ? task.id : taskId)}
                                 itemId={(task) => (task ? task.id : '')}
                                 itemToString={(task) => (task ? task.title : '')}
-                                triggerClassName="border-slate-100 dark:border-slate-600"
+                                triggerClassName="border border-slate-200 dark:border-slate-600"
                             />
                         </div>
                         <div className="flex items-center justify-between gap-x-4 w-full">
                             <Button
                                 variant={'outline'}
-                                className='flex items-center justify-center h-10 w-full rounded-lg  dark:text-gray-300' >
-                                <span>Clear Filter</span>
+                                className='flex items-center text-sm justify-center h-10 w-full rounded-lg  dark:text-gray-300' >
+                                <span className="text-sm">Clear Filter</span>
                             </Button>
                             <Button
-                                className='flex items-center justify-center h-10 w-full rounded-lg bg-primary dark:bg-primary-light dark:text-gray-300' >
-                                <span>Apply Filter</span>
+                                className='flex items-center text-sm justify-center h-10 w-full rounded-lg bg-primary dark:bg-primary-light dark:text-gray-300' >
+                                <span className="text-sm">Apply Filter</span>
                             </Button>
                         </div>
                     </div>
