d0f1ab2606ee602c7f899ca2d9201845fb4dc642
Innocent-akim <gentilakili98@gmail.com>
2024-08-30 11:52:50 +0200

    feat: MultiSelect component for universal variable naming and improved reusability in Modal Filter

--- apps/web/lib/features/integrations/calendar/time-sheet-filter-popover.tsx
+++ apps/web/lib/features/integrations/calendar/time-sheet-filter-popover.tsx
@@ -1,20 +1,18 @@
-import { useState } from "react";
 import { useOrganizationTeams, useTeamTasks } from "@app/hooks";
 import { Button } from "@components/ui/button"
-import { Modal, SelectItems } from "lib/components";
+import { Modal } from "lib/components";
 import { statusOptions } from "@app/constants";
+import { MultiSelect } from "lib/components/custom-select/multi-select";
+import { Input } from "@components/ui/input";
 interface TimeSheetFilterProps {
     isOpen: boolean,
     closeModal: () => void
 }
 
 export function TimeSheetFilter({ closeModal, isOpen }: TimeSheetFilterProps) {
-    const { teams, activeTeam } = useOrganizationTeams();
-    const { activeTeamTask, tasks } = useTeamTasks();
-    const [status, setStatus] = useState('');
-    const [taskId, setTaskId] = useState<string>('');
-    const [teamId, setTeamId] = useState<string>('');
-    const [membersId, setMembersId] = useState('')
+    const { activeTeam } = useOrganizationTeams();
+    const { tasks } = useTeamTasks();
+    // const [taskId, setTaskId] = useState<ITeamTask | ITeamTask[] | null>([]);
     return (
         <>
             <Modal
@@ -24,82 +22,100 @@ export function TimeSheetFilter({ closeModal, isOpen }: TimeSheetFilterProps) {
                 className="bg-light--theme-light dark:bg-dark--theme-light p-5 rounded-xl !w-[350px]  md:w-40 md:min-w-[24rem] h-[auto] justify-start shadow-xl"
                 titleClass="font-bold"
             >
-                <div className="w-full flex flex-col gap-5 ">
+                <div className="w-full flex flex-col">
                     <div className="grid gap-5">
 
                         <div className="">
                             <label className="block text-gray-600 mb-1 text-sm">
-                                <span className="text-sm">Team</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
+                                <span className="text-[12px]">Project</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
                             </label>
-                            <SelectItems
-                                defaultValue={activeTeam}
-                                items={teams}
-                                onValueChange={(team) => setTeamId(team ? team.id : '')}
-                                itemId={(team) => (team ? team.id : '')}
-                                itemToString={(team) => (team ? team.name : teamId)}
-                                triggerClassName="border border-gray-200 dark:border-gray-600"
-                            />
-                        </div>
-                        <div className="">
-                            <label className="block text-gray-600 mb-1 text-sm">
-                                <span className="text-sm">Employee</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
-                            </label>
-                            <SelectItems
+                            <MultiSelect
                                 items={activeTeam?.members ?? []}
-                                onValueChange={(members) => setMembersId(members ? members.id : '')}
-                                itemId={(members) => (members ? members.id : membersId)}
                                 itemToString={(members) => (members ? members.employee.fullName : '')}
-                                triggerClassName="border border-gray-200 dark:border-gray-600"
+                                itemId={(item) => item.id}
+                                onValueChange={(selectedItems) => console.log(selectedItems)}
+                                multiSelect={true} // Enable multi-select
+                                triggerClassName="dark:border-gray-700"
+
                             />
                         </div>
                         <div className="">
                             <label className="block text-gray-600 mb-1 text-sm">
-                                <span className="text-sm">Status</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
+                                <span className="text-[12px]">Employee</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
                             </label>
-                            <SelectItems
+                            <MultiSelect
+                                items={activeTeam?.members ?? []}
+                                itemToString={(members) => (members ? members.employee.fullName : '')}
+                                itemId={(item) => item.id}
+                                onValueChange={(selectedItems) => console.log(selectedItems)}
+                                multiSelect={true} // Enable multi-select
+                                triggerClassName="dark:border-gray-700"
+
+                            />
+                        </div>
+                        <div className="">
+                            <label className="block text-gray-600 mb-1 text-sm">
+                                <span className="text-[12px]">Status</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
+                            </label>
+                            <MultiSelect
                                 items={statusOptions}
-                                onValueChange={(value) => setStatus(value.value ? value.value : status)}
-                                itemId={(value) => value.label}
-                                itemToString={(value) => (value.label)}
-                                triggerClassName="border border-gray-200 dark:border-gray-600"
+                                itemToString={(status) => (status ? status.value : '')}
+                                itemId={(item) => item.value}
+                                onValueChange={(selectedItems) => console.log(selectedItems)}
+                                multiSelect={true} // Enable multi-select
+                                triggerClassName="dark:border-gray-700"
+
                             />
                         </div>
                         <div className="">
                             <label className="block text-gray-600 mb-1 text-sm">
-                                <span className="text-sm">Status</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
+                                <span className="text-[12px]">Task</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
                             </label>
-                            <SelectItems
-                                items={statusOptions}
-                                onValueChange={(value) => setStatus(value.value ? value.value : status)}
-                                itemId={(value) => value.label}
-                                itemToString={(value) => (value.label)}
-                                triggerClassName="border border-gray-200 dark:border-gray-600"
-                            />
-                        </div>
-                        <div className="">
-                            <label className="block text-gray-600 mb-1 text-sm">
-                                <span className="text-sm">Status</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
-                            </label>
-                            <SelectItems
-                                items={statusOptions}
-                                onValueChange={(value) => setStatus(value.value ? value.value : status)}
-                                itemId={(value) => value.label}
-                                itemToString={(value) => (value.label)}
-                                triggerClassName="border border-gray-200 dark:border-gray-600"
-                            />
-                        </div>
-                        <div className="">
-                            <label className="block text-gray-600 mb-1 text-sm">
-                                <span className="text-sm">Status</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
-                            </label>
-                            <SelectItems
-                                defaultValue={activeTeamTask}
+                            <MultiSelect
                                 items={tasks}
-                                onValueChange={(task) => setTaskId(task ? task.id : taskId)}
+                                onValueChange={(task) => task}
                                 itemId={(task) => (task ? task.id : '')}
                                 itemToString={(task) => (task ? task.title : '')}
-                                triggerClassName="border border-slate-200 dark:border-slate-600"
+                                multiSelect={true} // Enable multi-select
+                                triggerClassName="dark:border-gray-700"
+
                             />
+                        </div>
+                        <div className="">
+                            <label className="block text-gray-600 mb-1 text-sm">
+                                <span className="text-[12px]">Activity Level</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
+                            </label>
+                            <MultiSelect
+                                items={activeTeam?.members ?? []}
+                                itemToString={(members) => (members ? members.employee.fullName : '')}
+                                itemId={(item) => item.id}
+                                onValueChange={(selectedItems) => console.log(selectedItems)}
+                                multiSelect={true} // Enable multi-select
+                                triggerClassName="dark:border-gray-700"
+                            />
+                        </div>
+                        <div className="">
+                            <label className="block text-gray-600 mb-1 text-sm">
+                                <span className="text-[12px]">Worked hours</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
+                            </label>
+                            <div className="flex items-center w-full justify-between gap-x-4">
+                                <Input
+                                    className="border w-full dark:border-gray-700"
+                                    placeholder="Time"
+                                    required
+                                    value=""
+                                    name="from"
+                                />
+                                <span>to</span>
+                                <Input
+                                    className="border w-full dark:border-gray-700"
+                                    placeholder="Time"
+                                    required
+                                    value=""
+                                    name="to"
+                                />
+                            </div>
+
                         </div>
                         <div className="flex items-center justify-between gap-x-4 w-full">
                             <Button
