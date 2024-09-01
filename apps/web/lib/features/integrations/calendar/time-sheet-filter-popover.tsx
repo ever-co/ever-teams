46fbd9e5f541465b310fe17cd5520be9de10c043
AKILIMAILI CIZUNGU Innocent <51681130+Innocent-Akim@users.noreply.github.com>
2024-09-01 21:25:47 +0200

    [Feat]: implement multi-select dropdown with persistent open state for  (#2955)
    * feat: implement multi-select dropdown with persistent open state for multiple selections
    
    * feat: modal for filter and improv mult-select
    
    * feat: MultiSelect component for universal variable naming and improved reusability in Modal Filter

--- apps/web/lib/features/integrations/calendar/time-sheet-filter-popover.tsx
+++ apps/web/lib/features/integrations/calendar/time-sheet-filter-popover.tsx
@@ -1,104 +1,136 @@
-import { useState } from "react";
 import { useOrganizationTeams, useTeamTasks } from "@app/hooks";
 import { Button } from "@components/ui/button"
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@components/ui/popover"
-import { SettingFilterIcon } from "assets/svg"
-import { SelectItems } from "lib/components";
+import { Modal } from "lib/components";
 import { statusOptions } from "@app/constants";
+import { MultiSelect } from "lib/components/custom-select/multi-select";
+import { Input } from "@components/ui/input";
+interface TimeSheetFilterProps {
+    isOpen: boolean,
+    closeModal: () => void
+}
 
-
-export function TimeSheetFilter() {
-
-    const { teams, activeTeam } = useOrganizationTeams();
-    const { activeTeamTask, tasks } = useTeamTasks();
-    const [status, setStatus] = useState('');
-    const [taskId, setTaskId] = useState<string>('');
-    const [teamId, setTeamId] = useState<string>('');
-    const [membersId, setMembersId] = useState('')
-
-
+export function TimeSheetFilter({ closeModal, isOpen }: TimeSheetFilterProps) {
+    const { activeTeam } = useOrganizationTeams();
+    const { tasks } = useTeamTasks();
+    // const [taskId, setTaskId] = useState<ITeamTask | ITeamTask[] | null>([]);
     return (
-        <Popover>
-            <PopoverTrigger asChild>
-                <Button
-                    className='flex items-center justify-center h-10 rounded-lg bg-white dark:bg-dark--theme-light gap-x-3 border dark:border-gray-700 hover:bg-white' >
-                    <SettingFilterIcon className="text-gray-700 dark:text-white w-3.5" strokeWidth="1.8" />
-                    <div className="gap-x-2 flex items-center w-full">
-                        <span className="text-gray-700 dark:text-white">Filter</span>
-                        <div className="bg-gray-700 dark:bg-white h-6 w-6 rounded-full flex items-center  justify-center text-whiten dark:text-gray-700">
-                            <span>6</span>
-                        </div>
-                    </div>
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-[430px] shadow">
-                <div className="w-full flex flex-col gap-4">
-                    <div className="flex items-center justify-center">
-                        <h4 className="font-bold leading-none text-[20px] ">Select Filter</h4>
-                    </div>
-                    <div className="grid gap-3">
+        <>
+            <Modal
+                isOpen={isOpen}
+                closeModal={closeModal}
+                title={"Filters"}
+                className="bg-light--theme-light dark:bg-dark--theme-light p-5 rounded-xl !w-[350px]  md:w-40 md:min-w-[24rem] h-[auto] justify-start shadow-xl"
+                titleClass="font-bold"
+            >
+                <div className="w-full flex flex-col">
+                    <div className="grid gap-5">
 
                         <div className="">
-                            <label className="block text-gray-500 mb-1">
-                                Team<span className="text-[#de5505e1] ml-1">*</span>
+                            <label className="block text-gray-600 mb-1 text-sm">
+                                <span className="text-[12px]">Project</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
                             </label>
-                            <SelectItems
-                                defaultValue={activeTeam}
-                                items={teams}
-                                onValueChange={(team) => setTeamId(team ? team.id : '')}
-                                itemId={(team) => (team ? team.id : '')}
-                                itemToString={(team) => (team ? team.name : teamId)}
-                                triggerClassName="border-slate-100 dark:border-slate-600"
-                            />
-                        </div>
-                        <div className="">
-                            <label className="block text-gray-500 mb-1">
-                                Employee<span className="text-[#de5505e1] ml-1">*</span>
-                            </label>
-                            <SelectItems
+                            <MultiSelect
                                 items={activeTeam?.members ?? []}
-                                onValueChange={(members) => setMembersId(members ? members.id : '')}
-                                itemId={(members) => (members ? members.id : membersId)}
                                 itemToString={(members) => (members ? members.employee.fullName : '')}
-                                triggerClassName="border-slate-100 dark:border-slate-600"
+                                itemId={(item) => item.id}
+                                onValueChange={(selectedItems) => console.log(selectedItems)}
+                                multiSelect={true} // Enable multi-select
+                                triggerClassName="dark:border-gray-700"
+
                             />
                         </div>
                         <div className="">
-                            <label className="block text-gray-500 mb-1">
-                                Status<span className="text-[#de5505e1] ml-1">*</span>
+                            <label className="block text-gray-600 mb-1 text-sm">
+                                <span className="text-[12px]">Employee</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
                             </label>
-                            <SelectItems
+                            <MultiSelect
+                                items={activeTeam?.members ?? []}
+                                itemToString={(members) => (members ? members.employee.fullName : '')}
+                                itemId={(item) => item.id}
+                                onValueChange={(selectedItems) => console.log(selectedItems)}
+                                multiSelect={true} // Enable multi-select
+                                triggerClassName="dark:border-gray-700"
+
+                            />
+                        </div>
+                        <div className="">
+                            <label className="block text-gray-600 mb-1 text-sm">
+                                <span className="text-[12px]">Status</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
+                            </label>
+                            <MultiSelect
                                 items={statusOptions}
-                                onValueChange={(value) => setStatus(value.value ? value.value : status)}
-                                itemId={(value) => value.label}
-                                itemToString={(value) => (value.label)}
-                                triggerClassName="border-slate-100 dark:border-slate-600"
+                                itemToString={(status) => (status ? status.value : '')}
+                                itemId={(item) => item.value}
+                                onValueChange={(selectedItems) => console.log(selectedItems)}
+                                multiSelect={true} // Enable multi-select
+                                triggerClassName="dark:border-gray-700"
+
                             />
                         </div>
                         <div className="">
-                            <label className="block text-gray-500 mb-1">
-                                Task<span className="text-[#de5505e1] ml-1">*</span>
+                            <label className="block text-gray-600 mb-1 text-sm">
+                                <span className="text-[12px]">Task</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
                             </label>
-                            <SelectItems
-                                defaultValue={activeTeamTask}
+                            <MultiSelect
                                 items={tasks}
-                                onValueChange={(task) => setTaskId(task ? task.id : taskId)}
+                                onValueChange={(task) => task}
                                 itemId={(task) => (task ? task.id : '')}
                                 itemToString={(task) => (task ? task.title : '')}
-                                triggerClassName="border-slate-100 dark:border-slate-600"
+                                multiSelect={true} // Enable multi-select
+                                triggerClassName="dark:border-gray-700"
+
                             />
                         </div>
-                        <Button
-                            className='flex items-center justify-center h-10 rounded-lg bg-primary dark:bg-primary-light dark:text-gray-300' >
-                            <span>Add Filter</span>
-                        </Button>
+                        <div className="">
+                            <label className="block text-gray-600 mb-1 text-sm">
+                                <span className="text-[12px]">Activity Level</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
+                            </label>
+                            <MultiSelect
+                                items={activeTeam?.members ?? []}
+                                itemToString={(members) => (members ? members.employee.fullName : '')}
+                                itemId={(item) => item.id}
+                                onValueChange={(selectedItems) => console.log(selectedItems)}
+                                multiSelect={true} // Enable multi-select
+                                triggerClassName="dark:border-gray-700"
+                            />
+                        </div>
+                        <div className="">
+                            <label className="block text-gray-600 mb-1 text-sm">
+                                <span className="text-[12px]">Worked hours</span><span className="text-[#de5505e1] ml-1 text-sm">*</span>
+                            </label>
+                            <div className="flex items-center w-full justify-between gap-x-4">
+                                <Input
+                                    className="border w-full dark:border-gray-700"
+                                    placeholder="Time"
+                                    required
+                                    value=""
+                                    name="from"
+                                />
+                                <span>to</span>
+                                <Input
+                                    className="border w-full dark:border-gray-700"
+                                    placeholder="Time"
+                                    required
+                                    value=""
+                                    name="to"
+                                />
+                            </div>
+
+                        </div>
+                        <div className="flex items-center justify-between gap-x-4 w-full">
+                            <Button
+                                variant={'outline'}
+                                className='flex items-center text-sm justify-center h-10 w-full rounded-lg  dark:text-gray-300' >
+                                <span className="text-sm">Clear Filter</span>
+                            </Button>
+                            <Button
+                                className='flex items-center text-sm justify-center h-10 w-full rounded-lg bg-primary dark:bg-primary-light dark:text-gray-300' >
+                                <span className="text-sm">Apply Filter</span>
+                            </Button>
+                        </div>
                     </div>
                 </div>
-            </PopoverContent>
-        </Popover>
+            </Modal>
+        </>
     )
 }
