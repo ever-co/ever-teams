5e1f43ade5fe48166f36bc49023b82c0dafd6203
Innocent-akim <gentilakili98@gmail.com>
2024-09-04 15:08:45 +0200

    fix: resolve issue with PopoverContent and Command integration

--- apps/web/lib/components/combobox/index.tsx
+++ apps/web/lib/components/combobox/index.tsx
@@ -1,9 +1,8 @@
 "use client"
-
-import * as React from "react"
-import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons"
-import { cn } from "lib/utils"
-import { Button } from "@components/ui/button"
+import * as React from "react";
+import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
+import { cn } from "lib/utils";
+import { Button } from "@components/ui/button";
 import {
     Command,
     CommandEmpty,
@@ -11,13 +10,12 @@ import {
     CommandInput,
     CommandItem,
     CommandList,
-} from "@components/ui/command"
-
+} from "@components/ui/command";
 import {
     Popover,
     PopoverContent,
     PopoverTrigger,
-} from "@components/ui/popover"
+} from "@components/ui/popover";
 
 interface ComboboxProps<T> {
     items: T[]
@@ -30,22 +28,43 @@ interface ComboboxProps<T> {
     noResultsText?: string
     onChangeValue?: (value: T | null) => void
     className?: string
+    popoverClassName?: string
 }
-
+/**
+ *
+ *
+ * @export
+ * @template T
+ * @param {ComboboxProps<T>} {
+ *     items,
+ *     itemToString,
+ *     itemToValue,
+ *     placeholder = "Select item...",
+ *     buttonWidth = "w-[200px]",
+ *     commandInputHeight = "h-9",
+ *     noResultsText = "No item found.",
+ *     onChangeValue,
+ *     className,
+ *     popoverClassName
+ * }
+ * @return {*}
+ */
 export function CustomCombobox<T>({
     items,
     itemToString,
     itemToValue,
     placeholder = "Select item...",
     buttonWidth = "w-[200px]",
-    popoverWidth = "w-[200px]",
     commandInputHeight = "h-9",
     noResultsText = "No item found.",
     onChangeValue,
     className,
+    popoverClassName
 }: ComboboxProps<T>) {
     const [open, setOpen] = React.useState(false)
     const [value, setValue] = React.useState<T | null>(null)
+    const [popoverWidth, setPopoverWidth] = React.useState<number | null>(null);
+    const triggerRef = React.useRef<HTMLButtonElement>(null);
 
     const handleSelect = (currentValue: string) => {
         const selectedItem = items.find(item => itemToValue(item) === currentValue) || null
@@ -56,33 +75,45 @@ export function CustomCombobox<T>({
         }
     }
 
+    React.useEffect(() => {
+        if (triggerRef.current) {
+            setPopoverWidth(triggerRef.current.offsetWidth);
+        }
+    }, [triggerRef.current]);
     return (
         <Popover open={open} onOpenChange={setOpen}>
             <PopoverTrigger asChild>
                 <Button
+                    ref={triggerRef}
                     variant="outline"
                     role="combobox"
                     aria-expanded={open}
-                    className={cn(`${buttonWidth} justify-between`, className)}
+                    className={cn(
+                        'w-full justify-between text-left font-normal h-10 rounded-lg dark:bg-dark--theme-light',
+                        buttonWidth
+                    )}
                 >
-                    {value
-                        ? itemToString(value)
-                        : placeholder}
+                    {value ? itemToString(value) : placeholder}
                     <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                 </Button>
             </PopoverTrigger>
-            <PopoverContent className={`${popoverWidth} p-0`}>
-                <Command>
+            <PopoverContent className={cn(
+                'w-full max-w-full max-h-[80vh] border border-transparent dark:bg-dark--theme-light',
+                popoverClassName
+            )}
+                style={{ width: popoverWidth || 'auto', overflow: 'auto' }}>
+                <Command className="dark:bg-dark--theme-light">
                     <CommandInput placeholder="Search item..." className={commandInputHeight} />
                     <CommandList>
                         <CommandEmpty>{noResultsText}</CommandEmpty>
                         <CommandGroup>
                             {items.map((item) => (
                                 <CommandItem
-                                    className="w-full"
+                                    className="w-full dark:bg-dark--theme-light"
                                     key={itemToValue(item)}
                                     value={itemToValue(item)}
-                                    onSelect={handleSelect}>
+                                    onSelect={() => handleSelect(itemToValue(item))}
+                                >
                                     {itemToString(item)}
                                     <CheckIcon
                                         className={cn(
--- apps/web/lib/features/manual-time/add-manual-time-modal.tsx
+++ apps/web/lib/features/manual-time/add-manual-time-modal.tsx
@@ -14,6 +14,7 @@ import { IAddManualTimeRequest } from '@app/interfaces/timer/ITimerLogs';
 import { cn } from 'lib/utils';
 import { CalendarDays } from 'lucide-react';
 import { IoTime } from 'react-icons/io5';
+
 /**
  * Interface for the properties of the `AddManualTimeModal` component.
  *
