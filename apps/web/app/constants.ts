8f3fd05e7330a3fe162aa061681630ceeb4e4f75
samuel mbabhazi <111171386+samuelmbabhazi@users.noreply.github.com>
2024-09-15 21:17:08 +0200

    Improve branding (#3005)
    * improve branding
    
    * use config instead of .env directly
    
    * use config instead of .env directly
    
    * use config instead of .env directly
    
    * fix logo position
    
    * feedback integration
    
    * add default value
    
    * Update constants.ts
    
    * Update .env
    
    * Update .env.sample
    
    ---------
    
    Co-authored-by: Ruslan Konviser <evereq@gmail.com>

--- apps/web/app/constants.ts
+++ apps/web/app/constants.ts
@@ -77,6 +77,16 @@ export const APP_NAME = process.env.APP_NAME || 'Ever Teams';
 export const APP_SIGNATURE = process.env.APP_SIGNATURE || 'Ever Teams';
 export const APP_LOGO_URL = process.env.APP_LOGO_URL || 'https://app.ever.team/assets/ever-teams.png';
 export const APP_LINK = process.env.APP_LINK || 'https://ever.team/';
+export const APP_SLOGAN_TEXT = process.env.APP_SLOGAN_TEXT || 'Real-Time Clarity, Real-Time Reality™.';
+
+export const COMPANY_NAME = process.env.COMPANY_NAME || 'Ever Co. LTD';
+export const COMPANY_LINK = process.env.COMPANY_LINK || 'https://ever.co';
+
+export const TERMS_LINK = process.env.TERMS_LINK || 'https://ever.team/tos';
+export const PRIVACY_POLICY_LINK = process.env.PRIVACY_POLICY_LINK || 'https://ever.team/privacy';
+
+export const MAIN_PICTURE = process.env.MAIN_PICTURE || '/assets/cover/auth-bg-cover.png';
+export const MAIN_PICTURE_DARK = process.env.MAIN_PICTURE_DARK || '/assets/cover/auth-bg-cover-dark.png';
 
 export const CHARACTER_LIMIT_TO_SHOW = 20;
 
@@ -275,7 +285,7 @@ export const TASKS_ESTIMATE_HOURS_MODAL_DATE = 'tasks-estimate-hours-modal-date'
 export const DAILY_PLAN_ESTIMATE_HOURS_MODAL_DATE = 'daily-plan-estimate-hours-modal';
 export const DEFAULT_PLANNED_TASK_ID = 'default-planned-task-id';
 
-// OAuth providers keys
+// OAuth provider's keys
 
 export const APPLE_CLIENT_ID = process.env.APPLE_CLIENT_ID;
 export const APPLE_CLIENT_SECRET = process.env.APPLE_CLIENT_SECRET;
--- apps/web/lib/layout/auth-layout.tsx
+++ apps/web/lib/layout/auth-layout.tsx
@@ -5,6 +5,7 @@ import Image from 'next/legacy/image';
 import { PropsWithChildren, ReactNode } from 'react';
 import { useTranslations } from 'next-intl';
 import { Footer } from './footer';
+import { MAIN_PICTURE, MAIN_PICTURE_DARK } from '@app/constants';
 
 type Props = {
 	title?: string;
@@ -35,14 +36,15 @@ export function AuthLayout({ children, title, description, isAuthPage = true }: 
 						</div>
 
 						<div className="relative w-[110%] h-full min-h-[800px]">
-							{['auth-bg-cover.png', 'auth-bg-cover-dark.png'].map((image) => {
+							{[MAIN_PICTURE!, MAIN_PICTURE_DARK!].map((image, index) => {
+								const isDarkModeImage = index === 1; // The second image is the dark mode one
 								return (
 									<div
 										className={clsxm(
 											'ml-[12%] rounded-3xl absolute w-full h-full inset-0',
 											[
-												!image.endsWith('dark.png') && ['opacity-100 dark:opacity-0'],
-												image.endsWith('dark.png') && ['opacity-0 dark:opacity-100']
+												!isDarkModeImage && ['opacity-100 dark:opacity-0'],
+												isDarkModeImage && ['opacity-0 dark:opacity-100']
 											],
 											'shadow-[-76px_-13px_244px_-42px_rgba(40,32,72,0.55)]',
 											'dark:shadow-[-76px_-13px_244px_-42px_rgba(0,0,0,0.75)]'
@@ -50,7 +52,7 @@ export function AuthLayout({ children, title, description, isAuthPage = true }: 
 										key={image}
 									>
 										<Image
-											src={`/assets/cover/${image}`}
+											src={image}
 											layout="responsive"
 											objectFit="fill"
 											priority
