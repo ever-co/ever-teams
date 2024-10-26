7a008faf0931ee6a755b796da4b7959706466ed2
Arick Bulakali <arickbulakali@gmail.com>
2024-10-26 13:11:51 +0200

    fix(web): [Vertical Sidenar] Fix fullwidth switch with vertical sidebar

---
+++ apps/web/lib/layout/MainSidebarTrigger.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { cn } from '@/lib/utils';
+import { SidebarTrigger, useSidebar } from '@/components/ui/sidebar';
+const MainSidebarTrigger = () => {
+	const { isMobile, openMobile } = useSidebar();
+	return (
+		<SidebarTrigger
+			className={cn(
+				'size-7 !bg-[#1C75FD] fixed left-[4%] top-[11%] !flex md:!hidden items-center justify-center !rounded-full transition-all duration-300 filter drop-shadow-[0px_0px_6px_rgba(28,117,253,0.30)] z-[55]',
+				isMobile && !openMobile ? '!flex' : '!hidden'
+			)}
+		/>
+	);
+};
+
+export default MainSidebarTrigger;
