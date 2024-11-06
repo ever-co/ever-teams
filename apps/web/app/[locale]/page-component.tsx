1b06f0b2aaec958e4b1bcc88784be4a10b9ae024
Arick Bulakali <arickbulakali@gmail.com>
2024-11-06 11:13:26 +0200

    fix(web): fix main container horizontal scroll

--- apps/web/app/[locale]/page-component.tsx
+++ apps/web/app/[locale]/page-component.tsx
@@ -62,7 +62,7 @@ function MainPage() {
 	}
 	return (
 		<>
-			<div className="flex flex-col justify-between h-screen">
+			<div className="flex flex-col justify-between h-full min-h-screen">
 				{/* <div className="flex-grow "> */}
 				<MainLayout
 					showTimer={headerSize <= 11.8 && isTrackingEnabled}
