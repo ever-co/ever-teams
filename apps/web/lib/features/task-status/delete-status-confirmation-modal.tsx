9b7d39a75a9e6d9749238153cd822bcac03d2879
CREDO23 <thierrybakera12@gmail.com>
2024-11-12 11:31:28 +0200

    update the status to 'open' after deleting a used status

--- apps/web/lib/features/task-status/delete-status-confirmation-modal.tsx
+++ apps/web/lib/features/task-status/delete-status-confirmation-modal.tsx
@@ -1,8 +1,8 @@
-import { ITaskStatusItemList } from '@/app/interfaces';
-import { useTaskStatus } from '@app/hooks';
+import { ITaskStatusItemList, TaskStatusEnum } from '@/app/interfaces';
+import { useTaskStatus, useTeamTasks } from '@app/hooks';
 import { Button, Card, Modal, Text } from 'lib/components';
 import { useTranslations } from 'next-intl';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 
 interface DeleteTaskStatusModalProps {
 	open: boolean;
@@ -26,22 +26,37 @@ export function DeleteTaskStatusConfirmationModal(props: DeleteTaskStatusModalPr
 	const { closeModal, open, status, onCancel } = props;
 	const { deleteTaskStatus, deleteTaskStatusLoading } = useTaskStatus();
 	const t = useTranslations();
+	const { tasks, updateTask } = useTeamTasks();
+
+	// Filter tasks that are using the current status
+	const tasksUsingStatus = useMemo(() => tasks.filter((task) => task.status === status.name), [tasks, status.name]);
 
 	const handleCloseModal = useCallback(() => {
 		onCancel();
 		closeModal();
 	}, [closeModal, onCancel]);
 
-	// The function that will be called when the user confirms the action
+	// Function to handle task status deletion
 	const handleDeleteTaskStatus = useCallback(async () => {
-		try {
-			await deleteTaskStatus(status.id);
-		} catch (error) {
-			console.log(error);
+		if (!status.id) {
+			console.error('Status ID is not provided.');
+			return;
 		}
 
-		handleCloseModal();
-	}, [deleteTaskStatus, handleCloseModal, status]);
+		try {
+			// Update each task that uses the current status
+			const updatePromises = tasksUsingStatus.map((task) => updateTask({ ...task, status: TaskStatusEnum.OPEN }));
+
+			await Promise.all(updatePromises);
+
+			// Delete the task status after updating related tasks
+			await deleteTaskStatus(status.id);
+		} catch (error) {
+			console.error('Error while deleting task status:', error);
+		} finally {
+			handleCloseModal();
+		}
+	}, [deleteTaskStatus, handleCloseModal, status, tasksUsingStatus, updateTask]);
 
 	return (
 		<Modal
