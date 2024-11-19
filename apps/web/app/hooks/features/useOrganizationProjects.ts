f37fac7c15c617b671b3ba9e1002b1affcd58439
Thierry CH. <thierrybakera12@gmail.com>
2024-11-19 20:06:35 +0200

    [Feature] Show | Edit project information in the task details page (#3347)
    * add project in task details page / possiblity to edit
    
    * Update apps/web/components/pages/task/details-section/blocks/task-secondary-info.tsx
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ruslan Konviser <evereq@gmail.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- apps/web/app/hooks/features/useOrganizationProjects.ts
+++ apps/web/app/hooks/features/useOrganizationProjects.ts
@@ -1,59 +1,83 @@
 import {
 	editOrganizationProjectSettingAPI,
-	editOrganizationProjectAPI
+	editOrganizationProjectAPI,
+	getOrganizationProjectAPI,
+	getOrganizationProjectsAPI
 } from '@app/services/client/api';
 import { userState } from '@app/stores';
 import { useCallback } from 'react';
 import { useAtom } from 'jotai';
 import { useQuery } from '../useQuery';
+import { organizationProjectsState } from '@/app/stores/organization-projects';
 
 export function useOrganizationProjects() {
-  const [user] = useAtom(userState);
+	const [user] = useAtom(userState);
+	const [organizationProjects, setOrganizationProjects] = useAtom(organizationProjectsState);
 
-  const {
-    loading: editOrganizationProjectLoading,
-    queryCall: editOrganizationProjectQueryCall
-  } = useQuery(editOrganizationProjectAPI);
+	const { loading: editOrganizationProjectLoading, queryCall: editOrganizationProjectQueryCall } =
+		useQuery(editOrganizationProjectAPI);
 
-  const {
-    loading: editOrganizationProjectSettingLoading,
-    queryCall: editOrganizationProjectSettingQueryCall
-  } = useQuery(editOrganizationProjectSettingAPI);
+	const { loading: editOrganizationProjectSettingLoading, queryCall: editOrganizationProjectSettingQueryCall } =
+		useQuery(editOrganizationProjectSettingAPI);
 
-  const editOrganizationProjectSetting = useCallback(
-    (id: string, data: any) => {
-      if (user?.tenantId) {
-        return editOrganizationProjectSettingQueryCall(
-          id,
-          data,
-          user?.tenantId || ''
-        ).then((res) => {
-          return res;
-        });
-      }
-    },
-    [user, editOrganizationProjectSettingQueryCall]
-  );
+	const { loading: getOrganizationProjectLoading, queryCall: getOrganizationProjectQueryCall } =
+		useQuery(getOrganizationProjectAPI);
 
-  const editOrganizationProject = useCallback(
-    (id: string, data: any) => {
-      if (user?.tenantId) {
-        return editOrganizationProjectQueryCall(
-          id,
-          data,
-          user?.tenantId || ''
-        ).then((res) => {
-          return res;
-        });
-      }
-    },
-    [user, editOrganizationProjectQueryCall]
-  );
+	const { loading: getOrganizationProjectsLoading, queryCall: getOrganizationProjectsQueryCall } =
+		useQuery(getOrganizationProjectsAPI);
 
-  return {
-    editOrganizationProjectSetting,
-    editOrganizationProjectSettingLoading,
-    editOrganizationProject,
-    editOrganizationProjectLoading
-  };
+	const editOrganizationProjectSetting = useCallback(
+		(id: string, data: any) => {
+			if (user?.tenantId) {
+				return editOrganizationProjectSettingQueryCall(id, data, user?.tenantId || '').then((res) => {
+					return res;
+				});
+			}
+		},
+		[user, editOrganizationProjectSettingQueryCall]
+	);
+
+	const editOrganizationProject = useCallback(
+		(id: string, data: any) => {
+			if (user?.tenantId) {
+				return editOrganizationProjectQueryCall(id, data, user?.tenantId || '').then((res) => {
+					return res;
+				});
+			}
+		},
+		[user, editOrganizationProjectQueryCall]
+	);
+
+	const getOrganizationProject = useCallback(
+		async (id: string) => {
+			try {
+				return await getOrganizationProjectQueryCall(id);
+			} catch (error) {
+				console.log(error);
+			}
+		},
+		[getOrganizationProjectQueryCall]
+	);
+
+	const getOrganizationProjects = useCallback(async () => {
+		try {
+			const res = await getOrganizationProjectsQueryCall();
+
+			setOrganizationProjects(res.data.items);
+		} catch (error) {
+			console.log(error);
+		}
+	}, [getOrganizationProjectsQueryCall, setOrganizationProjects]);
+
+	return {
+		editOrganizationProjectSetting,
+		editOrganizationProjectSettingLoading,
+		editOrganizationProject,
+		editOrganizationProjectLoading,
+		getOrganizationProject,
+		getOrganizationProjectLoading,
+		getOrganizationProjects,
+		getOrganizationProjectsLoading,
+		organizationProjects,
+	};
 }
