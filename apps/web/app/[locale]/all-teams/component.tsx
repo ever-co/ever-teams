61170ae68ae96c5adb89db794fdbd89dae15ab94
Thierry CH. <thierrybakera12@gmail.com>
2024-12-08 19:27:33 +0200

    fix: fix header scroll and card items in the table view - all teams page (#3400)

--- apps/web/app/[locale]/all-teams/component.tsx
+++ apps/web/app/[locale]/all-teams/component.tsx
@@ -5,7 +5,7 @@ import { useAtomValue } from 'jotai';
 import { fullWidthState } from '@app/stores/fullWidth';
 import { withAuthentication } from 'lib/app/authenticator';
 import { Breadcrumb, Container } from 'lib/components';
-import { MainHeader, MainLayout } from 'lib/layout';
+import { MainLayout } from 'lib/layout';
 import { useOrganizationAndTeamManagers } from '@app/hooks/features/useOrganizationTeamManagers';
 import { useEffect } from 'react';
 import { useTranslations } from 'next-intl';
@@ -18,55 +18,57 @@ import { MemberFilter } from 'lib/features/all-teams/all-team-members-filter';
 import { useOrganizationTeams } from '@app/hooks';
 
 function AllTeamsPage() {
-  const t = useTranslations();
-  const fullWidth = useAtomValue(fullWidthState);
-  const view = useAtomValue(allTeamsHeaderTabs);
-  const { filteredTeams, userManagedTeams } = useOrganizationAndTeamManagers();
-  const { isTrackingEnabled } = useOrganizationTeams();
+	const t = useTranslations();
+	const fullWidth = useAtomValue(fullWidthState);
+	const view = useAtomValue(allTeamsHeaderTabs);
+	const { filteredTeams, userManagedTeams } = useOrganizationAndTeamManagers();
+	const { isTrackingEnabled } = useOrganizationTeams();
 
-  const breadcrumb = [
-    { title: JSON.parse(t('pages.home.BREADCRUMB')), href: '/' },
-    { title: t('common.ALL_TEAMS'), href: '/all-teams' }
-  ];
+	const breadcrumb = [
+		{ title: JSON.parse(t('pages.home.BREADCRUMB')), href: '/' },
+		{ title: t('common.ALL_TEAMS'), href: '/all-teams' }
+	];
 
-  /* If the user is not a manager in any team or if he's
+	/* If the user is not a manager in any team or if he's
 		manager in only one team, then redirect him to the home page
 	*/
-  if (userManagedTeams.length < 2) return <RedirectUser />;
+	if (userManagedTeams.length < 2) return <RedirectUser />;
 
-  return (
-    <MainLayout showTimer={isTrackingEnabled} className="items-start">
-      <MainHeader
-        fullWidth={fullWidth}
-        className={'pb-2 pt-10 sticky top-20 z-50'}
-      >
-        {/* Breadcrumb */}
-        <div className="flex flex-row items-start justify-between mb-5">
-          <Breadcrumb paths={breadcrumb} className="text-sm" />
-          <div className="flex flex-col items-end gap-2">
-            <div className="flex items-center justify-center h-10 gap-1 w-max">
-              <HeaderTabs />
-            </div>
-            <MemberFilter />
-          </div>
-        </div>
-        <TeamMemberHeader view={IssuesView.CARDS} />
-      </MainHeader>
-      <Container fullWidth={fullWidth} className="flex py-10 pt-20">
-        <AllTeamsMembers teams={filteredTeams} view={view} />
-      </Container>
-    </MainLayout>
-  );
+	return (
+		<MainLayout
+			showTimer={isTrackingEnabled}
+			className="items-start"
+			mainHeaderSlot={
+				<div className="flex w-full flex-col items-start  justify-between">
+					<div className="w-full flex items-center justify-between py-2 px-4">
+						<Breadcrumb paths={breadcrumb} className="text-sm" />
+						<div className="flex self-end items-center gap-2">
+							<div className="flex items-center justify-center h-10 gap-1 w-max">
+								<HeaderTabs />
+							</div>
+							<MemberFilter />
+						</div>
+					</div>
+
+					{view == IssuesView.CARDS && <TeamMemberHeader view={IssuesView.CARDS} />}
+				</div>
+			}
+		>
+			<Container fullWidth={fullWidth} className="mx-auto mt-5">
+				<AllTeamsMembers teams={filteredTeams} view={view} />
+			</Container>
+		</MainLayout>
+	);
 }
 
 function RedirectUser() {
-  const router = useRouter();
-  useEffect(() => {
-    router.push('/');
-  }, [router]);
-  return <></>;
+	const router = useRouter();
+	useEffect(() => {
+		router.push('/');
+	}, [router]);
+	return <></>;
 }
 
 export default withAuthentication(AllTeamsPage, {
-  displayName: 'AllManagedTeams'
+	displayName: 'AllManagedTeams'
 });
