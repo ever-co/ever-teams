6a6bbd4005f7096139ad8088aecfb3f970d84a54
sv7000 <sumitvds360@gmail.com>
2025-01-27 19:18:30 +0530

    feat: hcaptcha onSuccess and onError added

--- apps/web/app/[locale]/auth/team/component.tsx
+++ apps/web/app/[locale]/auth/team/component.tsx
@@ -135,18 +135,51 @@ function FillUserDataForm({
 	const t = useTranslations();
 	const captchaType = process.env.NEXT_PUBLIC_CAPTCHA_TYPE;
 
+	// const renderCaptcha = () => {
+	// 	switch (captchaType) {
+	// 		case 'hcaptcha':
+	// 			return (
+	// 				<HCaptcha
+	// 					sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY ?? ''}
+	// 				/>
+	// 			);
+	// 		case 'cloudflare':
+	// 			return (
+	// 				<Turnstile
+	// 					sitekey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY ?? ''}
+	// 				/>
+	// 			);
+	// 		case 'google':
+	// 		default:
+	// 			return <ReCAPTCHA errors={errors} handleOnChange={handleOnChange} />;
+	// 	}
+	// };
+
 	const renderCaptcha = () => {
+		const handleCaptchaVerify = (token: string) => {
+			handleOnChange({ target: { name: 'captchaToken', value: token } });
+		};
+
+		const handleCaptchaError = () => {
+			handleOnChange({ target: { name: 'captchaToken', value: '' } });
+			console.error('CAPTCHA verification failed.');
+		};
+
 		switch (captchaType) {
 			case 'hcaptcha':
 				return (
 					<HCaptcha
 						sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY ?? ''}
+						onVerify={(token) => handleCaptchaVerify(token)}
+						onError={() => handleCaptchaError()}
 					/>
 				);
 			case 'cloudflare':
 				return (
 					<Turnstile
 						sitekey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY ?? ''}
+						onSuccess={(token) => handleCaptchaVerify(token)}
+						onError={() => handleCaptchaError()}
 					/>
 				);
 			case 'google':
