0021745f028ef3af82ad7a04fd17278cdb8de6e6
AKILIMAILI CIZUNGU Innocent <51681130+Innocent-Akim@users.noreply.github.com>
2025-02-03 20:56:45 +0200

    [Feat]: Update date range picker and Select components (#3574)
    * feat(Dashboard): Update date range picker and select components
    
    * Add functionality to settings button

--- apps/web/app/[locale]/dashboard/team-dashboard/[teamId]/components/date-range-picker.tsx
+++ apps/web/app/[locale]/dashboard/team-dashboard/[teamId]/components/date-range-picker.tsx
@@ -21,6 +21,7 @@ import {
 } from 'date-fns';
 import { DateRange } from 'react-day-picker';
 import { useTranslations } from 'next-intl';
+import { SettingsIcon } from './team-icon';
 
 interface DateRangePickerProps {
 	className?: string;
@@ -28,7 +29,7 @@ interface DateRangePickerProps {
 }
 
 export function DateRangePicker({ className, onDateRangeChange }: DateRangePickerProps) {
-	const t=useTranslations();
+	const t = useTranslations();
 	const [dateRange, setDateRange] = React.useState<DateRange | undefined>({
 		from: new Date(),
 		to: new Date()
@@ -36,14 +37,14 @@ export function DateRangePicker({ className, onDateRangeChange }: DateRangePicke
 	const [isPopoverOpen, setIsPopoverOpen] = React.useState(false);
 	const [currentMonth, setCurrentMonth] = React.useState<Date>(new Date());
 
-  const handleDateRangeChange = (range: DateRange | undefined) => {
-     try {
-         setDateRange(range);
-         onDateRangeChange?.(range);
-     } catch (error) {
-       console.error('Error handling date range change:', error);
-     }
-     };
+	const handleDateRangeChange = (range: DateRange | undefined) => {
+		try {
+			setDateRange(range);
+			onDateRangeChange?.(range);
+		} catch (error) {
+			console.error('Error handling date range change:', error);
+		}
+	};
 
 	const predefinedRanges = [
 		{
@@ -105,7 +106,7 @@ export function DateRangePicker({ className, onDateRangeChange }: DateRangePicke
 			}
 		},
 		{
-			label:t('common.THIS_MONTH'),
+			label: t('common.THIS_MONTH'),
 			action: () => {
 				const today = new Date();
 				handleDateRangeChange({
@@ -146,33 +147,45 @@ export function DateRangePicker({ className, onDateRangeChange }: DateRangePicke
 
 		if (isSameYear(range.from, range.to)) {
 			if (isSameMonth(range.from, range.to)) {
-				return `${format(range.from, 'd')} - ${format(range.to, 'd MMM yyyy')}`;
+				return `${format(range.from, 'MMM d')} - ${format(range.to, 'd yyyy')}`;
 			}
-			return `${format(range.from, 'd MMM')} - ${format(range.to, 'd MMM yyyy')}`;
+			return `${format(range.from, 'MMM d')} - ${format(range.to, 'd MMM yyyy')}`;
 		}
-		return `${format(range.from, 'd MMM yyyy')} - ${format(range.to, 'd MMM yyyy')}`;
+		return `${format(range.from, 'MMM d')} - ${format(range.to, 'd MMM yyyy')}`;
 	};
 
 	return (
 		<Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
 			<PopoverTrigger asChild>
-				<Button
-					variant="outline"
-					className={cn(
-						'justify-between gap-2 px-3 py-2 min-w-[240px] text-center flex items-center',
-						!dateRange && 'text-muted-foreground',
-						className
-					)}
-				>
-					{dateRange ? formatDateRange(dateRange) : t('common.SELECT')}
-					<ChevronDown className="w-4 h-4" />
-				</Button>
+				<div className="flex items-center border border-[#E4E4E7] dark:border-[#2D2D2D] rounded-md w-[225px]">
+					<Button
+						variant="outline"
+						className={cn(
+							'flex items-center justify-between gap-2 px-3 py-1 w-[196px] h-[36px] bg-white dark:bg-dark--theme-light rounded-r-none',
+							!dateRange && 'text-muted-foreground',
+							className
+						)}
+					>
+						{dateRange ? formatDateRange(dateRange) : t('common.SELECT')}
+						<ChevronDown className="w-4 h-4" />
+					</Button>
+					<button
+						onClick={() => {
+							/* Add handler */
+						}}
+						title="Open settings"
+						aria-label="Open settings"
+						className="flex items-center justify-center gap-2 px-2 py-1 w-[36px] h-[36px] bg-white dark:bg-dark--theme-light border-l  border-l-[#E4E4E7] dark:border-l-[#2D2D2D] rounded-r-md"
+					>
+						<SettingsIcon />
+					</button>
+				</div>
 			</PopoverTrigger>
 			<PopoverContent
 				onClick={(e) => e.stopPropagation()}
 				onMouseDown={(e) => e.stopPropagation()}
 				onChange={(e) => e.stopPropagation()}
-				className="p-0 w-auto"
+				className="p-0 w-auto dark:bg-dark--theme-light dark:border-[#2D2D2D] border border-[#E4E4E7] rounded-md"
 				align="center"
 			>
 				<div className="flex flex-row-reverse">
