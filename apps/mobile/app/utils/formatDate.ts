05d3549a0988da37f3a82354146cd7eb5896a3f4
sergemuhundu <sergemuhundu2@gmail.com>
2025-03-18 21:37:11 +0100

    Fix expo build issues

--- apps/mobile/app/utils/formatDate.ts
+++ apps/mobile/app/utils/formatDate.ts
@@ -9,7 +9,7 @@ type Options = Parameters<typeof format>[2];
 
 const getLocale = (): Locale => {
 	const locale = I18n.currentLocale().split('-')[0];
-	return locale === 'ar' ? ar : locale === 'ko' ? ko : en;
+	return (locale === 'ar' ? ar : locale === 'ko' ? ko : en) as any;
 };
 
 export const formatDate = (date: string, dateFormat?: string, options?: Options) => {
