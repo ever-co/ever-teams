2b35c93039fe9634a67e78890f2efff691c26005
NdekoCode <arickbulakali@gmail.com>
2025-05-01 16:44:07 +0200

    refactor(web): enhance Tailwind configuration for typography
    - Added custom font sizes, line heights, and letter spacing to the Tailwind configuration for improved text styling.
    - Ensured better readability and consistency across the application by defining new typography settings.

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -33,6 +33,27 @@ module.exports = {
 			}
 		},
 		extend: {
+			fontSize: {
+				xs: ['12px', '18px'],
+				sm: ['14px', '20px'],
+				base: ['16px', '24px'],
+				lg: ['18px', '28px'],
+				xl: ['20px', '30px'],
+				'2xl': ['24px', '32px'],
+				'3xl': ['30px', '36px'],
+				'4xl': ['36px', '40px'],
+				'5xl': ['48px', '1']
+			},
+			lineHeight: {
+				tight: '1.2',
+				normal: '1.5',
+				relaxed: '1.625'
+			},
+			letterSpacing: {
+				tight: '-0.015em',
+				normal: '0em',
+				wide: '0.015em'
+			},
 			colors: {
 				transparent: 'transparent',
 				current: 'currentColor',
