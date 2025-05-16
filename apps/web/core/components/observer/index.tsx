70e72c6f6e355134d456324c91867e9030342479
CREDO23 <thierrybakera12@gmail.com>
2025-05-16 12:16:24 +0200

    fix case problem

---
+++ apps/web/core/components/observer/index.tsx
@@ -0,0 +1,62 @@
+import { useCallbackRef } from '@/core/hooks';
+import React, { JSX } from 'react';
+
+export const ObserverComponent = ({
+	isLast,
+	getNextData
+}: {
+	root?: JSX.Element;
+	isLast: boolean;
+	getNextData: () => any;
+}) => {
+	const getNextDataRef = useCallbackRef(getNextData);
+	const cardRef = React.useRef<HTMLDivElement>(null);
+
+	React.useEffect(() => {
+		if (!cardRef?.current) return;
+
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				if (isLast && entry.isIntersecting) {
+					// fetch with new Entry
+					console.log('IN OBSERVER');
+					getNextDataRef.current && getNextDataRef.current();
+					observer.unobserve(entry.target);
+				}
+			},
+			{
+				threshold: 1.0
+			}
+		);
+
+		observer.observe(cardRef.current);
+
+		return () => {
+			// eslint-disable-next-line react-hooks/exhaustive-deps
+			cardRef.current && observer.unobserve(cardRef.current);
+		};
+	}, [isLast, getNextDataRef]);
+	return <div ref={cardRef} className="-z-10 h-2 bg-transparent  " />;
+};
+
+export const useElementOnScreen = (options: IntersectionObserverInit | undefined) => {
+	const containerRef = React.useRef<HTMLDivElement>(null);
+	const [isVisible, setIsVisible] = React.useState();
+
+	const cbFunction = (entries: any[]) => {
+		const [entry] = entries;
+		setIsVisible(entry.isIntersecting);
+	};
+
+	React.useEffect(() => {
+		const observer = new IntersectionObserver(cbFunction, options);
+		if (containerRef.current) observer.observe(containerRef.current);
+
+		return () => {
+			// eslint-disable-next-line react-hooks/exhaustive-deps
+			if (containerRef.current) observer.unobserve(containerRef.current);
+		};
+	}, [containerRef, options]);
+
+	return [isVisible, containerRef];
+};
