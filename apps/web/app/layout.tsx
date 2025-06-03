c1f1871ff3a1fb4c34e075759254321a4f5dbbe3
NdekoCode <arickbulakali@gmail.com>
2025-06-03 08:46:01 +0200

    refactor(web): Simplify RootLayout structure and improve development tooling integration
    - Moved the dynamic import of `ReactQueryDevtools` to a more organized location within the RootLayout component.
    - Streamlined the layout structure by removing unnecessary HTML tags, enhancing readability and maintainability.
    - Ensured that the `Toaster` component remains integrated for user notifications while maintaining conditional rendering for development tools.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,10 +1,4 @@
-// `@tanstack/react-query-devtools` is useful only during development.
-// A dynamic import ensures it isn’t included in the production bundle.
-const ReactQueryDevtools =
-	process.env.NODE_ENV === 'development'
-		? dynamic(() => import('@tanstack/react-query-devtools').then((mod) => mod.ReactQueryDevtools))
-		: () => null;
-
+import '@/styles/globals.css';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { Toaster } from 'sonner';
 import { QueryClientProvider } from '@/core/components/providers/query-client-provider';
@@ -12,17 +6,19 @@ import dynamic from 'next/dynamic';
 
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
+// `@tanstack/react-query-devtools` is useful only during development.
+// A dynamic import ensures it isn’t included in the production bundle.
+const ReactQueryDevtools =
+	process.env.NODE_ENV === 'development'
+		? dynamic(() => import('@tanstack/react-query-devtools').then((mod) => mod.ReactQueryDevtools))
+		: () => null;
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
-			<body>
-				<QueryClientProvider>
-					{children}
-					<Toaster richColors />
-					{/* Render devtools only in development */}
-					{process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
-				</QueryClientProvider>
-			</body>
-		</html>
+		<QueryClientProvider>
+			{children}
+			<Toaster richColors />
+			{/* Render devtools only in development */}
+			{process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+		</QueryClientProvider>
 	);
 }
