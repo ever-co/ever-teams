696ca4091c8a734f139ef086a53c22457ff46800
CREDO23 <thierrybakera12@gmail.com>
2025-07-23 20:37:36 +0200

    update yarn lock

--- apps/web/core/components/common/team-member.tsx
+++ apps/web/core/components/common/team-member.tsx
@@ -67,10 +67,10 @@ const TeamMemberSection = () => {
 											<div className="w-32 h-3 mb-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
 										</div>
 									</div>
-									<div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
-									<div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
-									<div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
-									<div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-24"></div>
+									<div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24" />
+									<div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24" />
+									<div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24" />
+									<div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-24" />
 								</div>
 							</li>
 						);
