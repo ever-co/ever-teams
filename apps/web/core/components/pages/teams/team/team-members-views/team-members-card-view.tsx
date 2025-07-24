77062740c60c0b24b4b69bf8ac62266f5ef65128
CREDO23 <thierrybakera12@gmail.com>
2025-07-24 17:01:42 +0200

    show  only invitations that are not accepted

--- apps/web/core/components/pages/teams/team/team-members-views/team-members-card-view.tsx
+++ apps/web/core/components/pages/teams/team/team-members-views/team-members-card-view.tsx
@@ -6,6 +6,7 @@ import { UserTeamCard } from './user-team-card';
 import { TOrganizationTeamEmployee } from '@/core/types/schemas';
 import { InvitedCard, InviteUserTeamCard } from '@/core/components/teams/invite/user-invite-card';
 import { InviteFormModal } from '@/core/components/features/teams/invite-form-modal';
+import { EInviteStatus } from '@/core/types/generics/enums/invite';
 
 interface Props {
 	teamMembers: TOrganizationTeamEmployee[];
@@ -106,11 +107,13 @@ const TeamMembersCardView: React.FC<Props> = memo(
 					})}
 
 					{teamInvitations.length > 0 &&
-						teamInvitations.map((invitation) => (
-							<li key={invitation.id}>
-								<InvitedCard invitation={invitation} />
-							</li>
-						))}
+						teamInvitations
+							.filter((invitation) => invitation.status !== EInviteStatus.ACCEPTED)
+							.map((invitation) => (
+								<li key={invitation.id}>
+									<InvitedCard invitation={invitation} />
+								</li>
+							))}
 
 					{/* Loader skeleton */}
 					<Transition
