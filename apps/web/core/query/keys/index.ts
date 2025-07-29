795bae863505e62ee3e38e8b00f47936ea6eaaf1
CREDO23 <thierrybakera12@gmail.com>
2025-07-29 12:27:00 +0200

    support validateByCode & validateByToken query keys for invitations api calls

--- apps/web/core/query/keys/index.ts
+++ apps/web/core/query/keys/index.ts
@@ -84,6 +84,10 @@ export const queryKeys = {
 				['users', 'invitations', 'my', ...(tenantId ? [tenantId] : [])] as const,
 			operations: {
 				all: ['users', 'invitations', 'operations'] as const,
+				validateByCode: (code: string | undefined | null, email: string | undefined | null) =>
+					['validate-invite', ...(code ? [code] : []), ...(email ? [email] : [])] as const,
+				validateByToken: (token: string | undefined | null, email: string | undefined | null) =>
+					['validate-invite', ...(token ? [token] : []), ...(email ? [email] : [])] as const,
 				invite: (teamId: string | undefined | null) =>
 					['users', 'invitations', 'operations', 'invite', ...(teamId ? [teamId] : [])] as const,
 				remove: (invitationId: string | undefined | null) =>
