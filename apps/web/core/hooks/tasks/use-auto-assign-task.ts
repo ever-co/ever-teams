a2beeb07456c4830d0332dafee10fa1e92377a84
CREDO23 <thierrybakera12@gmail.com>
2025-08-01 15:10:44 +0200

    add ai bot suggestions

--- apps/web/core/hooks/tasks/use-auto-assign-task.ts
+++ apps/web/core/hooks/tasks/use-auto-assign-task.ts
@@ -36,7 +36,7 @@ export function useAutoAssignTask() {
 				members: [...(task.members || []), newMember ? newMember.employee : {}]
 			});
 		},
-		[updateTask, updateLoadingRef]
+		[updateTask, updateLoadingRef, activeTeam]
 	);
 
 	useEffect(() => {
