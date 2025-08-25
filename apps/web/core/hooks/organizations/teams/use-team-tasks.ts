e684cc6c11e0faa890439eee15ea23c476b012b2
CREDO23 <thierrybakera12@gmail.com>
2025-08-25 19:18:48 +0200

    clean up

--- apps/web/core/hooks/organizations/teams/use-team-tasks.ts
+++ apps/web/core/hooks/organizations/teams/use-team-tasks.ts
@@ -147,8 +147,6 @@ export function useTeamTasks() {
 					task.id === taskId ? { ...task, ...updatedTask } : task
 				);
 
-				setDetailedTask(updatedTask);
-
 				// Sync the tasks store
 				setAllTasks(updatedItems);
 
