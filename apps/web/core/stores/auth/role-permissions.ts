15bde4c90df8eb827bd1807c5db29c58d75eb60a
Thierry CH. <thierrybakera12@gmail.com>
2025-09-04 21:17:59 -0700

    [Feature] Implement “Get My Permissions” api service (#4000)
    * add my role permissions API service
    
    * Add a global jotai atom that holds the role permissions of the connected user
    
    * add ai bot suggestions
    
    * add ai bot suggestions
    
    * add ai bot suggestions

--- apps/web/core/stores/auth/role-permissions.ts
+++ apps/web/core/stores/auth/role-permissions.ts
@@ -2,7 +2,13 @@ import { TRolePermission } from '@/core/types/schemas/role/role-permission-schem
 import { atom } from 'jotai';
 
 export const rolePermissionsState = atom<TRolePermission[]>([]);
+export const myRolePermissionsState = atom<TRolePermission[]>([]);
 
 export const rolePermissionsFormatedState = atom<{
 	[key: string]: TRolePermission;
 }>({});
+
+export const myPermissions = atom<string[]>((get) => {
+	const myRolePermissions = get(myRolePermissionsState);
+	return myRolePermissions.map((item) => item.permission);
+});
