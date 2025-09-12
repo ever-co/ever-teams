0e2b60690d9ed688537682a496b7283c5ce93e75
CREDO23 <thierrybakera12@gmail.com>
2025-09-12 13:02:47 +0200

    fix UI issues

--- apps/web/core/components/pages/kanban/sort-tasks-status-settings.tsx
+++ apps/web/core/components/pages/kanban/sort-tasks-status-settings.tsx
@@ -64,44 +64,51 @@ const SortTasksStatusSettings = ({ arr, onClose }: { arr: TTaskStatus[]; onClose
 		}
 	};
 	return (
-		<div className="w-[600px] h-[700px] py-12 pr-2  bg-[#f2f2f2] dark:bg-[#191a20] rounded-lg flex flex-col justify-center items-center">
-			<h2 className="text-2xl font-bold text-center mb-4">{t('pages.settingsTeam.SORT_TASK_STATUSES')}</h2>
-			<ScrollArea className="h-[450px] pr-3">
-				<DragDropContext onDragEnd={onDragEnd}>
-					<Droppable droppableId="droppable">
-						{(provided) => (
-							<div {...provided.droppableProps} ref={provided.innerRef}>
-								<>
-									{items.map((item, index) => (
-										<Draggable key={item.id} draggableId={item.id} index={index}>
-											{(provided) => (
-												<div
-													className="py-2 text-base rounded-md w-80"
-													{...provided.draggableProps}
-													ref={provided.innerRef}
-												>
-													<div className="border flex justify-start items-center p-4 text-base rounded-md w-80">
-														<div {...provided.dragHandleProps}>
-															<SixSquareGridIcon className="w-6 h-6" />
+		<div className="w-[500px] h-[700px] bg-[#f2f2f2] dark:bg-[#191a20] rounded-lg flex flex-col justify-between items-center">
+			<div className="w-full grow flex flex-col item-center justify-center">
+				<h2 className="text-2xl font-bold text-center mb-4">{t('pages.settingsTeam.SORT_TASK_STATUSES')}</h2>
+				<div className="h-[450px] w-full flex items-center justify-center">
+					<ScrollArea className="h-full">
+						<DragDropContext onDragEnd={onDragEnd}>
+							<Droppable droppableId="droppable">
+								{(provided) => (
+									<div {...provided.droppableProps} ref={provided.innerRef}>
+										<>
+											{items.map((item, index) => (
+												<Draggable key={item.id} draggableId={item.id} index={index}>
+													{(provided) => (
+														<div
+															className="py-2 text-base rounded-md w-80"
+															{...provided.draggableProps}
+															ref={provided.innerRef}
+														>
+															<div className="border flex justify-start items-center p-4 text-base rounded-md w-80">
+																<div {...provided.dragHandleProps}>
+																	<SixSquareGridIcon className="w-6 h-6" />
+																</div>
+																<p className="capitalize">
+																	{item.name?.split('-').join(' ')}
+																</p>
+															</div>
 														</div>
-														<p className="capitalize">{item.name?.split('-').join(' ')}</p>
-													</div>
-												</div>
-											)}
-										</Draggable>
-									))}
-									{provided.placeholder}
-								</>
-							</div>
-						)}
-					</Droppable>
-				</DragDropContext>
-			</ScrollArea>
-			<div className="absolute bottom-4 -right-20 flex w-80 space-x-2 mt-2">
-				<Button onClick={onClose} variant={'outline'} className="p-4 w-28 !h-14 border-none ">
+													)}
+												</Draggable>
+											))}
+											{provided.placeholder}
+										</>
+									</div>
+								)}
+							</Droppable>
+						</DragDropContext>
+					</ScrollArea>
+				</div>
+			</div>
+
+			<div className=" shrink-0 w-full p-2 flex justify-end gap-2">
+				<Button onClick={onClose} variant={'outline'} className="p-4 w-28 !h-14 border-none">
 					{t('common.CANCEL')}
 				</Button>
-				<Button onClick={saveOrder} className="dark:text-white !h-14 space-x-2">
+				<Button onClick={saveOrder} className="dark:text-white w-28 !h-14 space-x-2">
 					{t('common.SAVE')} {saveLoader && <Spinner dark={false} />}
 				</Button>
 			</div>
