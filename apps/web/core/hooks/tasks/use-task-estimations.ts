0e66aadad0ef3c977688108917afb62a22341555
Thierry CH. <thierrybakera12@gmail.com>
2025-09-22 03:02:00 -0700

    [Task] Implement useMemberEstimation hook exposing addMemberEstimation & editMemberEstimation (#4023)
    * add add task estimation handler
    
    * add edit task estimation handler
    
    * clean up
    
    * Update apps/web/core/hooks/tasks/use-task-estimations.ts
    
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ruslan Konviser <evereq@gmail.com>
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

---
+++ apps/web/core/hooks/tasks/use-task-estimations.ts
@@ -0,0 +1,35 @@
+import { taskEstimationsService } from '@/core/services/client/api/tasks/task-estimations.service';
+import { TTaskEstimations } from '@/core/types/schemas/task/task.schema';
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
+
+export function useTaskEstimations() {
+	const addEstimationMutation = useMutation({
+		mutationFn: (data: TTaskEstimations) => {
+			return taskEstimationsService.addEstimation(data);
+		},
+		onSuccess: () => {
+			toast.success('Task estimation added successfully');
+		},
+		onError: (error) => {
+			toast.error('Error adding task estimation');
+		}
+	});
+
+	const editTaskEstimationMutation = useMutation({
+		mutationFn: (data: TTaskEstimations) => {
+			return taskEstimationsService.editEstimation(data);
+		},
+		onSuccess: () => {
+			toast.success('Task estimation updated successfully');
+		},
+		onError: (error) => {
+			toast.error('Error editing task estimation');
+		}
+	});
+
+	return {
+		addEstimationMutation,
+		editTaskEstimationMutation
+	};
+}
