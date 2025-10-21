2d65262d9ac139dbdd389457062777ce5b1d601a
CREDO23 <thierrybakera12@gmail.com>
2025-10-21 21:33:56 +0200

    clean up comments

--- apps/web/core/components/pages/settings/team/issues-settings.tsx
+++ apps/web/core/components/pages/settings/team/issues-settings.tsx
@@ -151,7 +151,7 @@ export const IssuesSettings = () => {
 				</div>
 			</div>
 			{/* TODO */}
-			<div className="hidden _flex w-full items-center justify-between gap-[2rem]">
+			<div className="hidden w-full items-center justify-between gap-[2rem]">
 				<Text className="flex-none flex-grow-0 w-1/5 text-lg font-normal text-gray-400 md-2">
 					{t('pages.settingsTeam.AUTO_STATUS')}
 				</Text>
@@ -164,9 +164,6 @@ export const IssuesSettings = () => {
 				<TaskVersionForm />
 			</div>
 			{/* TODO */}
-			{/* <div className="hidden _flex w-full items-center justify-between gap-[2rem]">
-				<IssueTypeForm />
-			</div> */}
 			<div className="flex w-full items-center justify-between gap-[2rem]">
 				<DefaultIssueTypeForm />
 			</div>
