18dd3d374f6839f493fff8284d4b727704feea20
CREDO23 <thierrybakera12@gmail.com>
2025-10-28 15:24:47 +0200

    handle team manager view

--- apps/web/core/components/common/color-picker.tsx
+++ apps/web/core/components/common/color-picker.tsx
@@ -4,7 +4,7 @@ import { useCallbackRef, useOrganizationTeams } from '@/core/hooks';
 import { useState, useRef, useCallback } from 'react';
 import { HexColorPicker } from 'react-colorful';
 import { CheckSquareOutlineIcon, EditPenUnderlineIcon, TrashIcon } from 'assets/svg';
-import {LoaderCircle } from 'lucide-react';
+import { LoaderCircle } from 'lucide-react';
 import { useAtomValue } from 'jotai';
 import { activeTeamState } from '@/core/stores';
 import { toast } from 'sonner';
@@ -195,59 +195,63 @@ export const ColorPicker = ({
 			className={`group px-3 relative w-[100%] h-[48px] border rounded-[10px] flex gap-1 items-center justify-between input-border  dark:bg-dark--theme-light`}
 		>
 			<span className="w-[5rem] shrink-0 text-left ">{color || 'Color'}</span>
-			<Popover className="group grow">
-				{({ open, close }) => (
-					<>
-						<PopoverButton className="w-full flex items-center gap-2 justify-between h-full outline-none">
-
-							{open ? (
-								<div className="grow h-full flex items-center justify-end gap-2">
-									{editOrganizationTeamLoading ? (
-										<LoaderCircle className="w-[18px] h-[18px] animate-spin" strokeWidth="1.4" />
-									) : (
-										<CheckSquareOutlineIcon
-											className="w-[18px] h-[18px]"
-											strokeWidth="1.4"
-											onClick={async (e) => {
-												e.stopPropagation();
-												await updateTeamColor();
-												close();
-											}}
-										/>
-									)}
-								</div>
-							) : (
-								<div className="grow h-full flex items-center  justify-end gap-2">
-									<EditPenUnderlineIcon className="w-6 h-6 cursor-pointer" />
-									{!open && color ? (
-										editOrganizationTeamLoading ? (
+			{isTeamManager ? (
+				<Popover className="group grow">
+					{({ open, close }) => (
+						<>
+							<PopoverButton className="w-full flex items-center gap-2 justify-between h-full outline-none">
+								{open ? (
+									<div className="grow h-full flex items-center justify-end gap-2">
+										{editOrganizationTeamLoading ? (
 											<LoaderCircle
 												className="w-[18px] h-[18px] animate-spin"
 												strokeWidth="1.4"
 											/>
 										) : (
-											<TrashIcon
-												className="w-5 cursor-pointer"
+											<CheckSquareOutlineIcon
+												className="w-[18px] h-[18px]"
+												strokeWidth="1.4"
 												onClick={async (e) => {
 													e.stopPropagation();
-													await removeTeamColor();
+													await updateTeamColor();
+													close();
 												}}
 											/>
-										)
-									) : null}
-								</div>
-							)}
-						</PopoverButton>
-						<PopoverPanel anchor="bottom end" className="h-[14rem] w-[13rem] mt-5">
-							<HexColorPicker
-								color={color || undefined}
-								onChange={handleColorChange}
-								key={color ? `color-picker-${color}` : 'color-picker-default'}
-							/>
-						</PopoverPanel>
-					</>
-				)}
-			</Popover>
+										)}
+									</div>
+								) : (
+									<div className="grow h-full flex items-center  justify-end gap-2">
+										<EditPenUnderlineIcon className="w-6 h-6 cursor-pointer" />
+										{!open && color ? (
+											editOrganizationTeamLoading ? (
+												<LoaderCircle
+													className="w-[18px] h-[18px] animate-spin"
+													strokeWidth="1.4"
+												/>
+											) : (
+												<TrashIcon
+													className="w-5 cursor-pointer"
+													onClick={async (e) => {
+														e.stopPropagation();
+														await removeTeamColor();
+													}}
+												/>
+											)
+										) : null}
+									</div>
+								)}
+							</PopoverButton>
+							<PopoverPanel anchor="bottom end" className="h-[14rem] w-[13rem] mt-5">
+								<HexColorPicker
+									color={color || undefined}
+									onChange={handleColorChange}
+									key={color ? `color-picker-${color}` : 'color-picker-default'}
+								/>
+							</PopoverPanel>
+						</>
+					)}
+				</Popover>
+			) : null}
 		</div>
 	);
 };
