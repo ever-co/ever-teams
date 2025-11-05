0b4700b1a311a1ff31a4cf9cd8aee0123be9d6a1
CREDO23 <thierrybakera12@gmail.com>
2025-11-05 13:02:38 +0200

    persist thee last view mode in local storage

--- apps/web/core/components/common/header-tabs.tsx
+++ apps/web/core/components/common/header-tabs.tsx
@@ -2,7 +2,7 @@ import { clsxm } from '@/core/lib/utils';
 import LinkWrapper from './link-wrapper';
 import { QueueListIcon, Squares2X2Icon, TableCellsIcon } from '@heroicons/react/20/solid';
 import KanbanIcon from '@/core/components/svgs/kanban';
-import { IssuesView } from '@/core/constants/config/constants';
+import { IssuesView, LAST_TEAM_MEMBERS_VIEW_MODE } from '@/core/constants/config/constants';
 import { useAtom } from 'jotai';
 import { headerTabs } from '@/core/stores/common/header-tabs';
 import { DottedLanguageObjectStringPaths, useTranslations } from 'next-intl';
@@ -19,6 +19,10 @@ const HeaderTabs = ({ linkAll, kanban = false }: { linkAll: boolean; kanban?: bo
 	const links = linkAll ? ['/', '/', '/', '/kanban'] : [undefined, undefined, undefined, '/kanban'];
 	const [view, setView] = useAtom(headerTabs);
 	const activeView = kanban ? IssuesView.KANBAN : view;
+
+	const setLastTeamMembersViewMode = (viewMode: IssuesView) => {
+		localStorage?.setItem(LAST_TEAM_MEMBERS_VIEW_MODE, viewMode);
+	};
 	return (
 		<>
 			{options.map(({ label, icon: Icon, view: optionView }, index) => (
@@ -38,6 +42,7 @@ const HeaderTabs = ({ linkAll, kanban = false }: { linkAll: boolean; kanban?: bo
 							onClick={() => {
 								if (links[index] !== '/kanban') {
 									setView(optionView);
+									setLastTeamMembersViewMode(optionView);
 								}
 							}}
 						>
