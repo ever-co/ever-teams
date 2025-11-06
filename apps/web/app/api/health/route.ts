6e7b4cc088ce3602972f0db9058ab9330a757a8c
NdekoCode <arickbulakali@gmail.com>
2025-11-06 12:15:29 +0200

    refactor: clean up unused imports and improve code readability

--- apps/web/app/api/health/route.ts
+++ apps/web/app/api/health/route.ts
@@ -1,6 +1,13 @@
-import { getDefaultRequest } from '@/core/services/server/requests/default';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-	return NextResponse.json(await getDefaultRequest());
+	return NextResponse.json({
+		data: {
+			status: 200,
+			message: 'Ever Teams Next.js API'
+		},
+		response: {
+			url: `${process.env.NEXT_PUBLIC_WEB_APP_URL || process.env.CLIENT_BASE_URL}/api/`
+		}
+	});
 }
