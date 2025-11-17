71d00ec837ff59aa5231568a00adfeb3f730e703
CREDO23 <thierrybakera12@gmail.com>
2025-11-17 09:36:38 +0200

    migrate to tailwindcss v4

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
+
 module.exports = {
 	plugins: {
-		tailwindcss: {
-			config: path.join(__dirname, 'tailwind.config.js')
-		},
+		// tailwindcss: {
+		// 	config: path.join(__dirname, 'tailwind.config.js')
+		// },
+		"@tailwindcss/postcss": {},
 		autoprefixer: {}
 		// 'flex-gap-polyfill': {},
 	}
